45932da17caf696bb3b4848baa723a4d6cc56e39
thomas <szemrot@hotmail.com>
2014-06-30 21:57:04 +0200

    Modified capa.sh

--- cuve/capa.sh
+++ cuve/capa.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 while true; 
 	do 
-		sudo /usr/share/adafruit/webide/repositories/my-pi-projects/cuve/capa.py; 
+		/usr/share/adafruit/webide/repositories/my-pi-projects/cuve/capa.py; 
         sleep 5; 
     done
