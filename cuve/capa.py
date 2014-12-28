b0c5863d665c99d543350c54bd11725aacd3bef6
thomas <szemrot@hotmail.com>
2014-12-28 16:19:59 +0100

    Modified capa.py

--- cuve/capa.py
+++ cuve/capa.py
@@ -64,7 +64,7 @@ while True:
     
         # Allow module to settle
         #time.sleep(0.5)
-        time.sleep(1)
+        time.sleep(2)
     
         # Send 10us pulse to trigger
         GPIO.output(GPIO_TRIGGER, True)
