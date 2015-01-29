a00ec72adda91055fd0eff7fc5cf0e2e168b8a60
thomas <szemrot@hotmail.com>
2015-01-29 11:54:56 +0100

    Modified capa.py

--- cuve/capa.py
+++ cuve/capa.py
@@ -173,7 +173,7 @@ while True:
     index = index +1
     if index == NDERIVE:
         index = 0
-    lissage = distance + sum/(len(derivee)*1.0)        
+    lissage = distance + sum(derivee)/(len(derivee)*1.0
         
     
     #logfile
