0159b10e8393fb11fdad595b4f2338f64fd8726a
thomas <szemrot@hotmail.com>
2015-01-31 21:27:20 +0100

    Modified creation_graph_year.sh

---
+++ cuve/creation_graph_year.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+DIR="/home/webide/repositories/my-pi-projects/cuve"
+
+JOUR=$(date '+%d-%m-%Y %H-%M-%S')
+
+# Affichage en degre C
+ECHELLE="cm"
+VOLUME="litres"
+
+
+#Graphs pour 6 mois
+rrdtool graph $DIR/h_cuve_6m.png --start -25w  --title="Hauteur d'eau sur 6 mois" --vertical-label="cm" DEF:haut=$DIR/capa_cuve.rrd:haut:AVERAGE LINE1:haut$COUL_ECHELLE:"Hauteur d'eau en $ECHELLE\n" GPRINT:haut:AVERAGE:"Moy\: %.0lf" GPRINT:haut:MIN:"Mini\: %.0lf" GPRINT:haut:MAX:"Maxi\: %.0lf" 
+
+#Graphs par an
+rrdtool graph $DIR/h_cuve_1y.png --start -1y  --title="Hauteur d'eau sur 1 an" --vertical-label="cm" DEF:haut=$DIR/capa_cuve.rrd:haut:AVERAGE LINE1:haut$COUL_ECHELLE:"Hauteur d'eau en $ECHELLE\n" GPRINT:haut:AVERAGE:"Moy\: %.0lf" GPRINT:haut:MIN:"Mini\: %.0lf" GPRINT:haut:MAX:"Maxi\: %.0lf"
