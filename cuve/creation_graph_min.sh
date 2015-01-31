4980260f11ffd2ba822cfdc47de3411ea11bde98
thomas <szemrot@hotmail.com>
2015-01-31 21:23:08 +0100

    Modified creation_graph_min.sh

---
+++ cuve/creation_graph_min.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+DIR="/home/webide/repositories/my-pi-projects/cuve"
+
+JOUR=$(date '+%d-%m-%Y %H-%M-%S')
+
+# Affichage en degre C
+ECHELLE="cm"
+VOLUME="litres"
+
+#Choix des couleurs
+COUL_ECHELLE="#000099" COUL_VOLUME="#FF0000"
+
+#Graphs par 5 minutes
+rrdtool graph $DIR/h_cuve_300s.png --start -300s --title="Hauteur d'eau sur 300s" --vertical-label="cm" DEF:haut=$DIR/capa_cuve.rrd:haut:AVERAGE LINE1:haut$COUL_ECHELLE:"Hauteur d'eau en $ECHELLE\n" GPRINT:haut:LAST:"Mesure\: %.0lf" GPRINT:haut:AVERAGE:"Moy\: %.0lf" GPRINT:haut:MIN:"Mini\: %.0lf" GPRINT:haut:MAX:"Maxi\: %.0lf" COMMENT:" \n" COMMENT:"Dernier releve $JOUR"
