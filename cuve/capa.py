6901ae73c60cbd82d65de75e8beb6047fd502e53
thomas <szemrot@hotmail.com>
2015-01-31 15:11:59 +0100

    Modified capa.py

--- cuve/capa.py
+++ cuve/capa.py
@@ -181,9 +181,9 @@ while True:
 #    derivee = [abs((liste_acquisition[i+1] - liste_acquisition[i])) for i in range(len(liste_acquisition)-1)]
 #    if derivee[index]>PEAK:
 #            derivee[index]=median(derivee)
-#    index = index +1
- 
-    if index == NDERIVE:
+
+    index = index +1
+     if index == NDERIVE:
         index = 0
 
     lissage = median(liste_acquisition)
