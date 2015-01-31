ee4616ae6c60043393205e83b083e478b38c223e
thomas <szemrot@hotmail.com>
2015-01-31 21:48:17 +0100

    Modified creation_graph_min.sh

--- cuve/creation_graph_min.sh
+++ cuve/creation_graph_min.sh
@@ -12,3 +12,6 @@ COUL_ECHELLE="#000099" COUL_VOLUME="#FF0000"
 
 #Graphs par 5 minutes
 rrdtool graph $DIR/h_cuve_300s.png --start -300s --title="Hauteur d'eau sur 300s" --vertical-label="cm" DEF:haut=$DIR/capa_cuve.rrd:haut:AVERAGE LINE1:haut$COUL_ECHELLE:"Hauteur d'eau en $ECHELLE\n" GPRINT:haut:LAST:"Mesure\: %.0lf" GPRINT:haut:AVERAGE:"Moy\: %.0lf" GPRINT:haut:MIN:"Mini\: %.0lf" GPRINT:haut:MAX:"Maxi\: %.0lf" COMMENT:" \n" COMMENT:"Dernier releve $JOUR"
+
+#Graphs par demi heure
+rrdtool graph $DIR/h_cuve_30m.png --start -0.5h --title="Hauteur d'eau sur 1 heure" --vertical-label="cm" DEF:haut=$DIR/capa_cuve.rrd:haut:AVERAGE LINE1:haut$COUL_ECHELLE:"Hauteur d'eau en $ECHELLE\n" GPRINT:haut:LAST:"Mesure\: %.0lf" GPRINT:haut:AVERAGE:"Moy\: %.0lf" GPRINT:haut:MIN:"Mini\: %.0lf" GPRINT:haut:MAX:"Maxi\: %.0lf" COMMENT:" \n" COMMENT:"Dernier releve $JOUR"
