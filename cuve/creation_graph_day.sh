870f8577dff3227642849f59af0bdcd40c7ce9a9
thomas <szemrot@hotmail.com>
2015-01-31 21:49:04 +0100

    Modified creation_graph_day.sh

--- cuve/creation_graph_day.sh
+++ cuve/creation_graph_day.sh
@@ -10,8 +10,5 @@ VOLUME="litres"
 #Choix des couleurs
 COUL_ECHELLE="#000099" COUL_VOLUME="#FF0000"
 
-#Graphs par jour
-rrdtool graph $DIR/h_cuve_24h.png --start -1d --title="Hauteur d'eau sur 24 heures" --vertical-label="cm" DEF:haut=$DIR/capa_cuve.rrd:haut:AVERAGE LINE1:haut$COUL_ECHELLE:"Hauteur d'eau en $ECHELLE\n" GPRINT:haut:AVERAGE:"Moy\: %.0lf" GPRINT:haut:MIN:"Mini\: %.0lf" GPRINT:haut:MAX:"Maxi\: %.0lf"
-
 #Graphs par semaine
 rrdtool graph $DIR/h_cuve_1w.png --start -1w --title="Hauteur d'eau sur 1 semaine" --vertical-label="cm" DEF:haut=$DIR/capa_cuve.rrd:haut:AVERAGE LINE1:haut$COUL_ECHELLE:"Hauteur d'eau en $ECHELLE\n" GPRINT:haut:AVERAGE:"Moy\: %.0lf" GPRINT:haut:MIN:"Mini\: %.0lf" GPRINT:haut:MAX:"Maxi\: %.0lf" 
