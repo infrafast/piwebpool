02feff006c4530cb0727759bbf95d0924364a679
thomas <szemrot@hotmail.com>
2015-01-31 18:59:21 +0100

    Modified capa.py

--- cuve/capa.py
+++ cuve/capa.py
@@ -153,9 +153,8 @@ delay = FREQ
 while True:
     
     distance=LectureDistanceMoyenne(GPIO_TRIGGER,GPIO_ECHO,SAMPLES)
-    distance = FOND-distance
     # Mesure hauteur d'eau = difference entre cuve pleine et capteur 18cm
-    #distance = FOND - distance
+    distance = FOND - distance
     
     vol = LARGEUR * LONGUEUR * distance
     volume = vol / 1000
