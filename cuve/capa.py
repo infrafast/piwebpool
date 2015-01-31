dc0ae9ddff3cbeb2b6b9ceab3dfc51d49de99e4b
thomas <szemrot@hotmail.com>
2015-01-31 20:16:59 +0100

    Modified capa.py

--- cuve/capa.py
+++ cuve/capa.py
@@ -171,7 +171,7 @@ while True:
         index = 0
 
 
-    delay = math.floor(FREQ/2 + median(derivee)*0.2*FREQ)
+    delay = math.floor(FREQ/2 + median(derivee)*0.3*FREQ)
 
     lissage = median(liste_acquisition)
     logger.info(str(distance)+" "+str(lissage)+" "+str(delay))
