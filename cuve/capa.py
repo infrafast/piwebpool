efe49df18366fb4035fad5684b1ced7c749983ae
thomas <szemrot@hotmail.com>
2015-01-31 15:06:03 +0100

    Modified capa.py

--- cuve/capa.py
+++ cuve/capa.py
@@ -172,7 +172,7 @@ while True:
 
 
     liste_acquisition[index]=distance
-    derivee = [abs((liste_acquisition[i+1] - liste_acquisition[i])) for i in range(len(liste_acquisition)-1)]
+    derivee = [abs((liste_acquisition[i+1] - liste_acquisition[i])) for i in range(len(liste_acquisition))]
     if derivee[index]>PEAK:
             derivee[index]=median(derivee)
     index = index +1
