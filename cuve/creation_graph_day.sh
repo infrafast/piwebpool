211277c5e8796e9101fa4e19416c4f4982582aa1
thomas <szemrot@hotmail.com>
2015-01-31 21:18:27 +0100

    Modified creation_graph_day.sh

---
+++ cuve/creation_graph_day.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+DIR="/home/webide/repositories/my-pi-projects/cuve"
+
+JOUR=$(date '+%d-%m-%Y %H-%M-%S')
+
+# Affichage en degre C
+ECHELLE="cm"
+VOLUME="litres"
+
+#Choix des couleurs
+COUL_ECHELLE="#000099" COUL_VOLUME="#FF0000"
+
+#Graphs par 4 heure
+rrdtool graph $DIR/h_cuve_4h.png --start -4h --title="Hauteur d'eau sur 4 heures" --vertical-label="cm" DEF:haut=$DIR/capa_cuve.rrd:haut:AVERAGE LINE1:haut$COUL_ECHELLE:"Hauteur d'eau en $ECHELLE\n" GPRINT:haut:LAST:"Mesure\: %.0lf" GPRINT:haut:AVERAGE:"Moy\: %.0lf" GPRINT:haut:MIN:"Mini\: %.0lf" GPRINT:haut:MAX:"Maxi\: %.0lf" COMMENT:" \n" COMMENT:"Dernier releve $JOUR"
+
+#Graphs par jour
+rrdtool graph $DIR/h_cuve_24h.png --start -1d --title="Hauteur d'eau sur 24 heures" --vertical-label="cm" DEF:haut=$DIR/capa_cuve.rrd:haut:AVERAGE LINE1:haut$COUL_ECHELLE:"Hauteur d'eau en $ECHELLE\n" GPRINT:haut:AVERAGE:"Moy\: %.0lf" GPRINT:haut:MIN:"Mini\: %.0lf" GPRINT:haut:MAX:"Maxi\: %.0lf"
