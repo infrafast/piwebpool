e970f2ce1cc2114076c98e54db1d44bb5ac50297
thomas <szemrot@hotmail.com>
2015-02-01 23:23:09 +0100

    Modified monitor.py

--- cuve/monitor.py
+++ cuve/monitor.py
@@ -59,4 +59,6 @@ ratio24h = (round(niveau_maxi / niveau_mini,2)-1)*100
 
 message="Variation volume: actuel="+str(ratio30s)+"% sur 8h="+str(ratio8h)+"% sur 24h="+str(ratio24h)+"%"
 print (message)
+if ratio30s > 10:
+    dropsms(
 
