aeed363d117aebb3032176f39543aa93764c34c9
thomas <szemrot@hotmail.com>
2015-02-01 22:26:04 +0100

    Modified monitor.py

--- cuve/monitor.py
+++ cuve/monitor.py
@@ -38,9 +38,9 @@ niveau_maxi = 64
 database_file = "/home/webide/repositories/my-pi-projects/cuve/capa_cuve.rrd"
 #startTime = str(now - retention)
 #endTime = str(now)
-MAX_values = rrdtool.fetch(database_file, 'MAX','-s', 'start', '-e', 'now')
+MAX_values = rrdtool.fetch(database_file, 'MAX','-s', 'start+0', '-e', 'now')
 niveau_maxi = (max(MAX_values[2])[0])
-MIN_values = rrdtool.fetch(database_file, 'MIN','-s', 'start', '-e', 'now')
+MIN_values = rrdtool.fetch(database_file, 'MIN','-s', 'start+0', '-e', 'now')
 niveau_mini = (min(MIN_values)[0])[0]
 
 print("max:"+str(niveau_maxi)+" min:"+str(niveau_mini))
