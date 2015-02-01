a0bacebb8213c9583f3a1599c06b992b8a7c8e0e
thomas <szemrot@hotmail.com>
2015-02-01 21:35:29 +0100

    Modified monitor.py

--- cuve/monitor.py
+++ cuve/monitor.py
@@ -6,28 +6,32 @@ import urllib2
 import sys
 import rrdtool
 
-user = '19107501'
-texte = 'hello world'
-pas = 'oGVsksAr0geO6j'
-url = 'https://smsapi.free-mobile.fr/sendmsg?&user='+user+'&pass='+pas+'&msg='+texte
+def dropsms(texte):
 
-req = urllib2.Request(url)
-try:
-  reponse = urllib2.urlopen(req)
+    user = '19107501'
+    pas = 'oGVsksAr0geO6j'
+    url = 'https://smsapi.free-mobile.fr/sendmsg?&user='+user+'&pass='+pas+'&msg='+texte
+    
+    req = urllib2.Request(url)
+    try:
+      reponse = urllib2.urlopen(req)
+    
+    except IOError, e:
+      if hasattr(e,'code'):
+        if e.code == 400:
+            print 'Un des paramètres obligatoires est manquant.'
+        if e.code == 402:
+            print 'Trop de SMS ont été envoyés en trop peu de temps.'
+        if e.code == 403:
+            print 'Le service n’est pas activé sur l’espace abonné, ou login / clé incorrect.'
+        if e.code == 500:
+            print 'Erreur côté serveur. Veuillez réessayez ultérieurement.'
+    
+    print 'Le SMS a été envoyé sur votre mobile.'
 
-except IOError, e:
-  if hasattr(e,'code'):
-    if e.code == 400:
-        print 'Un des paramètres obligatoires est manquant.'
-    if e.code == 402:
-        print 'Trop de SMS ont été envoyés en trop peu de temps.'
-    if e.code == 403:
-        print 'Le service n’est pas activé sur l’espace abonné, ou login / clé incorrect.'
-    if e.code == 500:
-        print 'Erreur côté serveur. Veuillez réessayez ultérieurement.'
 
-print 'Le SMS a été envoyé sur votre mobile.'
 
-    #base RDTOOL
-    database_file = "/home/webide/repositories/my-pi-projects/cuve/capa_cuve.rrd"
-    rrdtool.update(database_file, "N:%.2f" % lissage+":%.2f" % volume)
+
+#base RDTOOL
+database_file = "/home/webide/repositories/my-pi-projects/cuve/capa_cuve.rrd"
+rrdtool.update(database_file, "N:%.2f" % lissage+":%.2f" % volume)
