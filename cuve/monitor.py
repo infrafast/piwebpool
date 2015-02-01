f00145fdd2cf46094c7bee05b48ce72ed09cb1ae
thomas <szemrot@hotmail.com>
2015-02-01 21:31:17 +0100

    Modified monitor.py

---
+++ cuve/monitor.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+# -*-coding:Utf-8 -*
+# José - Juin 2014
+
+import urllib2
+import sys
+
+user = '19107501'
+texte = 'helo world'
+pas = 'oGVsksAr0geO6j'
+url = 'https://smsapi.free-mobile.fr/sendmsg?&user='+user+'&pass='+pas+'&msg='+texte
+
+req = urllib2.Request(url)
+try:
+  reponse = urllib2.urlopen(req)
+
+except IOError, e:
+  if hasattr(e,'code'):
+    if e.code == 400:
+        print 'Un des paramètres obligatoires est manquant.'
+    if e.code == 402:
+        print 'Trop de SMS ont été envoyés en trop peu de temps.'
+    if e.code == 403:
+        print 'Le service n’est pas activé sur l’espace abonné, ou login / clé incorrect.'
+    if e.code == 500:
+        print 'Erreur côté serveur. Veuillez réessayez ultérieurement.'
+
+print 'Le SMS a été envoyé sur votre mobile.'
