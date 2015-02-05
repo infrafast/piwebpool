a60af1016c9de8d5fe418c7ae777a57f63c2b2ef
thomas <szemrot@hotmail.com>
2015-02-05 13:42:38 +0100

    Modified monitor.py

--- cuve/monitor.py
+++ cuve/monitor.py
@@ -47,7 +47,7 @@ parser.add_argument("-a", "--auto", help="monitor tank volume variation to (defa
  
 args = parser.parse_args()
 if args.log:
-    ALERT = args.log
+    ALERT = args.auto
     
 alert = 0
 
