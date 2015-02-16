1feecd90a9fdce6c1cb4346c56c8fe62021a3a8a
thomas <szemrot@hotmail.com>
2015-02-16 19:11:21 +0100

    Modified monitor.py

--- cuve/monitor.py
+++ cuve/monitor.py
@@ -93,7 +93,10 @@ ratio30s = (round(niveau_maxi / niveau_mini,2)-1)*100
 if ratio8h > 7 and ratio8h < 9 :
     ALERT = 1
     message = message + " !WARNING!"
-if 
+# le niveau reste haut pendant plus de une minute; la pompe ne s'est pas mise en route...
+if MAX_values > 66 :
+    ALERT = 1
+    message = message + " PUMP BLOCKED"
 
 
 message=message+" current="+str(ratio30s)+"% ./last 8h="+str(ratio8h)+"% last 24h="+str(ratio24h)+"%"
