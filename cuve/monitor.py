f64ff8bec5f94102899d5ac0387aa060f454f2c5
thomas <szemrot@hotmail.com>
2015-02-16 20:04:43 +0100

    Modified monitor.py

--- cuve/monitor.py
+++ cuve/monitor.py
@@ -117,7 +117,7 @@ if ratio8h > 7 and ratio8h < 9 :
 
 
 message=message+" current="+str(ratio30s)+"% ./last 8h="+str(ratio8h)+"% last 24h="+str(ratio24h)+"%"+" AVG: "+str(AVERAGE_value)
-print (message)
+#print (message)
 logger.info(str(ratio30s)+" "+str(ratio8h)+" "+str(ratio24h)+" "+str(AVERAGE_value))
 
 if ALERT or SMS:
