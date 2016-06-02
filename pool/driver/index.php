37b81e1add040c1837c41cd5894c1f21f67f83ae
thomas <szemrot@hotmail.com>
2016-06-02 23:00:31 +0200

    Modified index.php

--- pool/driver/index.php
+++ pool/driver/index.php
@@ -391,6 +391,9 @@ $table = new TableGear($options);
   </div>
 <?= $table->getJavascript("jquery") ?>
 <table class="materialTab">
+<tr>
+<td colspan="3"><div onclick="demo();">Actions</div></td>
+</tr>
 <tr><th>Commande</th><th>PIN</th><th>Etat</th></tr>
 <?php foreach($materials as $material=>$pin){ ?>
 <tr>
@@ -398,9 +401,6 @@ $table = new TableGear($options);
 	<td><?php echo $pin.' ('.$pins[$pin] .')'; $pinState = getPinState($pin,$pins); ?></td>
 	<td><div onclick="changeState(<?php echo $pin; ?>,this)" class="pinState <?php echo $pinState; ?>"></div></td></tr>
 <?php } ?>
-<tr>
-<td colspan="3"><div onclick="demo();">Actions</div></td>
-</tr>
 </table>
 <script src="js/jquery.min.js"></script>
 <script src="js/script.js"></script>
