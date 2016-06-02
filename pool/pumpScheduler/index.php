df71fc206fd48cb4ff9514741837ff3e8b356cae
thomas <szemrot@hotmail.com>
2016-06-02 21:41:53 +0200

    Modified index.php

--- pool/pumpScheduler/index.php
+++ pool/pumpScheduler/index.php
@@ -377,7 +377,7 @@ $table = new TableGear($options);
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-  <title>TableGear for Mootools</title>
+  <title>Pool scheduler</title>
   <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/mootools/1.2.4/mootools-yui-compressed.js"></script>
   <script type="text/javascript" src="javascripts/TableGear1.6-MooTools.js"></script>
   <link type="text/css" rel="stylesheet" href="stylesheets/tablegear.css" />
