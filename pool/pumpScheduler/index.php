374215d8c1b8aa6971d739aaf0e2fa0afa8f031a
thomas <szemrot@hotmail.com>
2016-06-02 22:25:09 +0200

    Modified index.php

--- pool/pumpScheduler/index.php
+++ pool/pumpScheduler/index.php
@@ -38,7 +38,7 @@ $options["database"]["table"]       = "pumpSchedule";
 // deleteRowLabel  = Text or an element that is used for the delete label.
 //                   This accepts a string or an HTML object (more info above).
 //
-// $options["allowDelete"] = false;
+$options["allowDelete"] = false;
 //
 // $options["deleteRowLabel"] = "×";  // A simple character.
 // $options["deleteRowLabel"] = array("tag" => "img", "attrib" => array("src" => "images/delete.gif")); // Default delete image.
@@ -85,7 +85,7 @@ $options["database"]["table"]       = "pumpSchedule";
 // One example of this is a CURRENT_TIMESTAMP default for a date field.
 // Note that this option cannot be used when you are using a custom query with fetchData
 
-// $options["database"]["fetchEmptyRow"] = true;
+//$options["database"]["fetchEmptyRow"] = true;
 
 
 
@@ -140,6 +140,25 @@ $options["database"]["table"]       = "pumpSchedule";
 // $options["selects"] = array("<FIELD>" => "increment[range=10]");          // If the current value is 18, will output <option> tags with values from 13 - 23.
 // $options["selects"] = array("<FIELD>" => "increment[range=5,step=5]");    // If the current value is 18, will output <option> tags with values 8,13,18,23,28.
 
+//$options["selects"] = array("below0" => array("on" => 1, "off" => 0)); 
+$options["selects"] = array(
+	"below0" => array("on" => 1, "off" => 0),
+	"0to2" => array("on" => 1, "off" => 0),
+	"2to4" => array("on" => 1, "off" => 0),
+	"4to6" => array("on" => 1, "off" => 0),
+	"6to8" => array("on" => 1, "off" => 0),
+	"8to10" => array("on" => 1, "off" => 0),
+	"10to12" => array("on" => 1, "off" => 0),
+	"12to14" => array("on" => 1, "off" => 0),
+	"14to16" => array("on" => 1, "off" => 0),
+	"16to18" => array("on" => 1, "off" => 0),
+	"18to20" => array("on" => 1, "off" => 0),
+	"20to22" => array("on" => 1, "off" => 0),
+	"22to24" => array("on" => 1, "off" => 0),
+	"24to26" => array("on" => 1, "off" => 0),
+	"26to28" => array("on" => 1, "off" => 0),
+	"above28" => array("on" => 1, "off" => 0)
+); 
 
 // Advanced usage of "selects" allows for some pretty cool features, like date formatting.
 // The example below would output 11 <option> elements that increment in steps of 86400, or 1 day in seconds.
@@ -287,7 +306,22 @@ $options["database"]["table"]       = "pumpSchedule";
 //                using "associate" (see below).
 
 
-// $options["transform"]["<FIELD1>"] = "The data: {DATA} occurred in field {FIELD} (or column {COLUMN}) in row {KEY}.";  // Illustrating how to use a transform.
+$options["transform"]["below0"] = array("tag" => "img","attrib" => array("src" => "{DATA}.png"));
+$options["transform"]["0to2"] = array("tag" => "img","attrib" => array("src" => "{DATA}.png"));
+$options["transform"]["2to4"] = array("tag" => "img","attrib" => array("src" => "{DATA}.png"));
+$options["transform"]["4to6"] = array("tag" => "img","attrib" => array("src" => "{DATA}.png"));
+$options["transform"]["6to8"] = array("tag" => "img","attrib" => array("src" => "{DATA}.png"));
+$options["transform"]["8to10"] = array("tag" => "img","attrib" => array("src" => "{DATA}.png"));
+$options["transform"]["10to12"] = array("tag" => "img","attrib" => array("src" => "{DATA}.png"));
+$options["transform"]["12to14"] = array("tag" => "img","attrib" => array("src" => "{DATA}.png"));
+$options["transform"]["14to16"] = array("tag" => "img","attrib" => array("src" => "{DATA}.png"));
+$options["transform"]["16to18"] = array("tag" => "img","attrib" => array("src" => "{DATA}.png"));
+$options["transform"]["18to20"] = array("tag" => "img","attrib" => array("src" => "{DATA}.png"));
+$options["transform"]["20to22"] = array("tag" => "img","attrib" => array("src" => "{DATA}.png"));
+$options["transform"]["22to24"] = array("tag" => "img","attrib" => array("src" => "{DATA}.png"));
+$options["transform"]["24to26"] = array("tag" => "img","attrib" => array("src" => "{DATA}.png"));
+$options["transform"]["26to28"] = array("tag" => "img","attrib" => array("src" => "{DATA}.png"));
+$options["transform"]["above28"] = array("tag" => "img","attrib" => array("src" => "{DATA}.png"));
 //
 // $options["transform"]["<FIELD1>"] = array("tag" => "img",
 //                                           "attrib" => array("src" => "/path/to/thumbnails/{DATA}")); // A simple url to image transform.
@@ -335,7 +369,6 @@ $table = new TableGear($options);
 // you MUST include "SQL_CALC_FOUND_ROWS" after the SELECT clause and not have any LIMIT or ORDER BY clauses!
 //
 // $table->fetchData("SELECT SQL_CALC_FOUND_ROWS <FIELD1>,<FIELD2> FROM <DATABASE_TABLE> WHERE <etc..>");
-
 
 
 ?>
