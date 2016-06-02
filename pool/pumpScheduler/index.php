cb489005e3edc36bc654dda1773a5a6b0d25abeb
thomas <szemrot@hotmail.com>
2016-06-02 22:39:29 +0200

    Modified index.php

--- pool/pumpScheduler/index.php
+++ pool/pumpScheduler/index.php
@@ -376,7 +376,7 @@ $table = new TableGear($options);
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-  <title>TableGear for jQuery</title>
+  <title>Pool scheduler</title>
   <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
   <script type="text/javascript" src="javascripts/TableGear1.6.1-jQuery.js"></script>
   <link type="text/css" rel="stylesheet" href="stylesheets/tablegear.css" />
