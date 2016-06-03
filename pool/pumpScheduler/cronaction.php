dd32a87b13dc195fde3ccaf5a85490e0f8de0e5b
thomas <szemrot@hotmail.com>
2016-06-03 12:56:26 +0200

    Modified cronaction.php

--- pool/pumpScheduler/cronaction.php
+++ pool/pumpScheduler/cronaction.php
@@ -1,3 +1,29 @@
+
 <?php
-    echo "ctrontab";
+
+if (!$link = mysql_connect('localhost', 'root', 'Quintal74605')) {
+    echo 'Could not connect to mysql';
+    exit;
+}
+
+if (!mysql_select_db('pool', $link)) {
+    echo 'Could not select database';
+    exit;
+}
+
+$sql    = 'SELECT foo FROM pumpSchedule';
+$result = mysql_query($sql, $link);
+
+if (!$result) {
+    echo "DB Error, could not query the database\n";
+    echo 'MySQL Error: ' . mysql_error();
+    exit;
+}
+
+while ($row = mysql_fetch_assoc($result)) {
+    echo $row; //[''];
+}
+
+mysql_free_result($result);
+
 ?>
