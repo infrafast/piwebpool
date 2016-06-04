ac36c716e52613d46b728c0c15f15b3e72ea0367
thomas <szemrot@hotmail.com>
2016-06-04 23:12:36 +0200

    Modified action.php

--- pool/pumpScheduler/action.php
+++ pool/pumpScheduler/action.php
@@ -1,28 +1,55 @@
 <?php
+// invoke with action.php?action=changeState&pin=11&state=1
+
 require_once('common.php');
 
-$result['state']  = 0;
-$result['error']  = '';
+$result['state']  = "undef";
+$result['answer']  = 'OK';
+
+$pinParam=$_['pin'];
+$stateParam=$_['state'];
 
 switch($_['action']){
 	case 'changeState':
-		setPinState($pins[$_['pin']],$_['state']);
-		$result['state'] = 1;
+	    if (intval($stateParam)<0 or intval(stateParam)>1 or intval($pinParam)<1 or intval($pinParam)>26){
+	        $result['answer']  = 'Bad parameter';     
+	    }else  $result['state'] = setPinState($pins[$pinParam],$stateParam);
 	break;
 
 	case 'demo':
-		system("gpio write ".$pins[$materials["Filtration"]]." 1");
-		sleep(1);
-		system("gpio write ".$pins[$materials["Filtration"]]." 0");
-		sleep(1);
-		system("gpio write ".$pins[$materials["Prise libre"]]." 1");
-		sleep(1);
-		system("gpio write ".$pins[$materials["Prise libre"]]." 0");
-		$result['state'] = 1;
+	    $result['state'] = (setPinState($pins[$materials["Filtration"]],0));
+	    sleep(1);
+        $result['state'] = (setPinState($pins[$materials["Filtration"]],1));
+	    sleep(1);
+        $result['state'] = (setPinState($pins[$materials["Prise libre"]],0));
+	    sleep(1);
+        $result['state'] = (setPinState($pins[$materials["Prise libre"]],1));
 	break;
 
+	case 'getState':
+	    if (intval($pinParam)<1 or intval($pinParam)>26){
+	        $result['answer']  = 'Bad parameter';     
+	    }else  $result['state'] = (getPinState($pinParam,$pins)=='off'?true:false);
+	break;
+
+    case 'resetSchedule':
+        // OPEN CONNECTION...
+        $connection = mysql_connect($options["database"]["host"], $options["database"]["username"], $options["database"]["password"]) or die(mysql_error());
+        mysql_select_db($options["database"]["name"], $connection) or die(mysql_error());
+        
+        $path = $_SERVER['DOCUMENT_ROOT'];
+        $sql_filename = 'pumpSchedule.sql';
+        $sql_contents = file_get_contents($path.$sql_filename);
+        $sql_contents = explode(";", $sql_contents);
+         
+        foreach($sql_contents as $query){
+            $result = mysql_query($query);
+            if (!$result) echo "Error on import of ".$query;
+        }        
+    break;
+
 	default:
-		$result['error']  = 'No action defined';
+		$result['answer']  = 'Undefined action '.$_['action'];
 	break;
 }
 
