50c1f1dbdbee890a11f15a93b05a4df540dde6d6
thomas <szemrot@hotmail.com>
2016-06-04 10:52:16 +0200

    Modified functions.php

--- pool/pumpScheduler/functions.php
+++ pool/pumpScheduler/functions.php
@@ -3,8 +3,7 @@
 function getPinState($pin,$pins){
 	$commands = array();
 	exec("gpio read ".$pins[$pin],$commands,$return);
-	//return (trim($commands[0])=="1"?'on':'off');
-	return (trim($commands[0])=="0"?'on':'off');
+	return (trim($commands[0])=="1"?'on':'off');
 }
 
 function setPinState($pin,$state){
