192d4a3babb1e18f4405c018ba6d24237c3ed7df
thomas <szemrot@hotmail.com>
2016-06-06 12:36:38 +0200

    Modified index.php

--- pool/pumpScheduler/index.php
+++ pool/pumpScheduler/index.php
@@ -385,7 +385,7 @@ $table = new TableGear($options);
 </table>
 <table class="materialTab">
 <tr>
-<td colspan="3"><div onclick="scenario();">Actions</div></td>
+<td colspan="3"><div onclick="scenario();">Actions<img src='images/enCours.png'></img></div></td>
 </tr>
 <tr><th>Commande</th><th>PIN</th><th>Etat</th></tr>
 <?php foreach($materials as $material=>$pin){ ?>
