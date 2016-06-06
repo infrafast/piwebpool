066c558b4b960da89ffb0f569c2925457b027b4e
thomas <szemrot@hotmail.com>
2016-06-06 14:33:28 +0200

    Modified action.php

--- pool/pumpScheduler/action.php
+++ pool/pumpScheduler/action.php
@@ -38,7 +38,7 @@ switch($_['action']){
         mysql_select_db($options["database"]["name"], $connection) or die(mysql_error());
         
         $sql_contents = file_get_contents('pumpSchedule.sql');
-        $sql_contents = explode(";\n
+        $sql_contents = explode(";\n");
         foreach($sql_contents as $query){
             $outcome = mysql_query($query);
             if (!$outcome) $result['answer']=$query."failed "." filename:".$sql_filename." query:".$query;
