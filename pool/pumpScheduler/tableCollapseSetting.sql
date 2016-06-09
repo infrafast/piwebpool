7b6412ac31259ee7a926365fd6b14a9284a641a1
thomas <szemrot@hotmail.com>
2016-06-09 12:27:11 +0200

    Modified tableCollapseSetting.sql

--- pool/pumpScheduler/tableCollapseSetting.sql
+++ pool/pumpScheduler/tableCollapseSetting.sql
@@ -1,3 +1,3 @@
-DROP TABLE IF EXISTS `tableCollapseSetting`;
+DROP TABLE IF EXISTS `setting`;
 CREATE TABLE `tableCollapseSetting` ( `id` VARCHAR(40) NOT NULL , `value` BOOLEAN NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;
 INSERT INTO `tableCollapseSetting` (`id`, `value`) VALUES ('actionTable', '0'), ('scheduleTable', '0')
