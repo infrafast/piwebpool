f3ab1bbaaa9683dab5af20d07c36e4483b5af7a1
thomas <szemrot@hotmail.com>
2016-06-09 15:49:43 +0200

    Modified index.php

--- pool/pumpScheduler/index.php
+++ pool/pumpScheduler/index.php
@@ -418,7 +418,7 @@ $table = new TableGear($options);
     });*/
     getSetting("actionTableCollapse", document.getElementById('actionTable'));
     getSetting("scheduleTableCollapse", document.getElementById('scheduleTable'));
-    getSetting("sensorTableCollapse", document.getElementById('scheduleTable'));
+    getSetting("sensorTableCollapse", document.getElementById('sensorTable'));
 
 </script>
 
