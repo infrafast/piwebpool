4c665363ff4849b2a8e464a578b8e621705cd830
thomas <szemrot@hotmail.com>
2016-06-09 12:54:39 +0200

    Modified action.php

--- pool/pumpScheduler/action.php
+++ pool/pumpScheduler/action.php
@@ -56,7 +56,15 @@ switch($_['action']){
     case 'updateSetting':
         mysql_connect($options["database"]["host"],$options["database"]["username"],$options["database"]["password"]) or die('error connection');
         mysql_select_db($options["database"]["name"]) or die('error database selection');
-        
+        $query="UPDATE `settings` SET `value` = '1' WHERE `settings`.`id` =".$_['id']."'";
+        if (!$outcome) {
+             $result['answer']  = "ERROR";
+             $result['state'] =  mysql_error();
+        }else{
+            $row = mysql_fetch_assoc($outcome);
+            $result['state'] = $row;
+            mysql_free_result($result);
+        }       
         
     break;
     
