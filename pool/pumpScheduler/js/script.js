4efafc4fe55a0e22a3ef0c8f56c228c80c65146b
thomas <szemrot@hotmail.com>
2016-06-09 14:54:46 +0200

    Modified script.js

--- pool/pumpScheduler/js/script.js
+++ pool/pumpScheduler/js/script.js
@@ -22,7 +22,7 @@ function getSetting(id,elem){
         url: urlCall,
         success: function(r){
             var result = eval(r);
-            $(elem)=result.state;
+            $(elem).concat(result.state);
             alert(result.state+" elem: "+$(elem));
     }});   
 }
