c4a7f460bc444bb39dd05b62d239cf517cac042c
thomas <szemrot@hotmail.com>
2016-06-09 13:08:39 +0200

    Modified settings.sql

--- pool/pumpScheduler/settings.sql
+++ pool/pumpScheduler/settings.sql
@@ -1,3 +1,3 @@
 DROP TABLE IF EXISTS `settings`;
 CREATE TABLE `settings` ( `id` VARCHAR(40) NOT NULL , `value` BOOLEAN NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;
-INSERT INTO `settings` (`id`, `value`) VALUES ('actionTableCollapse', '0'), ('scheduleTableCollapse', '0')
+INSERT INTO `settings` (`id`, `value`) VALUES ('actionTableCollapse', '0'), ('scheduleTableCollapse', '0');
