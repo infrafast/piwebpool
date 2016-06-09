69775ee0f716e746138233d40c932922b68c2b53
thomas <szemrot@hotmail.com>
2016-06-09 13:48:45 +0200

    Modified script.js

--- pool/pumpScheduler/js/script.js
+++ pool/pumpScheduler/js/script.js
@@ -2,7 +2,7 @@ $('.header').click(function(){
    var val = function(_, value){return value=="-"?"+":"-"};
    $(this).find('span').text(val);
     $(this).nextUntil('tr.header').slideToggle(); 
-    var urlCall="./action.php?action=updateSetting&id=actionTableCollapse&value=".concat("0");
+    var urlCall="./action.php?action=updateSetting&id=actionTableCollapse&value=".concat(val);
     $.ajax({
 	    type: "POST",
 		url: urlCall,
