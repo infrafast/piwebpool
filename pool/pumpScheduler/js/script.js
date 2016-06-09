46637c5871bd793388a59872ac952673b239ae01
thomas <szemrot@hotmail.com>
2016-06-09 13:29:10 +0200

    Modified script.js

--- pool/pumpScheduler/js/script.js
+++ pool/pumpScheduler/js/script.js
@@ -3,7 +3,7 @@ $('.header').click(function(){
     $(this).nextUntil('tr.header').slideToggle(); 
     $.ajax({
 	    type: "POST",
-		url: "./action.php?action=updateSetting&id="+$(this).id+"&value="+value=='-'?'0':'1',
+		url: "./action.php?action=updateSetting&id=actionTableCollapse&value=1",
 	    success: function(r){
     }});
 });
