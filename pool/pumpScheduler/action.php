5852510070789a236c19109823f3cf1009330f8e
thomas <szemrot@hotmail.com>
2016-06-09 12:44:02 +0200

    Modified action.php

--- pool/pumpScheduler/action.php
+++ pool/pumpScheduler/action.php
@@ -47,8 +47,8 @@ switch($_['action']){
         foreach($sql_query as $sql){ 
             $outcome=mysql_query($sql);
             if (!$outcome){
-                $result['answer'] = mysql_error();
-                $result['state'] = $sql;
+                $result['answer'] = "ERROR";
+                $result['state'] = mysql_error();
             }
         }
         
