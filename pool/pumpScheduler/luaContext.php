d19d7b8d040fe924bfcad009809b1d437627c2ac
thomas <szemrot@hotmail.com>
2016-06-13 11:54:01 +0200

    Modified luaContext.php

--- pool/pumpScheduler/luaContext.php
+++ pool/pumpScheduler/luaContext.php
@@ -1,7 +1,8 @@
 <?php
 /*
 LUA CONTEXT*/
-require_once ("common.php");
+require_once ("functions.php");
+require_once ("configuration.php");
 
 $lua->assign("from", $a); /** assign a PHP var to Lua named from */
 
