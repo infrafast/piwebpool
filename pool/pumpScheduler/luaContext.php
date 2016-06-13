8ed89341d6074f5b52e207fc5beedac0830abcd5
thomas <szemrot@hotmail.com>
2016-06-13 12:10:50 +0200

    Modified luaContext.php

--- pool/pumpScheduler/luaContext.php
+++ pool/pumpScheduler/luaContext.php
@@ -4,6 +4,31 @@ LUA CONTEXT*/
 require_once ("functions.php");
 require_once ("configuration.php");
 
+
+
+
+// base class with member properties and methods
+class LuaPool {
+
+   var $lua;
+
+   function get() 
+   {
+       return $this->$lua
+   }
+
+   function run() 
+   {
+       return $this->edible;
+   }
+
+} // end of class Vegetable
+
+
+
+
+
+
 function getLua($script)
     static $lua=new Lua($script);
     $lua->assign("filtration", $materials["Filtration"]); /** assign a PHP var to Lua named from */
