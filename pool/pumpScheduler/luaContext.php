2f9a1bbe8d65e6d023e6016894cbeddbbc351a67
thomas <szemrot@hotmail.com>
2016-06-13 15:55:22 +0200

    Modified luaContext.php

--- pool/pumpScheduler/luaContext.php
+++ pool/pumpScheduler/luaContext.php
@@ -17,7 +17,7 @@ function getLua($file,$materials,$pins){
     }
     
     $lua->registerCallback("set", 'setPinState');
-    $lua->registerCallback("get", 'getPinState');
+    $lua->registerCallback("get", 'getPin');
     return $lua;
 }
 
