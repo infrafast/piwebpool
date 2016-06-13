99d311587d844c1d1c5c12a8ca7a75343f056a30
thomas <szemrot@hotmail.com>
2016-06-13 14:27:57 +0200

    Modified luaContext.php

--- pool/pumpScheduler/luaContext.php
+++ pool/pumpScheduler/luaContext.php
@@ -4,7 +4,7 @@ LUA CONTEXT*/
 require_once ("functions.php");
 require_once ("configuration.php");
 
-function getLua($file,$materials){
+function getLua($file,$materials,$pins){
     // add error caught
     $script=$file;
     $lua=new Lua($file);
