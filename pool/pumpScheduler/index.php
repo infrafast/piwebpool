6b63c645672f937f0244526d6e11ee35319c79f5
thomas <szemrot@hotmail.com>
2016-06-13 17:18:39 +0200

    Modified index.php

--- pool/pumpScheduler/index.php
+++ pool/pumpScheduler/index.php
@@ -435,7 +435,16 @@ $table = new TableGear($options);
   var blocklyArea = document.getElementById('blocklyArea');
   var blocklyDiv = document.getElementById('blocklyDiv');
   var workspace = Blockly.inject(blocklyDiv,
-      {toolbox: document.getElementById('toolbox')});
+      {toolbox: document.getElementById('toolbox'),
+        zoom:
+                 {controls: true,
+                  wheel: true,
+                  startScale: 1.0,
+                  maxScale: 3,
+                  minScale: 0.3,
+                  scaleSpeed: 1.2},
+             trashcan: true          
+      });
   var onresize = function(e) {
     // Compute the absolute coordinates and dimensions of blocklyArea.
     var element = blocklyArea;
