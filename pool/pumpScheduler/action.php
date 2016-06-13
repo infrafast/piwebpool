432b9f077660e29aad2412b721b88f45f7964ed7
thomas <szemrot@hotmail.com>
2016-06-13 12:28:09 +0200

    Modified action.php

--- pool/pumpScheduler/action.php
+++ pool/pumpScheduler/action.php
@@ -4,7 +4,7 @@
 // action.php?action=updateSetting&id=actionTableCollapse&value=1
 // action.php?action=resetSchedule
 require_once('common.php');
-require_once('luaContext.php')
+require_once('luaContext.php');
 
 $result['state']  = "undef";
 $result['answer']  = 'OK';
