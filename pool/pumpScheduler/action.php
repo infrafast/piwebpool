ff3bde86e6149ee78e4f3ec0701905f9bd7ca03e
thomas <szemrot@hotmail.com>
2016-06-13 13:56:22 +0200

    Modified action.php

--- pool/pumpScheduler/action.php
+++ pool/pumpScheduler/action.php
@@ -13,7 +13,10 @@ switch($_['action']){
 	case 'changeState':
 	    if (intval($_['state'])<0 or intval($_['state'])>1 or intval($_['pin'])<1 or intval($_['pin'])>26){
 	        $result['answer']  = 'Bad parameter';     
-	    }else  $result['state'] = setPinState($pins[$_['pin']],$_['state']);
+	    }else{
+	        $result['state'] = setPinState($pins[$_['pin']],$_['state']);
+	        echo "setPinState(".$pins[$_['pin']].",".$_['state']".)";
+	    }
 	break;
 
 	case 'scenario':
