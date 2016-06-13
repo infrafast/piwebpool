fe14fb3fed781fefea465e3ece727d92201a93d0
thomas <szemrot@hotmail.com>
2016-06-13 11:16:42 +0200

    Modified cronaction.php

--- pool/pumpScheduler/cronaction.php
+++ pool/pumpScheduler/cronaction.php
@@ -56,7 +56,7 @@ if (!setPinState($pins[$materials["Filtration"]],$pumpConsign==0?1:0)){
     exit;   
 }
 
- $lua = new Lua("luascripts/test.lua");
+$lua = new Lua("luascripts/test.lua");
 
 echo "\n[".date("Y-m-d H:i:s")."][tw:".$tw."][temp:".$temp."][setPinState:".$pins[$materials["Filtration"]]." ".$pumpConsign."]";
 
