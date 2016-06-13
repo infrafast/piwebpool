88b1fa34ce8d423fe416d22c3dfbd0d49b523347
thomas <szemrot@hotmail.com>
2016-06-13 11:28:16 +0200

    Modified luaContext.php

---
+++ pool/pumpScheduler/luaContext.php
@@ -0,0 +1,10 @@
+<?php
+/*
+LUA CONTEXT*/
+session_start();
+$start=microtime(true);
+require_once('configuration.php');
+require_once('functions.php');
+error_reporting(E_ALL);
+//Calage de la date
+?>
