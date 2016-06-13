007b34f78fe421f36d02a2ae82bcf61ef9bc85fa
thomas <szemrot@hotmail.com>
2016-06-13 16:56:40 +0200

    Modified index.php

--- pool/pumpScheduler/index.php
+++ pool/pumpScheduler/index.php
@@ -361,6 +361,12 @@ $table = new TableGear($options);
   <title>Gestion piscine</title>
   <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
   <script type="text/javascript" src="js/TableGear1.6.1-jQuery.js"></script>
+  
+<script src="blockly/blockly_compressed.js"></script>
+<script src="blockly/blocks_compressed.js"></script>
+<script src="blockly/msg/js/en.js"></script>
+
+  
   <link type="text/css" rel="stylesheet" href="css/tablegear.css" />
   <link rel="stylesheet" href="css/style.css">
 </head>
