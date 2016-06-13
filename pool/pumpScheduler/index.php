915d52b842f1bdee77ca4cf609ad21d79bdd45d7
thomas <szemrot@hotmail.com>
2016-06-13 17:23:11 +0200

    Modified index.php

--- pool/pumpScheduler/index.php
+++ pool/pumpScheduler/index.php
@@ -432,37 +432,11 @@ $table = new TableGear($options);
 </table>
 
 <script>
-  var blocklyArea = document.getElementById('blocklyArea');
-  var blocklyDiv = document.getElementById('blocklyDiv');
-  var workspace = Blockly.inject(blocklyDiv,
+  var workspace = Blockly.inject('blocklyDiv',
       {toolbox: document.getElementById('toolbox'),
-        zoom:
-                 {controls: true,
-                  wheel: true,
-                  startScale: 1.0,
-                  maxScale: 3,
-                  minScale: 0.3,
-                  scaleSpeed: 1.2},
-             trashcan: true          
+          
+          
       });
-  var onresize = function(e) {
-    // Compute the absolute coordinates and dimensions of blocklyArea.
-    var element = blocklyArea;
-    var x = 0;
-    var y = 0;
-    do {
-      x += element.offsetLeft;
-      y += element.offsetTop;
-      element = element.offsetParent;
-    } while (element);
-    // Position blocklyDiv over blocklyArea.
-    blocklyDiv.style.left = x + 'px';
-    blocklyDiv.style.top = y + 'px';
-    blocklyDiv.style.width = blocklyArea.offsetWidth + 'px';
-    blocklyDiv.style.height = blocklyArea.offsetHeight + 'px';
-  };
-  window.addEventListener('resize', onresize, false);
-  onresize();
 </script>
 
 <script src="js/jquery.min.js"></script>
