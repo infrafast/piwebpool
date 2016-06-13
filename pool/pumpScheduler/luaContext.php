2ba8f4a41c18525ea6b980c115f0fdb31d9c6dae
thomas <szemrot@hotmail.com>
2016-06-13 12:25:44 +0200

    Modified luaContext.php

--- pool/pumpScheduler/luaContext.php
+++ pool/pumpScheduler/luaContext.php
@@ -4,7 +4,7 @@ LUA CONTEXT*/
 require_once ("functions.php");
 require_once ("configuration.php");
 
-function getLua($file){
+function getLua($file,$materials){
     $script=$file;
     $lua=new Lua($file);
     $lua->assign("filtration", $materials["Filtration"]); /** assign a PHP var to Lua named from */        
