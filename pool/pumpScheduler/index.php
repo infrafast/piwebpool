8b71b2a7d45463f9f2e09ff0f3a46929eebb26f7
thomas <szemrot@hotmail.com>
2016-06-13 16:28:36 +0200

    Modified index.php

--- pool/pumpScheduler/index.php
+++ pool/pumpScheduler/index.php
@@ -428,6 +428,7 @@ $table = new TableGear($options);
     getSetting("actionTableCollapse", document.getElementById('actionTable'));
     getSetting("scheduleTableCollapse", document.getElementById('scheduleTable'));
     getSetting("sensorTableCollapse", document.getElementById('sensorTable'));
+    getSetting("blocklyTableCollapse", document.getElementById('blocklyTable'));
 </script>
 
 <script src="blockly/blockly_compressed.js"></script>
