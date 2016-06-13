7dcfc1930a34d72c7ed7fdd78b12df1598f86194
thomas <szemrot@hotmail.com>
2016-06-13 16:58:14 +0200

    Modified index.php

--- pool/pumpScheduler/index.php
+++ pool/pumpScheduler/index.php
@@ -420,6 +420,15 @@ $table = new TableGear($options);
 <tr><th>Blockly</th></th></tr>
 <tr>
     <td><div id="blocklyDiv" style="height: 480px; width: 600px;"></div>This is where blockly goes</td>
+    <xml id="toolbox" style="display: none">
+      <block type="controls_if"></block>
+      <block type="controls_repeat_ext"></block>
+      <block type="logic_compare"></block>
+      <block type="math_number"></block>
+      <block type="math_arithmetic"></block>
+      <block type="text"></block>
+      <block type="text_print"></block>
+    </xml>    
 </tr>
 </table>
 
