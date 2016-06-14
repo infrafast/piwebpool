e75cf50a612708c03230c32e0e7b48956ff0c7aa
thomas <szemrot@hotmail.com>
2016-06-14 10:31:41 +0200

    Modified luaContext.php

--- pool/pumpScheduler/luaContext.php
+++ pool/pumpScheduler/luaContext.php
@@ -5,28 +5,25 @@ require_once ("functions.php");
 require_once ("configuration.php");
 
 function goLua($file,$materials,$pins){
-
-    // add error caught
-    //} catch (LuaException $e) {
-    //     echo $e->getMessage();
-    //}
-
-    $lua=new Lua($file);
+    try{
+        $lua=new Lua($file);
+        
+        // record names of the command as variable and manipulate them directly phyisically in lua using set and get functions
+        // since the setPinState function gets the logical pin number, we have to get it from the table
     
-    // record names of the command as variable and manipulate them directly phyisically in lua using set and get functions
-    // since the setPinState function gets the logical pin number, we have to get it from the table
-
-    foreach($materials as $material=>$pin){
-        $luaVariables[$pin]=getPin($pins[$pin]);
-        $lua->assign($material, $pins[$pin]);     // option 1;
-        //echo "{material: ".$material." pin:".$pins[$pin]."}";
+        foreach($materials as $material=>$pin){
+            $luaVariables[$pin]=getPin($pins[$pin]);
+            $lua->assign($material, $pins[$pin]);     // option 1;
+            //echo "{material: ".$material." pin:".$pins[$pin]."}";
+        }
+        $lua->registerCallback("set", 'setPinState'); // option 1
+        $lua->registerCallback("get", 'getPin'); // option 1
+        
+        // execute the script
+        $lua->run();
+    } catch (LuaException $e) {
+         echo $e->getMessage();
     }
-    $lua->registerCallback("set", 'setPinState'); // option 1
-    $lua->registerCallback("get", 'getPin'); // option 1
-    
-    // execute the script
-    $lua->run();
-
     return $lua;
 }
 
