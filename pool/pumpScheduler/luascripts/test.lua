f74d2236c2916db1f54aeb512a550c2df92edc09
thomas <szemrot@hotmail.com>
2016-06-14 22:56:48 +0200

    Modified test.lua

--- pool/pumpScheduler/luascripts/test.lua
+++ pool/pumpScheduler/luascripts/test.lua
@@ -1,17 +1,5 @@
 function run()
-    filter=get(filtration);
-    traite=get(traitement);
-
-    if (filter == 0) then
-        set(filtration,1);
-    else
-        set(filtration,0);
-    end
-
-    if (traite == 0) then
-        set(traitement,1);
-    else
-        set(traitement,0);
-    end
+set(filtration,(not (get(filtration))));
+set(traitement,(not (get(traitement))));
     return "OK LUA";
 end
