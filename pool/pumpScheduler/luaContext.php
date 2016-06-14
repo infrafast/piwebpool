19aaf5929cbb93ab9129fa48043c52da5bf69676
thomas <szemrot@hotmail.com>
2016-06-14 10:16:06 +0200

    Modified luaContext.php

--- pool/pumpScheduler/luaContext.php
+++ pool/pumpScheduler/luaContext.php
@@ -32,7 +32,7 @@ function goLua($file,$materials,$pins){
     $lua->run();
     // option 2 
     // get back the variable modified by script to update command
-    foreach ($luaVariables as $luaVariable){
+    foreach ($luaVariables as $luaVariable=>$value){
         echo "{FROM PHP ".$luaVariable."=".$luaVariables[$luaVariable]." pin:".$pins[$materials[$luaVariable]]."}";
         //setPinState($pins[$pin],$value);      
     }
