3154f93786755ef9e4b0274b2feb92e43236160b
thomas <szemrot@hotmail.com>
2016-06-14 10:25:28 +0200

    Modified luaContext.php

--- pool/pumpScheduler/luaContext.php
+++ pool/pumpScheduler/luaContext.php
@@ -11,40 +11,23 @@ function goLua($file,$materials,$pins){
     //     echo $e->getMessage();
     //}
 
-    
-    
-    $script=$file;
+        $script=$file;
     $lua=new Lua($file);
-    
     
     // record names of the command as variable and manipulate them directly phyisically in lua using set and get functions
     // since the setPinState function gets the logical pin number, we have to get it from the table
-    // option 2 : PREFERRED
-    // register variable and manipulate them logically in lua, then resync physically here
 
     foreach($materials as $material=>$pin){
         $luaVariables[$pin]=getPin($pins[$pin]);
-        //$lua->assign($material, $pins[$pin]);     // option 1;
-        $lua->assign($material,$luaVariables[$pin]);      // option 2
+        $lua->assign($material, $pins[$pin]);     // option 1;
         //echo "{material: ".$material." pin:".$pins[$pin]."}";
     }
-    $toto ="PHP";
-    $lua->assign("toto",$toto);
-    //$lua->registerCallback("set", 'setPinState'); // option 1
-    //$lua->registerCallback("get", 'getPin'); // option 1
+    $lua->registerCallback("set", 'setPinState'); // option 1
+    $lua->registerCallback("get", 'getPin'); // option 1
     
-
     // execute the script
     $lua->run();
-    echo "TOTO AFTER LUA=".$toto;
-    // option 2 
-    // get back the variable modified by script to update command
-    foreach ($luaVariables as $luaVariable=>$value){
-        echo "{FROM PHP ".$luaVariable."=".$luaVariables[$luaVariable]." pin:".$pins[$materials[$luaVariable]]."}";
-        //setPinState($pins[$pin],$value);      
-    }
-    
-    
+
     return $lua;
 }
 
