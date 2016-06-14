09c2b0acceba9df09cfb79143fe2224face6eaed
thomas <szemrot@hotmail.com>
2016-06-14 10:48:24 +0200

    Modified luaContext.php

--- pool/pumpScheduler/luaContext.php
+++ pool/pumpScheduler/luaContext.php
@@ -7,11 +7,6 @@ require_once ("configuration.php");
 function goLua($file,$materials,$pins,&$feedback){
     try{
         $lua=new Lua($file);
-    } catch (LuaException $e) {
-         $feedback= $e->getMessage();
-         return false;       
-    }
-    try{
         // record names of the command as variable and manipulate them directly phyisically in lua using set and get functions
         // since the setPinState function gets the logical pin number, we have to get it from the table
     
