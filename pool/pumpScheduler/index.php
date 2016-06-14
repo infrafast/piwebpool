e966c6aa37e61d6a96b28587d7f63bc31e465219
thomas <szemrot@hotmail.com>
2016-06-14 14:42:34 +0200

    Modified index.php

--- pool/pumpScheduler/index.php
+++ pool/pumpScheduler/index.php
@@ -425,27 +425,15 @@ $table = new TableGear($options);
 <tr>
     <td><div id="blocklyDiv" style="height: 300px; width: 100%;"></div></td>
 <xml id="toolbox" style="display: none">
-  <category name="Core">
-    <category name="Control">
-      <block type="controls_if"></block>
-      <block type="controls_whileUntil"></block>
-    </category>
-    <category name="Logic">
-      <block type="logic_compare"></block>
-      <block type="logic_operation"></block>
-      <block type="logic_boolean"></block>
-    </category>
+  <category name="Control">
+    <block type="controls_if"></block>
+    <block type="controls_whileUntil"></block>
+    <block type="controls_for">
   </category>
-  <category name="Custom">
-    <block type="start"></block>
-    <category name="Move">
-      <block type="move_forward"></block>
-      <block type="move_backward"></block>
-    </category>
-    <category name="Turn">
-      <block type="turn_left"></block>
-      <block type="turn_right"></block>
-    </category>
+  <category name="Logic">
+    <block type="logic_compare"></block>
+    <block type="logic_operation"></block>
+    <block type="logic_boolean"></block>
   </category>
 </xml>
     
