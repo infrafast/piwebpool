44d6087c04622fc1a3cc9b5b44f6cfee20392c9c
thomas <szemrot@hotmail.com>
2016-06-14 09:36:30 +0200

    Modified luaContext.php

--- pool/pumpScheduler/luaContext.php
+++ pool/pumpScheduler/luaContext.php
@@ -13,16 +13,16 @@ function goLua($file,$materials,$pins){
     // option 1
     // record names of the command as variable and manipulate them directly phyisically in lua using set and get functions
     // since the setPinState function gets the logical pin number, we have to get it from the table
+    // option 2
+    // register variable and manipulate them logically in lua, then resync physically here
+
     foreach($materials as $material=>$pin){
-        $lua->assign($material, $pins[$pin]);    
+        $lua->assign($material, $pins[$pin]);     // option 1;
         //echo "{material: ".$material." pin:".$pins[$pin]."}";
     }
     $lua->registerCallback("set", 'setPinState');
     $lua->registerCallback("get", 'getPin');
     
-
-    // option 2
-    // register variable and manipulate them logically in lua, then resync physically here
 
     // execute the script
     $lua->run();
