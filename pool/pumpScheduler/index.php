5aee2cabd4cc2a652acb7755ea7626634f941a6c
thomas <szemrot@hotmail.com>
2016-06-14 11:14:23 +0200

    Modified index.php

--- pool/pumpScheduler/index.php
+++ pool/pumpScheduler/index.php
@@ -405,7 +405,7 @@ $table = new TableGear($options);
 <tr><th>Sonde</th><th>Valeur</th></tr>
 <tr>
     <td>PH</td>
-    <td><div onclick="" class=""></div>7.2</td>
+    <td><div onclick="" class=""></div><?php echo getTemperature(); ?></td>
 </tr>
 <tr>
     <td>Redox</td>
