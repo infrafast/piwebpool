5e7579c02242ba19e82a55809cdb24f1782eaffc
thomas <szemrot@hotmail.com>
2016-06-14 10:39:59 +0200

    Modified action.php

--- pool/pumpScheduler/action.php
+++ pool/pumpScheduler/action.php
@@ -97,7 +97,7 @@ switch($_['action']){
         break;
 
     case 'lua':
-       if (!goLua("luascripts/".$_['file'],$materials,$pins,$result['status'])) $result['answer']  = "ERROR";
+       if (!goLua("luascripts/".$_['file'],$materials,$pins,$result['state'])) $result['answer']  = "ERROR";
         break;
 
 	default:
