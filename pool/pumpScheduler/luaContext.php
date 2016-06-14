27195072287fea3cef5be278099069ba91f45742
thomas <szemrot@hotmail.com>
2016-06-14 10:46:35 +0200

    Modified luaContext.php

--- pool/pumpScheduler/luaContext.php
+++ pool/pumpScheduler/luaContext.php
@@ -28,7 +28,7 @@ function goLua($file,$materials,$pins,&$feedback){
     }
     try{
         // execute the script
-        $feedback= $lua->run();
+        $feedback= $lua->runi();
     } catch (LuaException $e) {
          $feedback= $e->getMessage();
          return false;
