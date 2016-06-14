53c3ccb4439945920484e8bc189908cabff8c5f3
thomas <szemrot@hotmail.com>
2016-06-14 10:26:20 +0200

    Modified test.lua

--- pool/pumpScheduler/luascripts/test.lua
+++ pool/pumpScheduler/luascripts/test.lua
@@ -1,19 +1,12 @@
 function run()
-    -- value=0;
+    value=0;
     print("\n{FROM LUA");
-    --print ("setFiltration=" .. value);
-    --set(filtration,value);
-    -- set(traitement,1);
-    -- filtre = get(filtration);
-    print ("toto=" .. toto);
+    print ("setFiltration=" .. value);
+    set(filtration,value);
+    set(traitement,1);
+    filtre = get(filtration);
     print ("filtration=" .. filtration);
     print ("traitement=" .. traitement);
-    if filtration == 0 then
-            filtration = 1
-    elseif filtration == 1 then
-            filtration = 0
-    end
-    toto="LUA";
     print ("END LUA}\n");
 
     return;
