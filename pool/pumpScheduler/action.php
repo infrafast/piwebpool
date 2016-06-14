a82a7fc33bf38f62cbda57ddb099b89ecc0bcdb4
thomas <szemrot@hotmail.com>
2016-06-14 14:04:45 +0200

    Modified action.php

--- pool/pumpScheduler/action.php
+++ pool/pumpScheduler/action.php
@@ -13,7 +13,7 @@ $result['answer']  = 'OK';
 switch($_['action']){
     
     case 'sms':
-            if (!sms($number,$_['message'],$result['state'])){
+            if (!sms($_['number'],$_['message'],$result['state'])){
                 $result['answer']="ERROR";
             }
         break;
