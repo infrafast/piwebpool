5b1034c24012e606842f60e1a9d25338f2fd94a6
thomas <szemrot@hotmail.com>
2016-06-15 17:54:34 +0200

    Modified test.lua

--- pool/pumpScheduler/luascripts/test.lua
+++ pool/pumpScheduler/luascripts/test.lua
@@ -1,6 +1,12 @@
 function run()
-    if not email('hello world from lua') then retour="email fail"; end
-    if not log("ceci est un log")  then retour=retour .. "log fail"; end
-    if not sms("sent sms")  then retour=retour .. "sms fail"; end
+    if not email('hello world from lua') then 
+        retour="email fail"; 
+    end
+    if not log("ceci est un log")  then 
+        retour=retour .. "log fail"; 
+    end
+    if not sms("sent sms")  then 
+        retour=retour .. "sms fail"; 
+    end
     return retour;
 end
