1d7c1a5b2d323fbae352a9ec8009086b57be3d5b
thomas <szemrot@hotmail.com>
2016-06-15 23:15:13 +0200

    Modified index.php

--- pool/pumpScheduler/index.php
+++ pool/pumpScheduler/index.php
@@ -435,6 +435,7 @@ $table = new TableGear($options);
         <block type="variables_set"><field name="VAR">variable</field></block>
         <block type="variables_get"><field name="VAR">variable</field></block>
         <block type="on_off"></block>
+        <block type="message"></block>
         <block type="math_number"></block>
         <block type="math_arithmetic"></block>
         <block type="setcommand"></block>
