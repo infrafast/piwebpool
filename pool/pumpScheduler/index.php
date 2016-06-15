c42b50143cd5720e719f644d4d91b2606ad7fb7b
thomas <szemrot@hotmail.com>
2016-06-15 14:36:02 +0200

    Modified index.php

--- pool/pumpScheduler/index.php
+++ pool/pumpScheduler/index.php
@@ -366,7 +366,7 @@ $table = new TableGear($options);
 <script src="blockly/lua_compressed.js"></script>
 <script src="blockly/blocks_compressed.js"></script>
 
-<script src="blockly/msg/js/en.js"></script>
+<script src="blockly/msg/js/fr.js"></script>
   
   <link type="text/css" rel="stylesheet" href="css/tablegear.css" />
   <link rel="stylesheet" href="css/style.css">
