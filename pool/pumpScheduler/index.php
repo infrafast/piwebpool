22f4aa646ac84a8cd48391a485b1350d4a451936
thomas <szemrot@hotmail.com>
2016-06-15 15:32:40 +0200

    Modified index.php

--- pool/pumpScheduler/index.php
+++ pool/pumpScheduler/index.php
@@ -535,7 +535,7 @@ $table = new TableGear($options);
     Blockly.Blocks['write_log'] = {
         init: function() {
             this.appendValueInput("NAME")
-            .setCheck(null);
+            .setCheck("String")
             .appendField("Log");
             this.setPreviousStatement(true, null);
             this.setNextStatement(true, null);
