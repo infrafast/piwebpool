29224620ce9458776e6a141d361915820b04cfb7
thomas <szemrot@hotmail.com>
2016-06-15 18:09:18 +0200

    Modified cronaction.php

--- pool/pumpScheduler/cronaction.php
+++ pool/pumpScheduler/cronaction.php
@@ -67,5 +67,5 @@ $lua = goLua("luascripts/test.lua",$materials,$pins,$luaFeedback);
 
 $state = "{tw:".$tw."}{temp:".$temp."}{setPinState:".$pins[$materials["filtration"]]." ".$pumpConsign."}{Lua:".$luaFeedback."}";
 appendlog("CRONACTION",$answer,$state);
-
+echo $answer.$state;
 ?>
