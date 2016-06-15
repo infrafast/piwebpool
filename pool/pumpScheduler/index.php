923b5f30954222df9b047f7f2f415ee702f2e24f
thomas <szemrot@hotmail.com>
2016-06-15 14:35:36 +0200

    Modified index.php

--- pool/pumpScheduler/index.php
+++ pool/pumpScheduler/index.php
@@ -533,6 +533,13 @@ $table = new TableGear($options);
     this.setColour(330);
   }
 };
+
+Blockly.JavaScript['write_log'] = function(block) {
+  var value_name = Blockly.JavaScript.valueToCode(block, 'NAME', Blockly.JavaScript.ORDER_ATOMIC);
+  // TODO: Assemble JavaScript into code variable.
+  var code = '...;\n';
+  return code;
+};
     
     
     workspace.addChangeListener(myUpdateFunction);
