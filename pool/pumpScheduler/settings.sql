3f41aab39ad797e6bd08bd142633d58b81356dac
thomas <szemrot@hotmail.com>
2016-06-15 21:51:06 +0200

    Modified settings.sql

--- pool/pumpScheduler/settings.sql
+++ pool/pumpScheduler/settings.sql
@@ -1,3 +1,16 @@
 DROP TABLE IF EXISTS `settings`;
-CREATE TABLE `settings` ( `id` VARCHAR(40) NOT NULL , `value` BOOLEAN NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;
-INSERT INTO `settings` (`id`, `value`) VALUES ('actionTableCollapse', '0'), ('scheduleTableCollapse', '0'), ('sensorTableCollapse', '0'), ('blocklyTableCollapse', '0');
+CREATE TABLE IF NOT EXISTS `settings` (
+  `id` varchar(40) NOT NULL,
+  `value` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+INSERT INTO `settings` (`id`, `value`) VALUES
+('actionTableCollapse', 1),
+('blocklyTableCollapse', 0),
+('scheduler', 1),
+('scheduleTableCollapse', 1),
+('sensorTableCollapse', 1);
+
+
+ALTER TABLE `settings`
+ ADD PRIMARY KEY (`id`);
