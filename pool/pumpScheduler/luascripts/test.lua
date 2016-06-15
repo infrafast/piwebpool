41eb5297dc9db959e369a565ac6f090d9c2961ce
thomas <szemrot@hotmail.com>
2016-06-15 18:25:44 +0200

    Modified test.lua

--- pool/pumpScheduler/luascripts/test.lua
+++ pool/pumpScheduler/luascripts/test.lua
@@ -1,13 +1,18 @@
 function run()
     retour="";
-    if not email('hello world from lua') then 
-        retour=" email fail"; 
-    end
-    if not log("ceci est un log") then 
-        retour=retour .. " log fail"; 
-    end
-    if not sms("sent sms") then 
-        retour=retour .. " sms fail"; 
-    end
+
+filter = get(filtration);
+
+if filter == 0 and (temperature) < 28 then
+  log('La pompe est à l\'arret et la temperature sous 28°');
+end
+if (temperature) < 4 then
+  set(filtration,(1));
+  log('Risque de gel, temperature =');
+  log((temperature));
+  log('Mise en route de la pompe');
+end
+
+
     return retour;
 end
