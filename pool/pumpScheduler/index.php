15392f96e623a0d51a5616019c8318fe95696eae
thomas <szemrot@hotmail.com>
2016-06-15 14:39:59 +0200

    Modified index.php

--- pool/pumpScheduler/index.php
+++ pool/pumpScheduler/index.php
@@ -511,23 +511,7 @@ $table = new TableGear($options);
       return [code, Blockly.Lua.ORDER_NONE];
     };
     
-    Blockly.Blocks['write_log'] = {
-        init: function() {
-            this.appendValueInput("NAME")
-            .setCheck("String")
-            .appendField("Log");
-            this.setPreviousStatement(true, null);
-            this.setNextStatement(true, null);
-            this.setColour(330);
-        }
-    };
-    
-    Blockly.JavaScript['write_log'] = function(block) {
-        var value_name = Blockly.JavaScript.valueToCode(block, 'NAME', Blockly.JavaScript.ORDER_ATOMIC);
-        // TODO: Assemble JavaScript into code variable.
-        var code = '...;\n';
-        return code;
-    };
+
     
 
   var workspace = Blockly.inject('blocklyDiv',
