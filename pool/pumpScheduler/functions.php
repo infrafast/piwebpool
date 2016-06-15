e28c52c349bf44f6c5eae18eb3b5d2fdc680ae0c
thomas <szemrot@hotmail.com>
2016-06-15 17:48:55 +0200

    Modified functions.php

--- pool/pumpScheduler/functions.php
+++ pool/pumpScheduler/functions.php
@@ -7,8 +7,9 @@ require "include/FreeSMS.php";
 function sendsms($message){
     $SMSuser = "19107501";
     $SMSkey = "oGVsksAr0geO6j";
+    $feedback="void";            
             
-    if (!sms( $SMSuser, $SMSkey, $message)){
+    if (!sms( $SMSuser, $SMSkey, $message,$feedback )){
         return false;
     }
     return true;
