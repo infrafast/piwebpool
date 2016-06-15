902144f3fbe515449c983837d65ce735d97907cd
thomas <szemrot@hotmail.com>
2016-06-15 23:10:04 +0200

    Modified index.php

--- pool/pumpScheduler/index.php
+++ pool/pumpScheduler/index.php
@@ -475,7 +475,7 @@ $table = new TableGear($options);
     Blockly.Blocks['setcommand'] = {
       init: function() {
         this.appendValueInput("NAME")
-            .setCheck("Boolean")
+            .setCheck("Number")
             .appendField(new Blockly.FieldDropdown([["filtration", "filtration"], ["traitement", "traitement"]]), "command");
         this.setPreviousStatement(true, null);
         this.setNextStatement(true, null);
