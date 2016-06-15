6a5095a792a6f73b0529f1b4357193fb93536da5
thomas <szemrot@hotmail.com>
2016-06-15 17:54:43 +0200

    Modified test.lua

--- pool/pumpScheduler/luascripts/test.lua
+++ pool/pumpScheduler/luascripts/test.lua
@@ -2,10 +2,10 @@ function run()
     if not email('hello world from lua') then 
         retour="email fail"; 
     end
-    if not log("ceci est un log")  then 
+    if not log("ceci est un log") then 
         retour=retour .. "log fail"; 
     end
-    if not sms("sent sms")  then 
+    if not sms("sent sms") then 
         retour=retour .. "sms fail"; 
     end
     return retour;
