cbfa779059d038739e726a2d7815c8c1637896ba
thomas <szemrot@hotmail.com>
2016-06-16 10:59:26 +0200

    Modified index.php

--- pool/pumpScheduler/index.php
+++ pool/pumpScheduler/index.php
@@ -458,12 +458,7 @@ $table = new TableGear($options);
 </tr>
 <tr><th>Valeur</th></tr>
 <tr>
-
- <label class="the-fix">
-  A good–looking textarea with a 100% width
-  <textarea></textarea>
- </label>
-
+    <textarea rows="14" cols="35" id="scriptarea" readonly style="color: grey; background-color: lightgrey "></textarea></div>    
 </tr>
 </table>
 
