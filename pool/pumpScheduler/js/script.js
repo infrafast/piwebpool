bf7b70ea1a8db4216e0c32231bf9776e44ff5fa9
thomas <szemrot@hotmail.com>
2016-06-16 10:05:15 +0200

    Modified script.js

--- pool/pumpScheduler/js/script.js
+++ pool/pumpScheduler/js/script.js
@@ -63,13 +63,13 @@ function updateScript(xml,lua){
 
 // the getXML has to be sync (async=flase) otehrwise we can't fetch the info frpm the database and return "undefined" variable value
 function getXMLScript(){
-    var xml_text;
+    var result;
 	$.ajax({
 		type: "POST",
 		url: "./action.php?action=getScript&id=main",
 		async:false,
 		success: function(r){
-			var result = eval(r);
+			result = eval(r);
 			if(result.answer != "OK"){          
 				alert('Erreur : '+result.state);
 			}else xml_text = result.state;
