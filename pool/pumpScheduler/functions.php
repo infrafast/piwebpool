cfc719301720d3f4bd169a3fd7643e614b3cf261
thomas <szemrot@hotmail.com>
2016-06-16 11:33:59 +0200

    Modified functions.php

--- pool/pumpScheduler/functions.php
+++ pool/pumpScheduler/functions.php
@@ -29,7 +29,7 @@ function appendlog($source,$answer,$status){
 }
 
 function getLog(){
-    $data = '';
+    $data = 'could not open logfile';
     $filename="logfile.txt";
     $fp = fopen($filename, "r");
     $block = 4096;
