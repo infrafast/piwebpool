9f8a073210c1f1969a113c9f9c050733fb05ccfe
thomas <szemrot@hotmail.com>
2016-06-16 09:33:59 +0200

    Modified script.js

--- pool/pumpScheduler/js/script.js
+++ pool/pumpScheduler/js/script.js
@@ -67,7 +67,7 @@ function getXMLScript(){
 			url: "./action.php?action=getScript&id=main",
 			success: function(r){
 				var result = eval(r);
-				if(result.answer != "OK"){          
+				if(result.answer != "POK"){          
 					alert('Erreur : '+result.state);
 				}else return result.state;
 	}});
