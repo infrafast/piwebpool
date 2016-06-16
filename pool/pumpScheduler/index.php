797a2f6729b2ab8503040b23fe8b7238fc49a811
thomas <szemrot@hotmail.com>
2016-06-16 11:12:29 +0200

    Modified index.php

--- pool/pumpScheduler/index.php
+++ pool/pumpScheduler/index.php
@@ -469,7 +469,7 @@ $table = new TableGear($options);
 
 <script>
     // retrieve logfile
-    document.getElementById('logFile').value = "COUCOU"; //getLog();
+    document.getElementById('logFile').value = getLog();
 
     Blockly.Blocks['sensors'] = {
       init: function() {
