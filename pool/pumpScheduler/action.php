68cae3166795d20b9b813a89a18cc1c66a890bc7
thomas <szemrot@hotmail.com>
2016-06-16 09:14:04 +0200

    Modified action.php

--- pool/pumpScheduler/action.php
+++ pool/pumpScheduler/action.php
@@ -105,7 +105,7 @@ switch($_['action']){
         $outcome = mysql_query($query);
         if (!$outcome) {
              $result['answer']  = "ERROR";
-             $result['state'] =  $query; /* mysql_error();*/
+             $result['state'] =  mysql_error(); /* mysql_error();*/
         }else{
             $result['state'] = $outcome;
         }       
@@ -120,7 +120,7 @@ switch($_['action']){
         $outcome = mysql_query($query);
         if (!$outcome) {
              $result['answer']  = "ERROR";
-             $result['state'] =  $query; /* mysql_error();*/
+             $result['state'] = mysql_error(); /* mysql_error();*/
         }else{
             $result['state'] = $outcome;
         }       
