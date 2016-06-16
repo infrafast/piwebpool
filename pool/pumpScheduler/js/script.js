c1429d66ad8aff267aae287c7fd3e8c02d222443
thomas <szemrot@hotmail.com>
2016-06-16 09:31:52 +0200

    Modified script.js

--- pool/pumpScheduler/js/script.js
+++ pool/pumpScheduler/js/script.js
@@ -62,8 +62,8 @@ function updateScript(xml,lua){
 
 
 function getXMLScript(){
-	alert("getscript");
-/*	$.ajax({
+	//alert("getscript");
+	$.ajax({
 			type: "POST",
 			url: "./action.php?action=getScript&id=main",
 			success: function(r){
@@ -71,7 +71,7 @@ function getXMLScript(){
 				if(result.answer != "OK"){          
 					alert('Erreur : '+result.state);
 				}else return result.state;
-	}});*/
+	}});
 }
 
 
