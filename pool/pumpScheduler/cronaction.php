47152f5e7f7b72c1646668cd074b355ad5b30efd
thomas <szemrot@hotmail.com>
2016-06-20 15:09:52 +0200

    Modified cronaction.php

--- pool/pumpScheduler/cronaction.php
+++ pool/pumpScheduler/cronaction.php
@@ -42,28 +42,12 @@ $temp=getPoolTemperature();
 
 
 
-$sql    = "SELECT ".$temp." FROM pumpSchedule where timeWindow='".$tw."'";
-$result = mysql_query($sql, $link);
 
-if (!$result) {
-    echo "DB Error, could not query the database\n";
-    echo 'MySQL Error: ' . mysql_error();
-    exit;
-}
-
-$pumpConsign=0;
-while ($row = mysql_fetch_assoc($result)) {
-    $pumpConsign=($row[$temp]);
-}
-// treat error case of unfound timewindow in the table
-//if ($pumpConsign="")
-
-mysql_free_result($result);
 
 $answer="OK";
 
 if (!setPinState($pins[$materials["filtration"]],$pumpConsign)) $answer="ERROR";
-$lua = goLua("luascripts/test.lua",$materials,$pins,$luaFeedback);
+$lua = goLua("function run() return 'RETURNOK'; end",$materials,$pins,$luaFeedback);
 
 $state = "{tw:".$tw."}{temp:".$temp."}{setPinState:".$pins[$materials["filtration"]]." ".$pumpConsign."}{Lua:".$luaFeedback."}";
 appendlog("CRONACTION",$answer,$state);
