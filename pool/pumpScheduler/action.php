b66044b4abd304bb0e787f7a40c2e68e271dae21
thomas <szemrot@hotmail.com>
2016-06-20 17:48:21 +0200

    Modified action.php

--- pool/pumpScheduler/action.php
+++ pool/pumpScheduler/action.php
@@ -10,19 +10,6 @@ require_once('luaContext.php');
 $result['state']  = "undef";
 $result['answer']  = 'OK';
 
-function wd_remove_accents($str, $charset='utf-8')
-{
-    $str = htmlentities($str, ENT_NOQUOTES, $charset);
-    
-    $str = preg_replace('#&([A-za-z])(?:acute|cedil|caron|circ|grave|orn|ring|slash|th|tilde|uml);#', '\1', $str);
-    $str = preg_replace('#&([A-za-z]{2})(?:lig);#', '\1', $str); // pour les ligatures e.g. '&oelig;'
-    $str = preg_replace('#&[^;]+;#', '', $str); // supprime les autres caractères
-    
-    return $str;
-}
-
-
-
 switch($_['action']){
     
     case 'sms':
