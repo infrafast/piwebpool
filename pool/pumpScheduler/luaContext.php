16a4ae2416f35e5a3f8a4bed4aeee29b1e8dc4d1
thomas <szemrot@hotmail.com>
2016-06-20 15:10:33 +0200

    Modified luaContext.php

--- pool/pumpScheduler/luaContext.php
+++ pool/pumpScheduler/luaContext.php
@@ -4,42 +4,6 @@ LUA CONTEXT*/
 require_once ("functions.php");
 include ("configuration.php");
 
-function retrieveLuaCode($table){
-    // connect to the database
-    if (!$link = mysql_connect($options["database"]["host"], $options["database"]["username"], $options["database"]["password"])) {
-        echo 'Could not connect to mysql';
-        exit;
-    }
-    
-    if (!mysql_select_db($options["database"]["name"], $link)) {
-        echo 'Could not select database';
-        exit;
-    }     
-    
-    $sql    = "SELECT lua FROM scripts where id='header'";
-    $result = mysql_query($sql, $link);
-    
-    if (!$result) {
-        echo "DB Error, could not query the database\n";
-        echo 'MySQL Error: ' . mysql_error();
-        exit;
-    }
-    
-    $pumpConsign=0;
-        while ($row = mysql_fetch_assoc($result)) {
-        $pumpConsign=($row[$temp]);
-    }
-    // treat error case of unfound timewindow in the table
-    //if ($pumpConsign="")
-    
-    mysql_free_result($result);
-    
-    
-    
-    
-}
-
-
 function goLua($luaCode,$materials,$pins,&$feedback){
     
     try{
