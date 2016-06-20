d9a12f4ebe0f7a057540ce354538673151405659
thomas <szemrot@hotmail.com>
2016-06-20 14:59:16 +0200

    Modified luaContext.php

--- pool/pumpScheduler/luaContext.php
+++ pool/pumpScheduler/luaContext.php
@@ -8,7 +8,7 @@ function goLua($file,$materials,$pins,&$feedback){
     try{
         //$lua=new Lua($file);
         $lua=new Lua();
-        $lua->eval("return RETURNOK");
+        $lua->eval("function run return RETURNOK");
         // record names of the command as variable and manipulate them directly phyisically in lua using set and get functions
         // since the setPinState function gets the logical pin number, we have to get it from the table
     
