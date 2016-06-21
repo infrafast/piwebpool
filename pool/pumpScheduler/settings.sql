253c317a3b1f8c95f2820f1ad017e30e6721cce7
thomas <szemrot@hotmail.com>
2016-06-21 12:09:53 +0200

    Modified settings.sql

--- pool/pumpScheduler/settings.sql
+++ pool/pumpScheduler/settings.sql
@@ -1,7 +1,8 @@
 DROP TABLE IF EXISTS `settings`;
 CREATE TABLE IF NOT EXISTS `settings` (
   `id` varchar(40) NOT NULL,
-  `value` tinyint(1) NOT NULL
+  `value` tinyint(1) NOT NULL,
+  `userSetting` BOOLEAN NOT NULL ,
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 INSERT INTO `settings` (`id`, `value`) VALUES
