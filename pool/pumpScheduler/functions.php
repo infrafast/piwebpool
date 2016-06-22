727128f4fa2c45664d3d6cbf5bcd07f8973ab380
thomas <szemrot@hotmail.com>
2016-06-22 12:27:10 +0200

    Modified functions.php

--- pool/pumpScheduler/functions.php
+++ pool/pumpScheduler/functions.php
@@ -4,6 +4,52 @@
 require "include/FreeSMS.php";
 include("include/php_serial.class.php");
 
+
+function doGraph(){
+//Set config directives
+    header("Content-type: image/png");
+    $cfg['title'] = 'Example graph';
+    $cfg['width'] = 500;
+    $cfg['height'] = 250;
+    
+    //Set data 1
+    $data1 = array(
+        '00' => 10,
+        '01' => 20,
+        '02' => 10,
+        '03' => 40,
+        '04' => 50,
+        '05' => 30,
+        '06' => 20,
+        '07' => 50,
+        '08' => 30,
+        '09' => 20,
+        '10' => 10
+    );
+
+    //Set data 2
+    $data2 = array(
+        '00' => 5,
+        '01' => 8,
+        '02' => 19,
+        '03' => 43,
+        '04' => 56,
+        '05' => 10,
+        '06' => 18,
+        '07' => 47,
+        '08' => 22,
+        '09' => 11,
+        '10' => 8
+    );
+    
+    //Create phpMyGraph instance
+    $graph = new verticalLineGraph();
+
+    //Parse
+    imagepng($graph->parseCompare($data1, $data2, $cfg));
+}
+
+
 function sendsms($message){
     $SMSuser = "19107501";
     $SMSkey = "oGVsksAr0geO6j";
