f273d6f3956752741a25143e43297b34b8ef5f9e
thomas <szemrot@hotmail.com>
2016-06-22 12:39:32 +0200

    Modified functions.php

--- pool/pumpScheduler/functions.php
+++ pool/pumpScheduler/functions.php
@@ -7,7 +7,7 @@ include_once('include/phpMyGraph5.0.php');
 
 function doGraph(){
 //Set config directives
-    header("Content-type: image/png");
+
     $cfg['title'] = 'Example graph';
     $cfg['width'] = 500;
     $cfg['height'] = 250;
