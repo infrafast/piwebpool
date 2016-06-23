5b7315cc613d67bc72859c9c9b9941972455e319
thomas <szemrot@hotmail.com>
2016-06-23 22:35:21 +0200

    Modified graph.php

--- pool/pumpScheduler/graph.php
+++ pool/pumpScheduler/graph.php
@@ -23,7 +23,7 @@ if (!mysql_select_db($options["database"]["name"], $link)) {
     exit;
 }
 
-$sql    = "select ".$_GET["graph"].",id from (select ".$_GET["graph"].", id, timeStamp from measures order by id desc) tempTable order by timeStamp ASC limit ".intval($_GET["period"]);
+$sql    = "select ".$_GET["graph"].",id, timeStamp from (select ".$_GET["graph"].", id, timeStamp from measures order by id desc) tempTable order by timeStamp ASC limit ".intval($_GET["period"]);
 
 $result = mysql_query($sql, $link);
 
