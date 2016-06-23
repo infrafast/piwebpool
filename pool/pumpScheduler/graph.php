d6cad6f3131b05b2e9e285a786c338039e2a40f8
thomas <szemrot@hotmail.com>
2016-06-23 17:01:33 +0200

    Modified graph.php

--- pool/pumpScheduler/graph.php
+++ pool/pumpScheduler/graph.php
@@ -31,7 +31,7 @@ if (!$result) {
         $pumpConsign=($row[$temp]);
     }
 
-Set data
+
 $query="select col1, col2 from tab1 where sub_project = 'sometext'";
 $result=mysql_query($query);
 $row = mysql_fetch_assoc($result);
@@ -40,51 +40,15 @@ $data = array($row['col1'] => $row['col2'],);
 switch($_GET["graph"]){
     
     case 'orp':
-    $data = array(
-        '00' => 700,
-        '01' => 750,
-        '02' => 730,
-        '03' => 650,
-        '04' => 600,
-        '05' => 630,
-        '06' => 700,
-        '07' => 600,
-        '08' => 700,
-        '09' => 720,
-        '10' => 730
-    );
+
     break;
     
     case 'temperature':
-    $data = array(
-        '00' => 10,
-        '01' => 12,
-        '02' => 13,
-        '03' => 15,
-        '04' => 10,
-        '05' => 12,
-        '06' => 10,
-        '07' => 10,
-        '08' => 17,
-        '09' => 12,
-        '10' => 16
-    );
+
     break;
     
     case 'ph':
-    $data = array(
-        '00' => 7.01,
-        '01' => 7.25,
-        '02' => 7.30,
-        '03' => 6.7,
-        '04' => 6.50,
-        '05' => 6.20,
-        '06' => 7.10,
-        '07' => 6.20,
-        '08' => 7.00,
-        '09' => 7.10,
-        '10' => 7.15
-    );
+
     break;    
 	
 	default:
@@ -92,6 +56,21 @@ switch($_GET["graph"]){
 		return;
 	break;
 }
+
+
+$data = array(
+    '00' => 700,
+    '01' => 750,
+    '02' => 730,
+    '03' => 650,
+    '04' => 600,
+    '05' => 630,
+    '06' => 700,
+    '07' => 600,
+    '08' => 700,
+    '09' => 720,
+    '10' => 730
+);
 
 header("Content-type: image/png");
 
