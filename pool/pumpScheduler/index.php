2dc1712f506494698f4e5cc93e9bf5f1ed659a17
thomas <szemrot@hotmail.com>
2016-06-23 14:27:18 +0200

    Modified index.php

--- pool/pumpScheduler/index.php
+++ pool/pumpScheduler/index.php
@@ -432,9 +432,9 @@ $tableSettings = new TableGear($optionsSet);
     <td><div onclick="refreshValue(this,'Temperature');" class="buttonState off"><?php echo getTemperature(); ?></div></td>
 </tr>
 <tr height="150px" id="graphID">
-    <td id="phGraphID" style="background-image:url('graph.php?graph=ph');background-repeat:no-repeat;background-size: 100% 100%;"></td>
-    <td id="orpGraphID" style="background-image:url('graph.php?graph=orp');background-repeat:no-repeat;background-size: 100% 100%;"></td>
-    <td id="temperatureGraphID" style="background-image:url('graph.php?graph=temperature');background-repeat:no-repeat;background-size: 100% 100%;"></td>
+    <td id="graph=ph" style="background-image:url('graph.php?graph=ph');background-repeat:no-repeat;background-size: 100% 100%;"></td>
+    <td id="graph=orp" style="background-image:url('graph.php?graph=orp');background-repeat:no-repeat;background-size: 100% 100%;"></td>
+    <td id="graph=temperature" style="background-image:url('graph.php?graph=temperature');background-repeat:no-repeat;background-size: 100% 100%;"></td>
 </tr>
 <tr>    
     <td></td>
