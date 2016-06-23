3ad670739a380407e758cfbbfd20c75dfe1a4a1a
thomas <szemrot@hotmail.com>
2016-06-23 15:38:44 +0200

    Modified cronaction.php

--- pool/pumpScheduler/cronaction.php
+++ pool/pumpScheduler/cronaction.php
@@ -114,7 +114,7 @@ if (!$result) {
     $measureIndex=$measureIndex+1;
         if ($measureIndex>168) $measureIndex=0;
     
-    $sql    = "INSERT INTO `measures` (`id`, `timestamp`, `orp`, `ph`, `temperature`) VALUES ('".$measureIndex+++"', CURRENT_TIMESTAMP,'"+ $orpValue+"', '"+ $phValue+"', '"+ $temperatureValue+"') ON DUPLICATE KEY UPDATE id="+$measureIndex+", orp=2, ph=4, temperature=6, timestamp=CURRENT_TIME;";
+    $sql    = "INSERT INTO `measures` (`id`, `timestamp`, `orp`, `ph`, `temperature`) VALUES ('".$measureIndex"', CURRENT_TIMESTAMP,'"+ $orpValue+"', '"+ $phValue+"', '"+ $temperatureValue+"') ON DUPLICATE KEY UPDATE id="+$measureIndex+", orp=2, ph=4, temperature=6, timestamp=CURRENT_TIME;";
     $result = mysql_query($sql, $link);
     
     if (!$result) {
