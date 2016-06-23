1daf11f1319878cddc7ce18dc7f6f4219bbd3a51
thomas <szemrot@hotmail.com>
2016-06-23 14:09:57 +0200

    Modified index.php

--- pool/pumpScheduler/index.php
+++ pool/pumpScheduler/index.php
@@ -432,7 +432,10 @@ $tableSettings = new TableGear($optionsSet);
     <td><div onclick="refreshValue(this,'Temperature');" class="buttonState off"><?php echo getTemperature(); ?></div></td>
 </tr>
 <tr height=150px>
-    <td style="background-image:url('graph.php?graph=ph&period='+document.getElementById('periodID').value);background-repeat:no-repeat;background-size: 100% 100%;"></td>
+    <script>
+        var phGraphURL = 'graph.php?graph=ph&period='+document.getElementById('periodID').value;
+    </script>
+    <td style="background-image:url(phGraphURL);background-repeat:no-repeat;background-size: 100% 100%;"></td>
     <td style="background-image:url('graph.php?graph=orp');background-repeat:no-repeat;background-size: 100% 100%;"></td>
     <td style="background-image:url('graph.php?graph=temperature');background-repeat:no-repeat;background-size: 100% 100%;"></td>
 </tr>
