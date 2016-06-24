c6af11a09d1194bbc1fb6d6e609d65eeee0558c7
thomas <szemrot@hotmail.com>
2016-06-24 15:14:19 +0200

    Modified script.js

--- pool/pumpScheduler/js/script.js
+++ pool/pumpScheduler/js/script.js
@@ -142,7 +142,7 @@ function resetSchedule(){
 
   function rgb2hex(red, green, blue) {
         var rgb = blue | (green << 8) | (red << 16);
-        return '#' + (0x1000000 + rgb).toString(16).slice(1)
+        return '#' + (0x1000000 + rgb).toString(16).slice(1);
   }
 
 function getColor(median,tolerance,value){
