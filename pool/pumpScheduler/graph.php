233c0aa0f3859e21b28ab5a3849ae5d4085db176
thomas <szemrot@hotmail.com>
2016-06-24 09:11:33 +0200

    Modified graph.php

--- pool/pumpScheduler/graph.php
+++ pool/pumpScheduler/graph.php
@@ -43,7 +43,7 @@ $graph = new phpMyGraph();
 
 //Parse
 if ($_GET["type"]=="bar"){
-    $cfg['label']=$_GET["graph"];
+    $cfg['label']=$_GET["title"];
     //$cfg['label-visible']=false;
     $cfg['value-label-visible']=false;
     $cfg['zero-line-visible']=false;
