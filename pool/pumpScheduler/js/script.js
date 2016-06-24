bd54fff33e33df995d6746546e900f3a0b4a0bd8
thomas <szemrot@hotmail.com>
2016-06-24 12:03:14 +0200

    Modified script.js

--- pool/pumpScheduler/js/script.js
+++ pool/pumpScheduler/js/script.js
@@ -46,7 +46,7 @@ function changeState(pin,elem){
 					$(elem).removeClass('on');
 					$(elem).removeClass('off');
 					$(elem).addClass((newState==1?'on':'off')); 
-					$(elem).html('<?php echo "<br>".(getPinState($pin,$pins)==1?"on":"off")."<br><br>";?>');
+					$(elem).html(newState==1?'on':'off');
 					//$(elem).getElementById("commandButtonID").innerHTML = "<br>whatever<br>";
 				}else{
 					alert('Erreur : '+result.answer);
