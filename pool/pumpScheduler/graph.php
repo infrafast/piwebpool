60083232901cce1a19fe8134d78256b73dbf155a
thomas <szemrot@hotmail.com>
2016-06-24 17:00:12 +0200

    Modified graph.php

--- pool/pumpScheduler/graph.php
+++ pool/pumpScheduler/graph.php
@@ -36,7 +36,7 @@ if (!$result) {
 $data = array();
 while ($row = mysql_fetch_assoc($result)){
     $date = strtotime($row['timeStamp']);
-    $hour = date('DH', $date);    
+    $hour = date('H', $date);    
     //$data[$row['timeStamp']] = $row[$_GET["graph"]];
     $data[$hour] = $row[$_GET["graph"]];    
     //echo "<br>".$row['timeStamp']." ". $row[$_GET["graph"]]." ".$hour;
