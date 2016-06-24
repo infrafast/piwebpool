f12b096f9dafed01c484542e6116a3adaaff4916
thomas <szemrot@hotmail.com>
2016-06-24 09:12:47 +0200

    Modified index.php

--- pool/pumpScheduler/index.php
+++ pool/pumpScheduler/index.php
@@ -398,7 +398,7 @@ $tableSettings = new TableGear($optionsSet);
 </tr>
 <tr><th>Action</th><th>Etat</th></tr>
 <?php foreach($materials as $material=>$pin){ ?>
-<tr>
+<tr height=60px>
 	<?php echo "<td style=\"background-image:url('graph.php?graph=pump&period=24&width=400&height=200&type=bar&title=".$material."');background-repeat:no-repeat;background-size: 100% 100%;\">"?>
 	</td>
 	<td><div onclick="changeState(<?php echo $pin; ?>,this)" class="buttonState <?php echo (getPinState($pin,$pins)=='on'?'off':'on'); ?>"></div></td></tr>
