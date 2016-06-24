a13fc1c80a5a8361f636e3bfed8f0d6a05164ac1
thomas <szemrot@hotmail.com>
2016-06-24 10:35:33 +0200

    Modified graph.php

--- pool/pumpScheduler/graph.php
+++ pool/pumpScheduler/graph.php
@@ -7,7 +7,7 @@ $cfg['width'] = $_GET["width"] ;
 $cfg['height'] = $_GET["height"];
 $cfg['key-color'] = "00a2e8";
 $cfg['key-color'] = "00a2e8";
-$cfg['column-color'] = "00ff00";
+$cfg['column-color'] = "00a2e8";
 $cfg['value-label-color'] = "ff0000";
 
 //keyvisible = affichage de l'echelle du temps
