f08343bd0938c00d8becd0e4a28ac1cf05eaf77d
thomas <szemrot@hotmail.com>
2016-06-24 12:11:52 +0200

    Modified script.js

--- pool/pumpScheduler/js/script.js
+++ pool/pumpScheduler/js/script.js
@@ -156,7 +156,7 @@ function refreshValue(elem,action){
 				if(result.answer == "OK"){
 				    var newValue = "<br>"+result.state+"<br>";
                     alert(newValue);
-				    $(elem).innerHtml();
+				    $(elem).innerHtml(newValue);
 				}else{
 					alert('Erreur : '+result.answer);
 				}
