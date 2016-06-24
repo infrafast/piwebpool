a02036177bd3247e4b1baf5dacd4207ea23f821f
thomas <szemrot@hotmail.com>
2016-06-24 10:45:29 +0200

    Modified index.php

--- pool/pumpScheduler/index.php
+++ pool/pumpScheduler/index.php
@@ -406,7 +406,7 @@ foreach($materials as $material=>$pin){ ?>
 <tr height=100px>
 	<?php echo "<td style=\"background-image:url('graph.php?graph=".$materialsColumn[$material]."&period=24&width=400&height=100&type=bar&title=".$material."');background-repeat:no-repeat;background-size: 100% 100%;\">"?>
 	</td>
-	<td height=100%><div onclick="changeState(<?php echo $pin; ?>,this)" class="buttonState <?php echo (getPinState($pin,$pins)=='on'?'off':'on'); ?>"></div></td>
+	<td height=100px><div onclick="changeState(<?php echo $pin; ?>,this)" class="buttonState <?php echo (getPinState($pin,$pins)=='on'?'off':'on'); ?>"></div></td>
 </tr>
 <?php } ?>
 </table>
