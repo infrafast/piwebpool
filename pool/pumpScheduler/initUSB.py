13df7ed979a415a6ff127b564bb470e68919166b
thomas <szemrot@hotmail.com>
2016-06-26 22:20:47 +0200

    Modified initUSB.py

--- pool/pumpScheduler/initUSB.py
+++ pool/pumpScheduler/initUSB.py
@@ -2,34 +2,32 @@ import os         #import OS module to easily exit the program
 import serial     #import serial module to enable serial commands
 import threading  #import threading module to create a separate thread for reading the serial port
 
-ser = serial.Serial(      #initiate the serial connection into the 'ser' variable
+ser0 = serial.Serial(      #initiate the serial connection into the 'ser' variable
   port='/dev/ttyUSB0',    #set the port address of the Atlas stamp
   baudrate=9600          #set the baudrate
 )
 
+ser1 = serial.Serial(      #initiate the serial connection into the 'ser' variable
+  port='/dev/ttyUSB1',    #set the port address of the Atlas stamp
+  baudrate=9600          #set the baudrate
+)
+
+#ser2 = serial.Serial(      #initiate the serial connection into the 'ser' variable
+#  port='/dev/ttyUSB0',    #set the port address of the Atlas stamp
+#  baudrate=9600          #set the baudrate
+#)
+
 def read_from_port(ser):    #create definition for your serial read thread
-  line="Go!"                #initiate read variable we'll call 'line'
   while True:               #start the While loop
     data = ser.read()       #read the serial port and store in the 'data' variable
     if(data == "\r"):       #if there is a carriage return
-      print ">> " + line    #print the output
       line = ""             #set the variable back to nothing
     else:
       line = line + data    #append the data onto the line variable
 
 ser.write('\r')     #an initial write to clear the serial buffer
-#flush = ser.read(3) #flush into variable (only needed for EZO circuits)
+ser1.write('\r')
 
-thread = threading.Thread(target=read_from_port, args=(ser,)) #create the thread to read the serial port, include the target definition and the serial protocol
-thread.start()  #start the thread
-print '\r\nEnter your commands below.\r\nInsert "exit" to leave the application.'
-
-while True:
-  input = raw_input("")  # get keyboard/command input
-
-  if input == 'exit':     #if you type 'exit'
-    ser.close()           #close the serial port
-    os._exit(1)           #exit the program
-
-  else:
-    ser.write(input + "\r") #write the command to the serial port
+ser.close()           #close the serial port
+ser1.close()
+os._exit(1)           #exit the program
