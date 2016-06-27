93c60a4bc7f9da33c271b81356ee0a75c55c24d8
thomas <szemrot@hotmail.com>
2016-06-27 16:04:07 +0200

    Modified script.js

--- pool/pumpScheduler/js/script.js
+++ pool/pumpScheduler/js/script.js
@@ -62,7 +62,7 @@ function updateScript(xml,lua,script){
 			type: "POST",
 			url: "./action.php?action=updateScript",
 			data:{id:script,xml:xml,lua:"info='à l\'huile de morue';"},
-			dataType: "",
+			dataType: "xml",
 			async:false,
 			success: function(r){
 				result = eval(r);
