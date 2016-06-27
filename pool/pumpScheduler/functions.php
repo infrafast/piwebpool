89ddb2150a594426178c0f21b5e89d15a5921f8d
thomas <szemrot@hotmail.com>
2016-06-27 09:46:37 +0200

    Modified functions.php

--- pool/pumpScheduler/functions.php
+++ pool/pumpScheduler/functions.php
@@ -11,7 +11,7 @@ function sendsms($message){
     $feedback="void";            
     $result=true;
     if (!sms( $SMSuser, $SMSkey, $message,$feedback )) $result=false;
-    appendlog("sendsms",$result,$feedback);
+    appendlog("sendsms:",$result,$feedback);
     return $result;
 }
 
