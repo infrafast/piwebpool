8a5051ea5cc642a21a0636b5004b1fd3328ccd48
thomas <szemrot@hotmail.com>
2016-06-27 13:06:16 +0200

    Modified functions.php

--- pool/pumpScheduler/functions.php
+++ pool/pumpScheduler/functions.php
@@ -37,7 +37,7 @@ function appendlog($source,$answer,$status,$logfilename="logfile.txt"){
     return file_put_contents($logfilename, "[".date("Y-m-d H:i:s")."][".$source.' ' .$answer."][".html_entity_decode($status)."]\n" , FILE_APPEND | LOCK_EX);
 }
 
-function getLog($lines = 24){
+function getLog($logfilename, $lines = 24){
     $data = '';
     $filename="./".$logfilename;
     $fp = fopen($filename, "r");
