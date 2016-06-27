151acae036fc9dc9fdda27ac2ac722c2aea34367
thomas <szemrot@hotmail.com>
2016-06-27 16:05:35 +0200

    Modified script.js

--- pool/pumpScheduler/js/script.js
+++ pool/pumpScheduler/js/script.js
@@ -61,7 +61,7 @@ function updateScript(xml,lua,script){
 	$.ajax({
 			type: "POST",
 			url: "./action.php?action=updateScript",
-			data:{id:script,xml:xml,lua:"info='à l\\'huile de morue';"},
+			data:{id:script,xml:xml,lua:"info='à l\\'huile de foi';"},
 			async:false,
 			success: function(r){
 				result = eval(r);
