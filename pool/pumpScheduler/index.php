b01b78a57f25744e05ba54e55dd350955e000b0d
thomas <szemrot@hotmail.com>
2016-06-27 17:26:03 +0200

    Modified index.php

--- pool/pumpScheduler/index.php
+++ pool/pumpScheduler/index.php
@@ -477,7 +477,7 @@ foreach($materials as $material=>$pin){ ?>
   <tr>
   <tr>
     <th>Code preview</th>
-    <tr><td id="scriptareaID" width="100%"><textarea rows="10" readonly style="color: grey; width: 100%;">Erreur chargement de script</textarea></div></tr>
+    <tr><td width="100%"><textarea id="scriptareaID" rows="10" readonly style="color: grey; width: 100%;">Erreur chargement de script</textarea></div></tr>
 </table>
 
 <table class="materialTab">
