f26e9934b6b10dda705a7082a46f05fe82f7dfc6
thomas <szemrot@hotmail.com>
2016-06-27 17:04:36 +0200

    Modified action.php

--- pool/pumpScheduler/action.php
+++ pool/pumpScheduler/action.php
@@ -60,7 +60,7 @@ switch($_['action']){
     case 'resetSchedule':
         $result['state'] = "reset done";
 
-        $dbms_schema='scripts.sql';
+        $dbms_schema='pumpSchedule.sql';
 
         mysql_connect($options["database"]["host"],$options["database"]["username"],$options["database"]["password"]) or die('error connection');
         mysql_select_db($options["database"]["name"]) or die('error database selection');
