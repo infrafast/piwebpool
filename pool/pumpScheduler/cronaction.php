62066391469b5abfbd4dfd7fc0f1f6a9db1eb0b0
thomas <szemrot@hotmail.com>
2016-06-27 10:51:40 +0200

    Modified cronaction.php

--- pool/pumpScheduler/cronaction.php
+++ pool/pumpScheduler/cronaction.php
@@ -131,8 +131,7 @@ if (!$result) {
     }
 }
 if ($answer=="OK")
-    //$state = "{Heure:".$tw."}{temperature:".$temp."}{setPinState:".$pins[$materials["filtration"]]." ".$pumpConsign."}{Lua:".$luaFeedback."}";
-    $state = "{Heure:".$tw."}{temperature:".$temp."}{Filtration:".$pumpConsign==1?"MARCHE":"ARRET"."}{Lua:".$luaFeedback."}";
+    $state = "{Heure:".$tw."}{temperature:".$temp."}{Filtration:".$pins[$materials["filtration"]]." ".$pumpConsign."}{Lua:".$luaFeedback."}";
 appendlog("CRONACTION",$answer,$state);
 echo $answer.$state;
 ?>
