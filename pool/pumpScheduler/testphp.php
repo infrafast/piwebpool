b97064f8946b2c36465153f6619ff1d2cd6474a5
thomas <szemrot@hotmail.com>
2016-06-27 14:41:59 +0200

    Modified testphp.php

--- pool/pumpScheduler/testphp.php
+++ pool/pumpScheduler/testphp.php
@@ -2,7 +2,10 @@
 
 require_once('functions.php');
 
-sendemail("test");
-sendsms("test");
+//sendemail("test");
+//sendsms("test");
+
+$ms="warningEau = ' Attention à la qualité de l'eau!';";
+echo addslashes($ms);
 
 ?>
