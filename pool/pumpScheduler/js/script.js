255f2247c1a704dd1933ef3c2304423b126561b7
thomas <szemrot@hotmail.com>
2016-06-27 16:19:43 +0200

    Modified script.js

--- pool/pumpScheduler/js/script.js
+++ pool/pumpScheduler/js/script.js
@@ -61,7 +61,7 @@ function updateScript(xml,lua,script){
 	$.ajax({
 			type: "POST",
 			url: "./action.php?action=updateScript",
-			data:{id:script,xml:xml,lua:"info='à l\\\\\\'huile de COCO';"},
+			data:{id:script,xml:xml,lua:lua},
 			async:false,
 			success: function(r){
 				result = eval(r);
