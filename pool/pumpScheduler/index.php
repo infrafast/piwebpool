ff320ab41bd7e9d08fd2dbdceaa0dc3907c215fe
thomas <szemrot@hotmail.com>
2016-06-27 15:32:08 +0200

    Modified index.php

--- pool/pumpScheduler/index.php
+++ pool/pumpScheduler/index.php
@@ -664,8 +664,9 @@ foreach($materials as $material=>$pin){ ?>
     // it update the call to the graph function according to the selected value
      	var cols = document.getElementById('graphID').getElementsByTagName('td'), colslen = cols.length, i = -1;
     	while(++i < colslen){
+    	    cols[i].style.backgroundSize="100% 100%";
             var imgSrc = 'url(graph.php?'+cols[i].id+'&period='+document.getElementById('periodID').value+'&width=500&height=200)'; 
-            cols[i].style.backgroundImage=imgSrc;    		
+            cols[i].style.backgroundImage=imgSrc; 		
             //alert(imgSrc);
     	}    
     }  
