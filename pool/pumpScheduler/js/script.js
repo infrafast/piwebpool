63460199286b17c9d16f2e7079d87df2f7e11a13
thomas <szemrot@hotmail.com>
2016-06-27 16:00:09 +0200

    Modified script.js

--- pool/pumpScheduler/js/script.js
+++ pool/pumpScheduler/js/script.js
@@ -61,7 +61,7 @@ function updateScript(xml,lua,script){
 	$.ajax({
 			type: "POST",
 			url: "./action.php?action=updateScript",
-			data:{id:script,xml:xml,lua:"info='à l\\'huile';"},
+			data:{id:script,xml:xml,lua:"info='à l\\\'huile';"},
 			dataType: "json",
 			async:false,
 			success: function(r){
