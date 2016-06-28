d66e30db866135af2df2e36dc8d24a7c48813349
thomas <szemrot@hotmail.com>
2016-06-28 14:20:21 +0200

    Modified cronaction.php

--- pool/pumpScheduler/cronaction.php
+++ pool/pumpScheduler/cronaction.php
@@ -124,7 +124,7 @@ if (!$result) {
             $sql = "INSERT INTO `measures` (`id`, `timestamp`, `orp`, `ph`, `temperature`";
             foreach($materials as $material=>$pin) $sql = $sql.", `".$materialsColumn[$material]."`";
             $sql = $sql.") VALUES ('".$measureIndex."', CURRENT_TIMESTAMP,'".$orpValue."', '".$phValue."', '".$temperatureValue;
-            foreach($materials as $material=>$pin) $sql = $sql.", `".getPin($pins[$materials[$material]])."`";
+            foreach($materials as $material=>$pin) $sql = $sql.", '".getPin($pins[$materials[$material]])."'";
             $sql = $sql.") ON DUPLICATE KEY UPDATE id=".$measureIndex.", orp=".$orpValue.", ph=".$phValue.", temperature=".$temperatureValue.", timestamp=CURRENT_TIME";
             foreach($materials as $material=>$pin) $sql = $sql.", ".$materialsColumn[$material]."=".getPin($pins[$materials[$material]]);
             $sql = $sql.";";
