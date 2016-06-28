4354dca35ab493663e238576b5c7785eeb6590bf
thomas <szemrot@hotmail.com>
2016-06-28 12:55:14 +0200

    Modified cronaction.php

--- pool/pumpScheduler/cronaction.php
+++ pool/pumpScheduler/cronaction.php
@@ -113,6 +113,8 @@ if (!$result) {
         $temperatureValue = getTemperature();
         $measureIndex=$measureIndex+1;
         if ($measureIndex>168) $measureIndex=0;
+        
+        
             
         $sql="UPDATE settings SET value=".$measureIndex." WHERE id='measureIndex'";
         $result = mysql_query($sql, $link);
