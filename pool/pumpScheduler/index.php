205f45c0566ffcebc96155dca6c1cf45275a1e97
thomas <szemrot@hotmail.com>
2016-06-28 16:12:09 +0200

    Modified index.php

--- pool/pumpScheduler/index.php
+++ pool/pumpScheduler/index.php
@@ -439,8 +439,8 @@ foreach($materials as $material=>$pin){ ?>
     <tr class="header" id="blocklyTable">
         <td colspan="1"><b><span>-</span> Programmation</b></td>
     </tr>
-    <tr>
-        <th style="width: 100%;">
+    <th style="width: 100%;">
+    <td>
               <select  name="luascript"  id="scriptID">
                     <option value="main">defaut</option>
                     <option value="custom">perso</option>
@@ -448,8 +448,8 @@ foreach($materials as $material=>$pin){ ?>
                 <input type="button" value="sauver" onclick="saveCode(document.getElementById('scriptID').value);">
                 <input type="button" value="charger" onclick="loadXML(document.getElementById('scriptID').value);">
                 <input type="button" value="executer" onclick="">
-        </th>
-    </tr>
+    </td>
+    </th>
     <tr>
       <td><div id="blocklyDiv" style="height: 500px; width: 100%;"></div></td>
         <xml id="toolbox" style="display: none">
