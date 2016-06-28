e8e276c72223f1fa432acd361da5fa8fca1d8763
thomas <szemrot@hotmail.com>
2016-06-28 17:07:54 +0200

    Modified functions.php

--- pool/pumpScheduler/functions.php
+++ pool/pumpScheduler/functions.php
@@ -39,7 +39,7 @@ function appendlog($source,$answer,$status,$filename="logfile.txt"){
     // remove \n and \r so logfile are written on one line only except the last one
     $text =  preg_replace('~[\r\n]+~', '', "[".date("Y-m-d H:i:s")."][".$source.' ' .$answer."][".html_entity_decode($status)."]")."\n";
 	if (!file_exists($filename)) { touch($filename); chmod($filename, 0666); }
-	if (filesize($filename) > (1024*1024)*0.5) {
+	if (filesize($filename) > 37868) {
 		$filename2 = $filename."old";
 		if (file_exists($filename2)) unlink($filename2);
 		rename($filename, $filename2);
