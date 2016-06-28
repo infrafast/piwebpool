903969fdb73f1cf043bdf2cac619ad6b8e9c51aa
thomas <szemrot@hotmail.com>
2016-06-28 10:52:17 +0200

    Modified testphp.php

--- pool/pumpScheduler/testphp.php
+++ pool/pumpScheduler/testphp.php
@@ -5,7 +5,24 @@ require_once('functions.php');
 //sendemail("test");
 //sendsms("test");
 
-$ms="warningEau = ' Attention à la qualité de l'eau!';";
-echo mysql_real_escape_string(htmlspecialchars(addslashes($ms)));
+
+class workerThread extends Thread {
+public function __construct($i){
+  $this->i=$i;
+}
+
+public function run(){
+  while(true){
+   echo $this->i;
+   sleep(1);
+  }
+}
+}
+
+for($i=0;$i<50;$i++){
+$workers[$i]=new workerThread($i);
+$workers[$i]->start();
+}
+
 
 ?>
