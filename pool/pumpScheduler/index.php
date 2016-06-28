a4937183c812caddef73df9d199d95776e69f77f
thomas <szemrot@hotmail.com>
2016-06-28 16:04:37 +0200

    Modified index.php

--- pool/pumpScheduler/index.php
+++ pool/pumpScheduler/index.php
@@ -509,7 +509,6 @@ foreach($materials as $material=>$pin){ ?>
     refreshValue(document.getElementById('divORPMeasureID'),'ORP');
     refreshValue(document.getElementById('divTemperatureMeasureID'),'Temperature');
     
-    
     // draw measures graph
     updateGraph();
     
@@ -626,17 +625,33 @@ foreach($materials as $material=>$pin){ ?>
     };
 
 
-  var workspace = Blockly.inject('blocklyDiv',
-      {toolbox: document.getElementById('toolbox'),
-        zoom:
-             {controls: true,
-              wheel: true,
-              startScale: 1.0,
-              maxScale: 3,
-              minScale: 0.3,
-              scaleSpeed: 1.2},
-         trashcan: true          
-      });
+
+var blocklyArea = document.getElementById('blocklyArea');
+  var blocklyDiv = document.getElementById('blocklyDiv');
+  var workspace = Blockly.inject(blocklyDiv,
+      {toolbox: document.getElementById('toolbox')});
+  var onresize = function(e) {
+    // Compute the absolute coordinates and dimensions of blocklyArea.
+    var element = blocklyArea;
+    var x = 0;
+    var y = 0;
+    do {
+      x += element.offsetLeft;
+      y += element.offsetTop;
+      element = element.offsetParent;
+    } while (element);
+    // Position blocklyDiv over blocklyArea.
+    blocklyDiv.style.left = x + 'px';
+    blocklyDiv.style.top = y + 'px';
+    blocklyDiv.style.width = blocklyArea.offsetWidth + 'px';
+    blocklyDiv.style.height = blocklyArea.offsetHeight + 'px';
+  };
+  window.addEventListener('resize', onresize, false);
+  onresize();
+
+
+
+
     
     loadXML("main");
 
