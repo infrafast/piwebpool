48ceaccbab2087a8f2b24d32f8300f400770a6c8
thomas <szemrot@hotmail.com>
2016-06-28 17:02:51 +0200

    Modified script.js

--- pool/pumpScheduler/js/script.js
+++ pool/pumpScheduler/js/script.js
@@ -113,7 +113,6 @@ function forceCron(){
 	alert("call force cron");
 	$.ajax({
 		    type: "POST",
-		    async: false,
 			url: "./action.php?action=forceCron",
 		success: function(r){
 			result = eval(r);
