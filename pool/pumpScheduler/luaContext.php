9e327177133e9e8d6431867ccde383a389898b05
thomas <szemrot@hotmail.com>
2016-06-28 10:28:28 +0200

    Modified luaContext.php

--- pool/pumpScheduler/luaContext.php
+++ pool/pumpScheduler/luaContext.php
@@ -23,7 +23,8 @@ function goLua($luaCode,$materials,$pins,&$feedback){
         $lua->assign("temperature",getTemperature());
         $lua->assign("ph",getPh());
         $lua->assign("orp",getORP());
-        $lua->assign("time",intval(getCurrentTimeWindow()));
+        $lua->assign("period",intval(getCurrentTimeWindow()));
+        $lua->assign("hour",intval(getCurrentTime()));
         
         $lua->registerCallback("set", 'setPinState'); 
         $lua->registerCallback("get", 'getPin'); 
