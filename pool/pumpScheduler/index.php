0f38791711fb56623cf723ab2eab67f79e6a5fd9
thomas <szemrot@hotmail.com>
2016-06-28 14:37:32 +0200

    Modified index.php

--- pool/pumpScheduler/index.php
+++ pool/pumpScheduler/index.php
@@ -393,10 +393,7 @@ $tableSettings = new TableGear($optionsSet);
 </tr>
 <tr><th>Etat ces dernières 24h</th><th>Etat actuel</th></tr>
 <?php 
-$materialsColumn = array(
-			"filtration"=>"pump",
-			"traitement"=>"treatment",
-		    );
+
 foreach($materials as $material=>$pin){ ?>
 <tr height=80px>
 	<?php echo "<td style=\"background-image:url('graph.php?graph=".$materialsColumn[$material]."&period=24&width=850&height=90&type=bar&title=".$material."');background-repeat:no-repeat;background-size: 100% 100%;\">"?>
