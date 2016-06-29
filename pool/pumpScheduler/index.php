315b1613c352a7dfed2a7bcfb16179c2609db4dc
thomas <szemrot@hotmail.com>
2016-06-29 17:01:57 +0200

    Modified index.php

--- pool/pumpScheduler/index.php
+++ pool/pumpScheduler/index.php
@@ -694,7 +694,7 @@ $tableSettings = new TableGear($optionsSet);
             var elemID = 'div'+id+'MeasureID';
             var elem = document.getElementById(elemID);
 
-            alert(actionCall('action=calibrate&id='+id,false,'Placer la sonde dans la solution\npendant 2 minutes puis confirmez',false, true))
+            alert(actionCall('action=calibrate&id='+id,false,'Placer la sonde dans la solution\npendant 2 minutes puis confirmez',false, true));
             //refreshValue(elem,id);                
         }
           
