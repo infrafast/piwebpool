4de6bcef209ccf3edecffb9e0c9c042ef5e80f53
thomas <szemrot@hotmail.com>
2016-06-29 21:24:53 +0200

    Modified script.js

--- pool/pumpScheduler/js/script.js
+++ pool/pumpScheduler/js/script.js
@@ -143,10 +143,10 @@ function getColor(median,value){
     var diff = Math.abs(value - median);
     var ecart = diff/median;
 
-    var colorName='grey';
-    if (ecart>0.5) colorName='Tomato';
-    if (ecart>0.3) colorname='Orange';
-    if (ecart>0.1) colorname='Yellow';
+    var colorName='';
+    if (ecart<0.5) colorName='Orange';
+    if (ecart<0.3) colorname='Yellow';
+    if (ecart<0.1) colorname='LimeGreen';
     alert(ecart);
     return colorName;
 }
