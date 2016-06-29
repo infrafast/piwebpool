3453445b90821a56b8cf96b6b14a0785df3f37c6
thomas <szemrot@hotmail.com>
2016-06-29 16:12:56 +0200

    Modified script.js

--- pool/pumpScheduler/js/script.js
+++ pool/pumpScheduler/js/script.js
@@ -21,7 +21,7 @@ function actionCall(UrlData, async=true, messageBox=null, feedback=false, confir
     if (messageBox!==null)
         if (confirmation!==false){
             result = confirm(messageBox);
-            if (true !== result) return;
+            if (true !== result) return false;
         }else alert(messageBox);
 	$.ajax({
 		    type: "POST",
