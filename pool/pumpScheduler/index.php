7f9befa7933b9dce74604bb93295115c8991f12d
thomas <szemrot@hotmail.com>
2016-06-29 16:59:31 +0200

    Modified index.php

--- pool/pumpScheduler/index.php
+++ pool/pumpScheduler/index.php
@@ -694,7 +694,7 @@ $tableSettings = new TableGear($optionsSet);
             var elemID = 'div'+id+'MeasureID';
             var elem = document.getElementById(elemID);
 
-            if (actionCall('action=calibrate&id='+id,false,'Placer la sonde dans la solution\npendant 2 minutes puis confirmez',true, true)!=false)
+            if (actionCall('action=calibrate&id='+id,false,'Placer la sonde dans la solution\npendant 2 minutes puis confirmez',false, true)!=false)
                 alert("call next");
         }
           
