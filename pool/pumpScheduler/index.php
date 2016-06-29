6811d2c0cd0733e79e5ed489171510e132094e32
thomas <szemrot@hotmail.com>
2016-06-29 17:32:35 +0200

    Modified index.php

--- pool/pumpScheduler/index.php
+++ pool/pumpScheduler/index.php
@@ -693,7 +693,8 @@ $tableSettings = new TableGear($optionsSet);
         function calibrateAndRefresh(id){
             var elemID = 'div'+id+'MeasureID';
             var elem = document.getElementById(elemID);
-            elem.style.backgroundImage='images/loading.gif';
+            
+            document.getElementById(PhCalibrateID).style.backgroundImage='images/loading.gif';
             if(actionCall('action=calibrate&id='+id,false,'Placer la sonde dans la solution\npendant 2 minutes puis confirmez',false, true)=="OK")
                 elem.click();                
         }
