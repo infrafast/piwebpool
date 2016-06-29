fcab257776b4c5aa906fc7e11910e6d3c18daca1
thomas <szemrot@hotmail.com>
2016-06-29 09:34:31 +0200

    Modified script.js

--- pool/pumpScheduler/js/script.js
+++ pool/pumpScheduler/js/script.js
@@ -30,7 +30,7 @@ function actionCall(data, wait=true, messageBox=null, feedback=false, confirm=fa
 			async:wait,
 		success: function(r){
 			result = eval(r);
-			
+			if (feedback===true) alert("Resultat: "+result.answer+(result.answer===true?"":result.state));
 	    }
 	});
 	return result.state;
