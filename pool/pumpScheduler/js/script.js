1301b0eb2fb39a11c48f7e199ccf837d3f568583
thomas <szemrot@hotmail.com>
2016-06-29 22:31:57 +0200

    Modified script.js

--- pool/pumpScheduler/js/script.js
+++ pool/pumpScheduler/js/script.js
@@ -143,9 +143,9 @@ function getColor(middleVal,compareVal){
     var diffVal = Math.abs(compareVal - middleVal);
     var ecartVal = diffVal/middleVal;
     var colorSensor="LimeGreen";
-    if (ecartVal>0.5) alert("Tomato");
-    if (ecartVal>0.3) alert("Orange");
-    if (ecartVal>0.1) alert("Yellow");
+    if (ecartVal>0.5) colorSensor="Tomato";
+    if (ecartVal>0.3) alercolorSensort="Orange";
+    if (ecartVal>0.1) colorSensor="Yellow";
     return colorSensor;
 }
 
