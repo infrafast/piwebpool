3f93da5cc271f4da18ee845088593b4e158ca3b2
thomas <szemrot@hotmail.com>
2016-06-29 17:23:30 +0200

    Modified index.php

--- pool/pumpScheduler/index.php
+++ pool/pumpScheduler/index.php
@@ -423,7 +423,7 @@ $tableSettings = new TableGear($optionsSet);
         <td id="graph=temperature" style="background-repeat:no-repeat; background-image: url('images/loading.gif');"></td>
     </tr>
     <tr>
-        <td><input type="button" value="Etalonner (ph7)" onclick="calibrateAndRefresh('Ph');"></td>
+        <td style="background-repeat:no-repeat; background-image: url('images/loading.gif');"><input type="button" value="Etalonner (ph7)" onclick="calibrateAndRefresh('Ph');"></td>
         <td><input type="button" value="Etalonner (650mv)" onclick="actionCall('action=calibrate&id=ORP',false,'Placer la sonde dans solution 650mV\npendant 2 minutes puis confirmez',true, true);"></td>
         <td></td>
     </tr>
