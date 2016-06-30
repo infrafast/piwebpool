5825a40d444d65e8213313f81b31cd6dbbb8a52d
thomas <szemrot@hotmail.com>
2016-06-30 16:18:03 +0200

    Modified index.php

--- pool/pumpScheduler/index.php
+++ pool/pumpScheduler/index.php
@@ -701,7 +701,8 @@ $tableSettings = new TableGear($optionsSet);
                 if (i>(graphTypes.length)-1) i=0;
                 nextGraphType = graphTypes[i];                
             }
-            //element.removeClass(currentGraphType).addClass(nextGraphType);
+            element.removeClass(currentGraphType)
+            element.addClass(nextGraphType);
             updateGraph(element);
         }
           
