aecb4ee8d57c88dfa87874d98a011d2cd1e1ab27
thomas <szemrot@hotmail.com>
2016-06-30 15:28:07 +0200

    Modified index.php

--- pool/pumpScheduler/index.php
+++ pool/pumpScheduler/index.php
@@ -680,7 +680,7 @@ $tableSettings = new TableGear($optionsSet);
           //alert("sent: "+Blockly.Lua.workspaceToCode(workspace));
         }    
         
-        function updateGraph(element,typeGraph){
+        function updateGraph(element){
             element.style.backgroundSize="100% 100%";
             // need tu tune the width/hgith 
             //var imgSrc = 'url(graph.php?'+element.id+'&period='+document.getElementById('periodID').value+'&width=500&height=200&type='+typeGraph+')';
