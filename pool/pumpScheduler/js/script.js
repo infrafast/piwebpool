1e1e92ba9620486857025b7183053907f521d21e
thomas <szemrot@hotmail.com>
2016-06-30 09:00:35 +0200

    Modified script.js

--- pool/pumpScheduler/js/script.js
+++ pool/pumpScheduler/js/script.js
@@ -187,13 +187,13 @@ function refreshValue(elem,action){
                 var tolerance;
                 if(action=='Ph'){
                     median=7.24;
-                    tolerance=0.01;
+
                 }else if (action=='ORP'){
                     median=715;
-                    tolerance=0.02;                        
+
                 }else{
                     median=25;
-                    tolerance=2;                        
+
                 }
                 var color=getColor(median,newValue);
                 //alert(color);
