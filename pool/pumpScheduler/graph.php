5add80b09e14350474c381d10bdb65aabc1efe71
thomas <szemrot@hotmail.com>
2016-06-30 17:00:49 +0200

    Modified graph.php

--- pool/pumpScheduler/graph.php
+++ pool/pumpScheduler/graph.php
@@ -28,29 +28,6 @@ $hint = array(
 
 
 
-function calculateTextBox($text,$fontFile,$fontSize,$fontAngle) {
-    /************
-    simple function that calculates the *exact* bounding box (single pixel precision).
-    The function returns an associative array with these keys:
-    left, top:  coordinates you will pass to imagettftext
-    width, height: dimension of the image you have to create
-    *************/
-    $rect = imagettfbbox($fontSize,$fontAngle,$fontFile,$text);
-    $minX = min(array($rect[0],$rect[2],$rect[4],$rect[6]));
-    $maxX = max(array($rect[0],$rect[2],$rect[4],$rect[6]));
-    $minY = min(array($rect[1],$rect[3],$rect[5],$rect[7]));
-    $maxY = max(array($rect[1],$rect[3],$rect[5],$rect[7]));
-   
-    return array(
-     "left"   => abs($minX) - 1,
-     "top"    => abs($minY) - 1,
-     "width"  => $maxX - $minX,
-     "height" => $maxY - $minY,
-     "box"    => $rect
-    );
-} 
-
-
 // STARTS HERE
 
 
@@ -116,30 +93,18 @@ switch ($_GET["type"]){
         // treat message in function (periode). ex: forecast
     default:
         if ($text_string==null) $text_string="unknown or undefined graph type ".$_GET["type"];
-        $font_ttf = './arial.ttf'; 
-        $font_size = 22; 
-        $text_angle        = 0; 
-        $text_padding    = 10; // Img padding - around text 
-        
-        $the_box        = calculateTextBox($text_string, $font_ttf, $font_size, $text_angle); 
-        
-        $imgWidth    = $cfg['width'] + $text_padding; 
-        $imgHeight    = $cfg['height'] + $text_padding; 
-        
-        $image = imagecreate($imgWidth,$imgHeight); 
-        imagefill($image, imagecolorallocate($image,240,240,240)); 
-        $color = imagecolorallocate($image,0,0,0); 
-        imagettftext($image, 
-            $font_size, 
-            $text_angle, 
-            $the_box["left"] + ($imgWidth / 2) - ($cfg['width'] / 2), 
-            $the_box["top"] + ($imgHeight / 2) - ($cfg['height'] / 2), 
-            $color, 
-            $font_ttf, 
-            $text_string); 
-        
-        imagegif($image); 
-        imagedestroy($image);
+        $img = imagecreate($cfg['width'], $cfg['height']);
+        // Transparent background
+        $black = imagecolorallocate($img, 0, 0, 0);
+        imagecolortransparent($img, $black);
+        // Red text
+        $fontsize = 5;
+        $red = imagecolorallocate($img, 255, 0, 0);
+        imagestring($img, $fontsize, 0, 0, utf8_decode($text), $red);
+    
+        imagepng($img);
+        imagedestroy($img);
+
     break;    
 }
 
