6590cfbfb626ec9a833a9750cd004367d53b1950
thomas <szemrot@hotmail.com>
2016-06-30 17:43:58 +0200

    Modified index.php

--- pool/pumpScheduler/index.php
+++ pool/pumpScheduler/index.php
@@ -390,7 +390,7 @@ $tableSettings = new TableGear($optionsSet);
     
     <tr>
         <th>OK<br><br><br></th>
-        <th><div id="weather"></div></th>
+        <div id="weather"></div>
         <th>OK</th>
     </tr>
 
