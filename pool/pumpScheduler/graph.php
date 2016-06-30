fa912ae52b81e45fec4f4acf78b5383ff2042cc4
thomas <szemrot@hotmail.com>
2016-06-30 10:54:33 +0200

    Modified graph.php

--- pool/pumpScheduler/graph.php
+++ pool/pumpScheduler/graph.php
@@ -79,7 +79,7 @@ switch ($_GET["type"]){
         $height = 480;
         $fontsize = 5;
     
-        $text=$hint[[$_GET["graph"][0]];
+        $text=$hint[$_GET["graph"][0]];
         
     
         $img = imagecreate($cfg['width'], $cfg['height']);
