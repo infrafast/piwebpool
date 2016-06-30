b71d8c7c39525c1cd17988ea468ccb7b53791e16
thomas <szemrot@hotmail.com>
2016-06-30 10:52:57 +0200

    Modified graph.php

--- pool/pumpScheduler/graph.php
+++ pool/pumpScheduler/graph.php
@@ -14,7 +14,7 @@ $cfg['background-color'] = "f0f0f0";
 
 $hint = array(
 			"pump"=>"Pump message",
-			"pac"=>"PAC message",
+			"pac"=> array("PAC message1","PAC message 2)",
 		    "treatment1"=>"treatment 1 message",
     	    "treatment2"=>"treamtemt 2 message",
 		    "ph"=>"ph message",
