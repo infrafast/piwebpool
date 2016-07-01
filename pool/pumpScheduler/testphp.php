8b8cf4d73544e0b0974fc9de7a1505f1498ac480
szemro <szemrot@hotmail.com>
2016-07-01 15:04:02 +0200

    Modified testphp.php

--- pool/pumpScheduler/testphp.php
+++ pool/pumpScheduler/testphp.php
@@ -1,28 +1,14 @@
-<?php
+ <?php
 
-require_once('functions.php');
+	require_once('functions.php');
 
-//sendemail("test");
-//sendsms("test");
-
-
-class workerThread extends Thread {
-public function __construct($i){
-  $this->i=$i;
-}
-
-public function run(){
-  while(true){
-   echo $this->i;
-   sleep(1);
-  }
-}
-}
-
-for($i=0;$i<50;$i++){
-$workers[$i]=new workerThread($i);
-$workers[$i]->start();
-}
-
+    //sendemail("test");
+    //sendsms("test");
+    $dev=array();
+    $dev=getDeviceFromFile();
+    if ($dev!=null){
+        echo "devices are:\n".$dev;
+        
+    }else echo "\nerror";
 
 ?>
