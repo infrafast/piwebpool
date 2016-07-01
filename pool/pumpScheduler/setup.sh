3b31dcf70ecdb9186e3d0662de8184f52e0a0253
szemro <szemrot@hotmail.com>
2016-07-01 10:06:03 +0200

    Modified setup.sh

--- pool/pumpScheduler/setup.sh
+++ pool/pumpScheduler/setup.sh
@@ -8,3 +8,11 @@ mysql -uroot -p Quintal74605 pool < ./sql/scripts.sql
 mysql -uroot -p Quintal74605 pool < ./sql/settings.sql
 
 #file permission and configuration
+sudo chmod 774 /etc/ssmtp/ssmtp.conf
+sudo usermod -a -G webide www-data
+chmod 0775 css js
+
+#periodic execution of the script
+ln -s /var/www/html/pumpScheduler/hourlycrontab.sh /etc/cron.hourly/pumpScheduler
+#this is to access the ttyUSB0 from apache
+sudo usermod -a -G dialout www-data
