1fd01ef8beadf3a5d56488ff3989b6d22c31e3f6
szemro <szemrot@hotmail.com>
2016-07-01 09:56:11 +0200

    Modified setup.sh

--- pool/pumpScheduler/setup.sh
+++ pool/pumpScheduler/setup.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-mysql -uroot -p Quintal74605 pool < ./sql/
-mysql -uroot -p Quintal74605 pool < ./sql/
-mysql -uroot -p Quintal74605 pool < ./sql/
-mysql -uroot -p Quintal74605 pool < ./sql/
+mysql -uroot -p Quintal74605 pool < ./sql/create.sql
+mysql -uroot -p Quintal74605 pool < ./sql/measures.sql
+mysql -uroot -p Quintal74605 pool < ./sql/pumpSchedule.sql
+mysql -uroot -p Quintal74605 pool < ./sql/scripts.sql
+mysql -uroot -p Quintal74605 pool < ./sql/settings.sql
