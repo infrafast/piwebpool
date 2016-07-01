89d27498899150f3fa6938f836f04347a4ad19d2
szemro <szemrot@hotmail.com>
2016-07-01 10:19:00 +0200

    Modified setup.sh

--- pool/pumpScheduler/setup.sh
+++ pool/pumpScheduler/setup.sh
@@ -24,6 +24,6 @@ sudo chmod 774 /etc/ssmtp/ssmtp.conf
 sudo usermod -a -G webide www-data
 chmod 0775 css js
 #periodic execution of the script
-ln -s /var/www/html/pumpScheduler/hourlycrontab.sh /etc/cron.hourly/pumpScheduler
+sudo ln -s /var/www/html/pumpScheduler/hourlycrontab.sh /etc/cron.hourly/pumpScheduler
 #this is to access the ttyUSB0 from apache
 sudo usermod -a -G dialout www-data
