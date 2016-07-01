57fc3ebc4e218c93818bfa46500b4acfd2807614
szemro <szemrot@hotmail.com>
2016-07-01 15:04:14 +0200

    Modified functions.php

--- pool/pumpScheduler/functions.php
+++ pool/pumpScheduler/functions.php
@@ -5,6 +5,28 @@ require "include/FreeSMS.php";
 include("include/php_serial.class.php");
 include_once('include/phpMyGraph5.0.php'); 
 
+/*
+wiringPi
+Pin 	BCM
+GPIO 	Name 	Header 	Name 	BCM
+GPIO 	wiringPi
+Pin
+– 	– 	3.3v 	1 | 2 	5v 	– 	–
+8 	0 	SDA0 	3 | 4 	DNC 	– 	–
+9 	1 	SCL0 	5 | 6 	0v 	– 	–
+7 	4 	GPIO 7 	7 | 8 	TxD 	14 	15
+– 	– 	DNC 	9 | 10 	RxD 	15 	16
+0 	17 	GPIO 0 	11 | 12 	GPIO 1 	18 	1
+2 	21 	GPIO 2 	13 | 14 	DNC 	– 	–
+3 	22 	GPIO 3 	15 | 16 	GPIO 4 	23 	4
+– 	– 	DNC 	17 | 18 	GPIO 5 	24 	5
+12 	10 	MOSI 	19 | 20 	DNC 	– 	–
+13 	9 	MISO 	21 | 22 	GPIO 6 	25 	6
+14 	11 	SCLK 	23 | 24 	CE0 	8 	10
+– 	– 	DNC 	25 | 26 	CE1 	7 	11
+*/
+
+
 
 function sendsms($message){
     $SMSuser = "19107501";
@@ -381,29 +403,16 @@ function split_sql_file($sql, $delimiter)
    return $output;
 }
 
-
-
-/*
-wiringPi
-Pin 	BCM
-GPIO 	Name 	Header 	Name 	BCM
-GPIO 	wiringPi
-Pin
-– 	– 	3.3v 	1 | 2 	5v 	– 	–
-8 	0 	SDA0 	3 | 4 	DNC 	– 	–
-9 	1 	SCL0 	5 | 6 	0v 	– 	–
-7 	4 	GPIO 7 	7 | 8 	TxD 	14 	15
-– 	– 	DNC 	9 | 10 	RxD 	15 	16
-0 	17 	GPIO 0 	11 | 12 	GPIO 1 	18 	1
-2 	21 	GPIO 2 	13 | 14 	DNC 	– 	–
-3 	22 	GPIO 3 	15 | 16 	GPIO 4 	23 	4
-– 	– 	DNC 	17 | 18 	GPIO 5 	24 	5
-12 	10 	MOSI 	19 | 20 	DNC 	– 	–
-13 	9 	MISO 	21 | 22 	GPIO 6 	25 	6
-14 	11 	SCLK 	23 | 24 	CE0 	8 	10
-– 	– 	DNC 	25 | 26 	CE1 	7 	11
-*/
-
+function getDeviceFromFile(){
+    if ($data = file('USBdevices.id')){
+        $returnArray = array();
+        foreach($data as $line) {
+            $explode = explode(":", $line);
+            $returnArray[$explode[0]] = $explode[1];
+        }
+        return $returnArray;
+    }else return null;
+}
 
 ?>
 
