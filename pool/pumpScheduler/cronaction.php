f9eae1db06945f502c52dd4bfbd4960e81696ea8
szemro <szemrot@hotmail.com>
2016-07-01 15:55:25 +0200

    Modified cronaction.php

--- pool/pumpScheduler/cronaction.php
+++ pool/pumpScheduler/cronaction.php
@@ -116,7 +116,11 @@ if (!$result) {
             
             $phValue = getPh();
             $orpValue = getORP();
-            $temperatureValue = getTemperature();    
+            $temperatureValue = getTemperature();
+            if($phValue==null)  $phValue=-99;
+            if($orpValue==null)  $orpValue=-99;
+            if($temperatureValue==null)  $temperatureValue=-99;
+            
 
             $sql = "INSERT INTO `measures` (`id`, `timestamp`, `orp`, `ph`, `temperature`";
             foreach($materials as $material=>$pin) $sql = $sql.", `".$materialsColumn[$material]."`";
