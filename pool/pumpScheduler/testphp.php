e2a92546e972a5e8b896a84ac0e84954f0002453
szemro <szemrot@hotmail.com>
2016-07-02 13:58:41 +0200

    Modified testphp.php

--- pool/pumpScheduler/testphp.php
+++ pool/pumpScheduler/testphp.php
@@ -4,9 +4,10 @@
 
     //sendemail("test");
     //sendsms("test");
-    $id="ph";
-    $dev=getDeviceZ($id);
-    if ($dev!=null) echo $id."=".$dev;
-    else echo "\nerror";
+    //$id="ph";
+    //$dev=getDeviceZ($id);
+    //if ($dev!=null) echo $id."=".$dev;
+    //else echo "\nerror";
+    echo getTemperature();
 
 ?>
