78b9638da00ded985241dcffdffd2efc10359d5f
szemro <szemrot@hotmail.com>
2016-07-04 14:33:23 +0000

    Modified action.php

--- pool/action.php
+++ pool/action.php
@@ -200,7 +200,7 @@ switch($_['action']){
 	    if (isset($_GET['SwitchFilterPCL'])){
 	        $_['action'] = "PCL";
             setPinState($pins[$materials["filtration"]],$_GET['SwitchFilterPCL']);
-            $result['state']  =  getPin($pins[$materials["filtration"]]); //idem as getFilterStatePCL;
+            $result['state'] = getPin($pins[$materials["filtration"]]); //idem as getFilterStatePCL;
 	    }else if (isset($_GET['switchTreatment1PCL'])){
 	        $_['action'] = "PCL";
 	        
