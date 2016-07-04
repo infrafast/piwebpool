e1f3aaace9aede43572f16b026b95c01f18a54d8
szemro <szemrot@hotmail.com>
2016-07-04 14:06:35 +0000

    Modified action.php

--- pool/action.php
+++ pool/action.php
@@ -205,7 +205,7 @@ switch($_['action']){
 	        
 	    }else if (isset($_GET['getFilterStatePCL'])){
 	        $_['action'] = "PCL";
-	        $result['state']  = $pins[$materials["traitement1"]]; //getPin();
+	        $result['state']  = $pins[$materials["traitement2"]]; //getPin();
     	}else{    
         	$result['answer']  = "ERROR";
     		
