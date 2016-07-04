9a0889e791c09046344093700c90168b303487cb
szemro <szemrot@hotmail.com>
2016-07-04 14:43:36 +0000

    Modified action.php

--- pool/action.php
+++ pool/action.php
@@ -201,7 +201,7 @@ switch($_['action']){
 	        $_['action'] = "PCL";
             setPinState($pins[$materials["filtration"]],$_GET['SwitchFilterPCL']);
             $result['state'] = getPin($pins[$materials["filtration"]]); //idem as getFilterStatePCL;
-            echo "<state>1</state>";exit;
+            echo "";exit;
 	    }else if (isset($_GET['switchTreatment1PCL'])){
 	        $_['action'] = "PCL";
 	        
