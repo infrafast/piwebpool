e873ac87f3bcfb089b2858ae901f1481eee328d7
szemro <szemrot@hotmail.com>
2016-07-04 08:54:11 +0000

    Modified functions.php

--- pool/pumpScheduler/functions.php
+++ pool/pumpScheduler/functions.php
@@ -168,8 +168,7 @@ function getDevice($id){
 
 
 function getTemperature(){
-    // must return false  if wrong value
-    //return rand(30,32);
+    return rand(30,32);
     for ($i = 0; $i < 2; $i++){
         $v1 = round(readSensor(getDevice("temp")), 1,PHP_ROUND_HALF_UP);  
         if ($v1>0 and $v1<35) return $v1;
@@ -179,7 +178,7 @@ function getTemperature(){
 
 // use "I" command to determine where PH and ORP and TEMP sensors are connected ttyUSB
 function getPh(){
-    //return round( (0 + (10 - 0) * (mt_rand() / mt_getrandmax())), 1, PHP_ROUND_HALF_UP);
+    return round( (0 + (10 - 0) * (mt_rand() / mt_getrandmax())), 1, PHP_ROUND_HALF_UP);
     for ($i = 0; $i < 2; $i++){
         $v1 = round(readSensor(getDevice("ph")), 2,PHP_ROUND_HALF_UP);  
         if ($v1>0 and $v1<10) return $v1;
@@ -188,7 +187,7 @@ function getPh(){
 }
 
 function getORP(){
-    //return intval(rand(0,1000));
+    return intval(rand(0,1000));
     for ($i = 0; $i < 2; $i++){
         $v1 = intval(readSensor(getDevice("orp")));      
         if ($v1>0 and $v1<1000) return $v1;
