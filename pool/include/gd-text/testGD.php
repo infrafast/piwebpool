582b22bc890c6a4393fa9f0ef77803db5f66f5ec
szemro <szemrot@hotmail.com>
2016-07-05 09:07:49 +0000

    Modified testGD.php

--- pool/include/gd-text/testGD.php
+++ pool/include/gd-text/testGD.php
@@ -21,23 +21,5 @@ $box->setBox(20, 20, 460, 460);
 $box->setTextAlign('left', 'top');
 $box->draw("Franchise\nBold");
 
-$box = new Box($im);
-$box->setFontFace(__DIR__.'/Pacifico.ttf'); // http://www.dafont.com/pacifico.font
-$box->setFontSize(80);
-$box->setFontColor(new Color(255, 255, 255));
-$box->setTextShadow(new Color(0, 0, 0, 50), 0, -2);
-$box->setBox(20, 20, 460, 460);
-$box->setTextAlign('center', 'center');
-$box->draw("Pacifico");
-
-$box = new Box($im);
-$box->setFontFace(__DIR__.'/Prisma.otf'); // http://www.dafont.com/prisma.font
-$box->setFontSize(70);
-$box->setFontColor(new Color(148, 212, 1));
-$box->setTextShadow(new Color(0, 0, 0, 50), 0, -2);
-$box->setBox(20, 20, 460, 460);
-$box->setTextAlign('right', 'bottom');
-$box->draw("Prisma");
-
 header("Content-type: image/png");
 ?>
