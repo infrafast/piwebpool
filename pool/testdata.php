066a84604bd7f2f0330e0fdbf989e7d6cd245fb7
szemro <szemrot@hotmail.com>
2016-07-05 18:06:09 +0200

    Modified testdata.php

--- pool/testdata.php
+++ pool/testdata.php
@@ -38,8 +38,8 @@ for ($x = 0; $x <= 128; $x++) {
             $state.="+".mysql_error();
         }else{
             
-            $phValue = getPh()-($x/10);
-            $orpValue = getORP()-($x*20);
+            $phValue = getPh();//-($x/10);
+            $orpValue = getORP();//-($x*20);
             $temperatureValue = getTemperature();
     		$pinVal=getVal();
     		
