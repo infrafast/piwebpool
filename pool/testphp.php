fa1b79d8655855a06c76a5605d4e5801f5a20b1e
szemro <szemrot@hotmail.com>
2016-07-05 13:10:42 +0200

    Modified testphp.php

--- pool/testphp.php
+++ pool/testphp.php
@@ -2,15 +2,44 @@
 
 	require_once('functions.php');
 
-    //sendemail("test");
-    //sendsms("test");
-    //$id="ph";
-    //$dev=getDeviceZ($id);
-    //if ($dev!=null) echo $id."=".$dev;
-    //else echo "\nerror";
-    echo getTemperature()."\n";
-    echo getORP()."\n";
-    echo getPh()."\n";
-    
- 
+$PopulationOfTexas = array(
+    1999 => 20.56, // in millions
+    2000 => 21.56,
+    2001 => 22.56,
+    2002 => 23.56
+);
+
+//generate an array sohwing the difference in each year compared to the previous year
+$differneces = array();
+$lastyear = null;
+foreach($PopulationOfTexas as $k=>$v){
+    if(empty($lastyear)){$lastyear = $v; continue;}
+    $differneces[$k] = $v - $lastyear;
+    $lastyear = $v;
+
+    //use this later
+    $lastitem = array("year"=>$k, "data"=>$v);
+}
+
+//get the average difference per year
+$count = 0;
+$total = 0;
+foreach($differneces as $k=>$v){
+    $count++;
+    $total += $v;
+}
+
+$average = number_format(($total/$count), 2);
+
+//make a prediction
+$predictions = array();
+for($i=0;$i<5;$i++){
+    $year = isset($year) ? $year+1 : $lastitem["year"]+1;
+    $prediction = isset($prediction) ? $prediction+floatval($average) : $lastitem["data"]+floatval($average);
+    $predictions[$year] = $prediction;
+}
+
+print_r($predictions);
+
+
 ?>
