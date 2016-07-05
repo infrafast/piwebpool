eb1cc1604ca8130a20bc91b821f0439bc7c29557
szemro <szemrot@hotmail.com>
2016-07-05 13:59:44 +0200

    Modified graph.php

--- pool/graph.php
+++ pool/graph.php
@@ -86,8 +86,8 @@ $data = array();
 while ($row = mysql_fetch_assoc($result)){
     $date = strtotime($row['timeStamp']);
     $hour = date('DH', $date);    
-    //$data[$row['timeStamp']] = $row[$_GET["graph"]];
-    $data[$hour] = $row[$_GET["graph"]];    
+    $data[$row['timeStamp']] = $row[$_GET["graph"]];
+    //$data[$hour] = $row[$_GET["graph"]];    
    // echo "<br>".$row['timeStamp']." ". $row[$_GET["graph"]]." ".$hour;
 }
 //exit;
