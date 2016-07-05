662cd2f047cc5b7c4ced375f6f15c92cb82cda70
szemro <szemrot@hotmail.com>
2016-07-05 11:50:18 +0200

    Modified testGD.php

--- pool/include/gd-text/testGD.php
+++ pool/include/gd-text/testGD.php
@@ -8,19 +8,19 @@ require "src/VerticalAlignment.php";
 
 use GDText\Box;
 use GDText\Color;
+
 $im = imagecreatetruecolor(500, 500);
 $backgroundColor = imagecolorallocate($im, 0, 18, 64);
 imagefill($im, 0, 0, $backgroundColor);
 
 $box = new Box($im);
-$box->setFontFace('arial.ttf'); // http://www.dafont.com/pacifico.font
-$box->setFontSize(80);
-$box->setFontColor([255, 255, 255]);
-$box->setTextShadow([0, 0, 0, 50], 0, -2);
-$box->setLeading(0.7);
+$box->setFontFace(__DIR__.'/Franchise-Bold-hinted.ttf'); // http://www.dafont.com/franchise.font
+$box->setFontColor(new Color(255, 75, 140));
+$box->setTextShadow(new Color(0, 0, 0, 50), 2, 2);
+$box->setFontSize(40);
 $box->setBox(20, 20, 460, 460);
-$box->setTextAlign('center', 'center');
-$box->draw("Pacifico");
+$box->setTextAlign('left', 'top');
+$box->draw("Franchise\nBold");
 
 header("Content-type: image/png");
 imagepng($im);
