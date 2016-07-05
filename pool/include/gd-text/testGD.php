8df579b13e0a88fbe733010109f168de13f5b421
szemro <szemrot@hotmail.com>
2016-07-05 09:06:12 +0000

    Modified testGD.php

---
+++ pool/include/gd-text/testGD.php
@@ -0,0 +1,39 @@
+<?php
+require __DIR__.'/../vendor/autoload.php';
+
+use GDText\Box;
+use GDText\Color;
+
+$im = imagecreatetruecolor(500, 500);
+$backgroundColor = imagecolorallocate($im, 0, 18, 64);
+imagefill($im, 0, 0, $backgroundColor);
+
+$box = new Box($im);
+$box->setFontFace(__DIR__.'/Franchise-Bold-hinted.ttf'); // http://www.dafont.com/franchise.font
+$box->setFontColor(new Color(255, 75, 140));
+$box->setTextShadow(new Color(0, 0, 0, 50), 2, 2);
+$box->setFontSize(40);
+$box->setBox(20, 20, 460, 460);
+$box->setTextAlign('left', 'top');
+$box->draw("Franchise\nBold");
+
+$box = new Box($im);
+$box->setFontFace(__DIR__.'/Pacifico.ttf'); // http://www.dafont.com/pacifico.font
+$box->setFontSize(80);
+$box->setFontColor(new Color(255, 255, 255));
+$box->setTextShadow(new Color(0, 0, 0, 50), 0, -2);
+$box->setBox(20, 20, 460, 460);
+$box->setTextAlign('center', 'center');
+$box->draw("Pacifico");
+
+$box = new Box($im);
+$box->setFontFace(__DIR__.'/Prisma.otf'); // http://www.dafont.com/prisma.font
+$box->setFontSize(70);
+$box->setFontColor(new Color(148, 212, 1));
+$box->setTextShadow(new Color(0, 0, 0, 50), 0, -2);
+$box->setBox(20, 20, 460, 460);
+$box->setTextAlign('right', 'bottom');
+$box->draw("Prisma");
+
+header("Content-type: image/png");
+?>
