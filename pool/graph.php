8beb95d7fcb4479e19f00a6965de263896d96e51
szemro <szemrot@hotmail.com>
2016-07-05 14:00:59 +0200

    Modified graph.php

--- pool/graph.php
+++ pool/graph.php
@@ -73,6 +73,7 @@ if (!mysql_select_db($options["database"]["name"], $link)) {
 // use order by timeStamp to really get the last value but they are aggregated by day...
 //$sql = "select ".$_GET["graph"].",id, timeStamp from (select ".$_GET["graph"].", id, timeStamp from measures order by timeStamp desc limit ".intval($_GET["period"]).") tempTable order by timeStamp asc";
 // use order by id for test purpose
+// *******
 $sql = "select ".$_GET["graph"].",id, timeStamp from (select ".$_GET["graph"].", id, timeStamp from measures order by id desc limit ".intval($_GET["period"]).") tempTable order by id asc";
 //echo $sql; exit;
 $result = mysql_query($sql, $link);
@@ -87,6 +88,7 @@ while ($row = mysql_fetch_assoc($result)){
     $date = strtotime($row['timeStamp']);
     $hour = date('DH', $date);    
     $data[$row['timeStamp']] = $row[$_GET["graph"]];
+    // *******
     //$data[$hour] = $row[$_GET["graph"]];    
    // echo "<br>".$row['timeStamp']." ". $row[$_GET["graph"]]." ".$hour;
 }
