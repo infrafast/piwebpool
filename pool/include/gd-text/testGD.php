af860538f0a3ae091d93291f69d5f7621daf2ed3
szemro <szemrot@hotmail.com>
2016-07-05 11:22:52 +0200

    Modified testGD.php

--- pool/include/gd-text/testGD.php
+++ pool/include/gd-text/testGD.php
@@ -14,7 +14,7 @@ imagefill($im, 0, 0, $backgroundColor);
 
 $box = new Box($im);
 putenv('GDFONTPATH=' . realpath('.'));
-$box->setFontFace('arial.ttf'); // http://www.dafont.com/franchise.font
+$box->setFontFace('Roboto-Regular.ttf'); // http://www.dafont.com/franchise.font
 $box->setFontColor(new Color(255, 75, 140));
 $box->setTextShadow(new Color(0, 0, 0, 50), 2, 2);
 $box->setFontSize(40);
