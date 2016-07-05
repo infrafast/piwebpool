004867893667654795e86cac218a769546f6ec01
szemro <szemrot@hotmail.com>
2016-07-05 13:32:13 +0200

    Modified graph.php

--- pool/graph.php
+++ pool/graph.php
@@ -115,7 +115,7 @@ switch ($_GET["type"]){
     case "textType":
         $text=$hint[$_GET["graph"]][0];
         $text.="\nLes stats sont effectuées sur la base de ".$_GET["period"];
-        $text.="\nTrend:".getTrend(array_column($data,1));
+        $text.="\nTrend:".getTrend();
         // treat message in function (periode). ex: forecast
         // $data contains all info
         
