095e3c177ee469b64d1798be3751d52656f5db81
szemro <szemrot@hotmail.com>
2016-07-05 07:57:48 +0000

    Modified testphp.php

---
+++ pool/testphp.php
@@ -0,0 +1,16 @@
+ <?php
+
+	require_once('functions.php');
+
+    //sendemail("test");
+    //sendsms("test");
+    //$id="ph";
+    //$dev=getDeviceZ($id);
+    //if ($dev!=null) echo $id."=".$dev;
+    //else echo "\nerror";
+    echo getTemperature()."\n";
+    echo getORP()."\n";
+    echo getPh()."\n";
+    
+ 
+?>
