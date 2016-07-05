cd381fcac151d0759188cec0c4c7d1330e0ec8a7
szemro <szemrot@hotmail.com>
2016-07-05 12:09:37 +0200

    Modified graph.php

--- pool/graph.php
+++ pool/graph.php
@@ -110,8 +110,8 @@ switch ($_GET["type"]){
 
         $box = new Box($im);
         $box->setFontFace('./fonts/Roboto-Regular.ttf'); // http://www.dafont.com/franchise.font
-        $box->setFontColor(new Color(200, 200, 200));
-        $box->setTextShadow(new Color(0, 0, 0, 50), 2, 2);
+        $box->setFontColor(new Color(0, 0, 0, 50));
+        $box->setTextShadow(new Color(200, 200, 200), 2, 2);
         $box->setFontSize(15);
         $box->setBox(20, 20, 460, 460);
         $box->setTextAlign('left', 'top');
