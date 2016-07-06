c267d921bb6d6cfbbddab17f92f00d12c7980fe5
szemro <szemrot@hotmail.com>
2016-07-06 17:34:23 +0200

    Modified testdata.php

--- pool/testdata.php
+++ pool/testdata.php
@@ -52,7 +52,7 @@ function generate($period,$phv1,$phv2,$orpv1,$orpv2,$link,$materialsColumn,$mate
                 echo mysql_error(); exit;
             }else{
                 
-                $phValue=$phStep*$x+($phv1+(rand(0,5)<2?0:rand(-0.5,0.5)));
+                $phValue=$phStep*$x+($phv1+(rand(0,5)<2?0:rand(-0.8,0.8)));
                 $orpValue=$orpStep*$x+($orpv1+(rand(0,5)<2?0:intval(rand(-$orpValue*0.01,$orpValue*0.05))));
                 
                 $temperatureValue = getTemperature();
