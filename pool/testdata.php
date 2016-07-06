3c867b6bc1a7bc53ce9fce6bfaa0415f47358fd8
szemro <szemrot@hotmail.com>
2016-07-06 15:48:56 +0200

    Modified testdata.php

--- pool/testdata.php
+++ pool/testdata.php
@@ -14,7 +14,7 @@ if (!mysql_select_db($options["database"]["name"], $link)) {
 } 
 
 //increase
-generate(24,1,3,0.3,1,300,10,$link,$materialsColumn,$materials,$pins);
+generate(24,2,2.5,0.3,1,300,10,$link,$materialsColumn,$materials,$pins);
 //decrease
 
 //stable
