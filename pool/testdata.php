e0ceb0bcc759b11ea4e2a149f4b733a0dc961def
szemro <szemrot@hotmail.com>
2016-07-06 15:43:12 +0200

    Modified testdata.php

--- pool/testdata.php
+++ pool/testdata.php
@@ -29,9 +29,9 @@ generate(24,1,3,0.3,1,300,10,$link,$materialsColumn,$materials,$pins);
 
 function generate($period,$phv1,$phv2,$phnoise,$orpv1,$orpv2,$orpnoise,$link,$materialsColumn,$materials,$pins){
     $phStep = ($phv2 - $phv1)/$period;
-    $phValue = $phv1;
+    $phValue = 0;
     $orpStep = ($orpv2 - $orpv1)/$period;
-    $orpValue = $orpv1;
+    $orpValue = 0;
     
     for ($x = 0; $x <= $period; $x++) {
         $sql    = "SELECT value from settings where id='measureIndex';";
