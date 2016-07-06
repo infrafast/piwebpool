94ec72c557ce62faec1084ef1834291b3880bce7
szemro <szemrot@hotmail.com>
2016-07-06 17:44:06 +0200

    Modified testdata.php

--- pool/testdata.php
+++ pool/testdata.php
@@ -53,7 +53,7 @@ function generate($period,$phv1,$phv2,$orpv1,$orpv2,$link,$materialsColumn,$mate
             }else{
                 
                 $phValue=($phStep*$x)+$phv1;
-                $phvalue=rand($phvalue*0.7,$phvalue*1.5);
+                $phvalue= round( (($phvalue*0.7) + (($phvalue*1.5) - ($phvalue*0.7)) * (mt_rand() / mt_getrandmax())), 1, PHP_ROUND_HALF_UP);
                 $orpValue=($orpStep*$x)+($orpv1+(rand(0,8)<3?0:intval(rand(-$orpValue*0.01,$orpValue*0.05))));
                 
                 $temperatureValue = getTemperature();
