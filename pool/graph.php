99b428a830b3dc1d0f34e9360ebac36231c16e73
szemro <szemrot@hotmail.com>
2016-07-06 18:14:55 +0200

    Modified graph.php

--- pool/graph.php
+++ pool/graph.php
@@ -225,8 +225,6 @@ switch ($_GET["type"]){
 
         $avg=($avg+$values[count($values)-1])/2;
         // we ponderate the average by re-avergaring with the last measure to make sure the instant value is not falsing the overall estimation
-
-
         $diffVal = $avg-$reference;
         $ecartVal = $diffVal/$reference;
         $currentValueIndicator = "correct"; 
