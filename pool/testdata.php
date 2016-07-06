cec99fe3967557ade22f2722a999c98e94613815
szemro <szemrot@hotmail.com>
2016-07-06 15:44:05 +0200

    Modified testdata.php

--- pool/testdata.php
+++ pool/testdata.php
@@ -53,7 +53,7 @@ function generate($period,$phv1,$phv2,$phnoise,$orpv1,$orpv2,$orpnoise,$link,$ma
                 $state.="+".mysql_error();
             }else{
                 
-                $phValue+=$phStep+round( (0.0 + ($phnoise - 0.0) * (mt_rand() / mt_getrandmax())), 1, PHP_ROUND_HALF_UP);;
+                $phValue+=$phStep+round( (-$phnoise + ($phnoise - (-$phnoise)) * (mt_rand() / mt_getrandmax())), 1, PHP_ROUND_HALF_UP);;
                 $orpValue+=$orpStep+intval(rand(0,$orpnoise));
                 
                 $temperatureValue = getTemperature();
