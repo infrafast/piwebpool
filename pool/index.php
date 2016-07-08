7b71c21a698c7630793c8ac422d855b1601d9b31
infrafast <szemrot@hotmail.com>
2016-07-08 15:14:46 +0000

    Modified index.php

--- pool/index.php
+++ pool/index.php
@@ -195,7 +195,7 @@ $tableSettings = new TableGear($optionsSet);
         // collapse all table as per settings stored in the database
         var collapsableTableList = ['actionTable','Planificateur','sensorTable','blocklyTable','logTable','Parametres'];
         for (var tableID in collapsableTableList) {
-          if (actionCall('action=getSetting&id='+collapsableTableList[tableID],false,"table:"+collapsableTableList[tableID],true)=="1") 
+          if (actionCall('action=getSetting&id='+collapsableTableList[tableID],false)=="1") 
             document.getElementById(collapsableTableList[tableID]).click();
         }
     
