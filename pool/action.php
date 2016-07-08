99b1ef5281476a03e6c0ef664b8329f32d8a86fc
infrafast <szemrot@hotmail.com>
2016-07-08 12:47:49 +0000

    Modified action.php

--- pool/action.php
+++ pool/action.php
@@ -241,8 +241,9 @@ if(isset($_['action'])){
     $result['state'] = getPin($pins[$materials["traitement1"]]);
 ////////// GET
 }else if (isset($_GET['getStatePLC'])){
-    $material = $_GET['getStatePLC'];
-    $result['state'] = getPin($pins[$materials[$material]]);
+    $_['material'] = $_GET['getStatePLC'];
+    getState($_,$materials,$result);
+    //$result['state'] = getPin($pins[$materials[$material]]);
 }else{   
 	$result['answer']  = "ERROR";
 	$result['state'] = 'Undefined call '.$param;
