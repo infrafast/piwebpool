65bc12fa0b6560a90f5ab9a9187880040e3af2cf
infrafast <szemrot@hotmail.com>
2016-07-08 15:48:14 +0000

    Modified pull.sh

--- pySerial/pull.sh
+++ pySerial/pull.sh
@@ -2,13 +2,3 @@
 awk '
 /EOF/ {exit;}
  {print;}' < /dev/ttyUSB2 > usb2    
-
-
-
-while read line; do
-    if [ "$line" != "toto" ]; then
-        echo "$line" > usb2
-    else
-        break
-    fi
-done < /dev/ttyUSB2
