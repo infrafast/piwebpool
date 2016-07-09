cfb6d218b6e4e53f2f6add6a457f4697053003ab
infrafast <szemrot@hotmail.com>
2016-07-09 08:50:19 +0000

    Modified cronaction.php

--- pool/cronaction.php
+++ pool/cronaction.php
@@ -90,7 +90,6 @@ if (!$result) {
                 while ($row = mysql_fetch_assoc($result)) $luaCode=htmlspecialchars_decode(($row['lua']));
                 mysql_free_result($result);
             }
-            appendlualog($luaCode);
             // call lua execution built from Header + Content + Footer and passing the access to the pins so they can be manipulated by lua code
             goLua($concat[0].$luaCode.$concat[1],$materials,$pins,$luaFeedback);
         }
