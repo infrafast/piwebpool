f5854f6b152b897599dbf59c87852e6de6e4fa46
infrafast <szemrot@hotmail.com>
2016-07-09 07:56:31 +0000

    Modified index.php

--- pool/index.php
+++ pool/index.php
@@ -414,7 +414,7 @@ $tableSettings = new TableGear($optionsSet);
             var calibrateID=id+"CalibrateID";
             var calibrate=document.getElementById(calibrateID);
             calibrate.style.backgroundImage="url('images/loading.gif')";
-            if(actionCall('action=calibrate&id='+id,false,'Placer la sonde dans la solution\npendant 2 minutes puis confirmez',false, true)!=false)
+            if(actionCall('action=calibrate&id='+id+'&value='+calVal,false,'Placer la sonde dans la solution\npendant 2 minutes puis confirmez',false, true)!=false)
                 elem.click();
             //remove loading icon
             calibrate.style.backgroundImage="url('')";
