46d08ce5997fdf462c01d31b7154f6483fe231fe
infrafast <szemrot@hotmail.com>
2016-07-09 08:43:17 +0000

    Modified action.php

--- pool/action.php
+++ pool/action.php
@@ -126,7 +126,7 @@ if(isset($_['action'])){
         case 'updateScript':
             mysql_connect($options["database"]["host"],$options["database"]["username"],$options["database"]["password"]) or die('error connection');
             mysql_select_db($options["database"]["name"]) or die('error database selection');
-            $cleanLuaCode="if (coucou) > 15 then\n  set(traitement1,(1));\n  email(\'qualité de l\\\'eau\');\n  log(\'ok\');\nend\n"; //mysql_real_escape_string(htmlspecialchars_decode($_['lua']));
+            $cleanLuaCode="if (coucou) > 15 then quaité de l'eau"; //mysql_real_escape_string(htmlspecialchars_decode($_['lua']));
             $cleanXMLCode=mysql_real_escape_string(htmlspecialchars_decode($_['xml']));
             $query="UPDATE `scripts` SET `xml` = '".$cleanXMLCode."',`lua`='".$cleanLuaCode."' WHERE `id`='".$_['id']."'";
             
