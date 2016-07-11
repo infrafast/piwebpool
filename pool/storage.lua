c6fbff3eee952390cfbb774ae3fab60f944411dd
infrafast <szemrot@hotmail.com>
2016-07-11 22:56:40 +0200

    Modified storage.lua

--- pool/storage.lua
+++ pool/storage.lua
@@ -6,12 +6,7 @@ local obj1 = {
 	[0] = 0;
 	[1] = 0;
 	[2] = 0;
-	[3] = 0;
-	[4] = 0;
-	[5] = 0;
-	[6] = 0;
-	[7] = 0;
-	[8] = 0;	
-	[9] = 0;		
+	memoire["temperatureFaible"] = 0;
+	memoire["gel"] = 0;
 }
 return obj1
