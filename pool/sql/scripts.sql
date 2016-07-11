7171995993a89342a39ee2b285897d700d7d64b5
infrafast <szemrot@hotmail.com>
2016-07-11 15:52:09 +0200

    Modified scripts.sql

--- pool/sql/scripts.sql
+++ pool/sql/scripts.sql
@@ -10,10 +10,8 @@ INSERT INTO `scripts` (`id`, `xml`, `lua`) VALUES
 ('main', '<xml xmlns="http://www.w3.org/1999/xhtml"><block type="controls_if" id="#k:`8J.D:{zzYDg00AnU" x="109" y="30"><value name="IF0"><block type="logic_compare" id="-92_1,Ve7hB!5Q.,VRKh"><field name="OP">GT</field><value name="A"><block type="sensors" id="#lk|+Xd;OpMg*=]))P}s"><field name="select">temperature</field></block></value><value name="B"><block type="math_number" id="wNzqwnx3yZzR=IGMAM1*"><field name="NUM">15</field></block></value></block></value><statement name="DO0"><block type="setcommand" id="bh90jirdqe!b.NXEf9?B"><field name="command">traitement1</field><value name="NAME"><block type="on_off" id="UIbvnLJ%MRvmz07jC%pw"><field name="command">1</field></block></value></block></statement></block></xml>', 'if (temperature) > 15 then\n  set(traitement1,(1));\nend\n'
 );
 
-INSERT INTO INTO `scripts` (`id`, `xml`, `lua`) VALUES 
-('header', '', LOAD_FILE('header.lua'),
-('header', '', LOAD_FILE('footer.lua'),
-);
+insert into scripts (id,xml,lua) values ('header','',LOAD_FILE('/usr/share/adafruit/webide/repositories/piweb/pool/sql/header.lua'));
+insert into scripts (id,xml,lua) values ('footer','',LOAD_FILE('/usr/share/adafruit/webide/repositories/piweb/pool/sql/footer.lua'));
 
 ALTER TABLE `scripts`
  ADD PRIMARY KEY (`id`);
