5543ba4c327fbbed01aed1cc08ca5d4e283db9cb
infrafast <szemrot@hotmail.com>
2016-07-11 17:13:04 +0200

    Modified storage.lua

--- pool/storage.lua
+++ pool/storage.lua
@@ -4,11 +4,14 @@ local multiRefObjects = {
 } -- multiRefObjects
 local obj1 = {
 	[0] = 0;
-	[2] = 0;
 	[1] = 0;
+	[2] = 0;
 	[3] = 0;
 	[4] = 0;
 	[5] = 0;
-	[6] = 0;	
+	[6] = 0;
+	[7] = 0;
+	[8] = 0;	
+	[9] = 0;		
 }
 return obj1
