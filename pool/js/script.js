b0abbe180ea49cd9bd69f916553c3c2c920b4621
infrafast <szemrot@hotmail.com>
2016-07-12 09:03:21 +0200

    Modified script.js

--- pool/js/script.js
+++ pool/js/script.js
@@ -20,7 +20,7 @@ function actionCall(UrlData, async=true, messageBox=null, feedback=false, confir
     var result;
     if (messageBox!==null)
         if (confirmation!==false){
-            result = confirm(messageBox);
+            result = confirm(messageBox)+"\n"+UrlData;
             if (true !== result) return false;
         }else alert(messageBox);
 	$.ajax({
