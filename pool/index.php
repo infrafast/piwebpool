2545cfaf9699511bc49ccfa9f9e32a58781bd4b1
infrafast <szemrot@hotmail.com>
2016-07-12 08:31:14 +0200

    Modified index.php

--- pool/index.php
+++ pool/index.php
@@ -405,7 +405,7 @@ $tableSettings = new TableGear($optionsSet);
         function saveCode(script){
           var xml = Blockly.Xml.workspaceToDom(workspace);
           var xml_text = Blockly.Xml.domToText(xml);
-          alert("Sauvegarde: "+updateScript(xml_text,Blockly.Lua.workspaceToCode(workspace),script));
+          //alert("Sauvegarde: "+updateScript(xml_text,Blockly.Lua.workspaceToCode(workspace),script));
           //alert("sent: "+Blockly.Lua.workspaceToCode(workspace));
         }    
         
