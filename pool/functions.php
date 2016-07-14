fd850357cedbc063a516968098359c507e8dcfb7
infrafast <szemrot@hotmail.com>
2016-07-14 17:56:06 +0200

    Modified functions.php

--- pool/functions.php
+++ pool/functions.php
@@ -198,7 +198,7 @@ function getORP(){
 
 function readSensorStream($device){
     $v1="ERR";
-   // $v1=file_get_contents("/dev/ttyUSB2",$v1);
+    $v1=file_get_contents("/dev/ttyUSB2",$v1);
     echo $v1;
     return substr($v1,0,5);
 }
