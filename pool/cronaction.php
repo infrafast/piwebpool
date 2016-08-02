2322bfa1d444b009e935395d6b8c6e99c118dac7
infrafast <szemrot@hotmail.com>
2016-08-02 14:57:39 +0200

    Modified cronaction.php

--- pool/cronaction.php
+++ pool/cronaction.php
@@ -189,11 +189,11 @@ if (!$result) {
                 $output = curl_exec( $ch );
                 curl_close($ch);
                 $arr = json_decode($output,true);
-//                print_r($arr);
-                foreach($arr['status'] as $val)
-                {
-                        echo $val['thumbnailURL'].'<br>';       
-                }                 
+                print_r($arr);
+//                foreach($arr['status'] as $val)
+//                {
+//                        echo $val['thumbnailURL'].'<br>';       
+//                }                 
             }
         }
     }
