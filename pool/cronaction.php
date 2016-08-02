0b07effe3ecb1c88cee7840e82e9cd61cec61d72
infrafast <szemrot@hotmail.com>
2016-08-02 15:50:36 +0200

    Modified cronaction.php

--- pool/cronaction.php
+++ pool/cronaction.php
@@ -192,7 +192,7 @@ if (!$result) {
                 //print_r($arr);
                 foreach($arr as $val=>$status)
                 {
-                        echo $status.' ';       
+                        echo $val.' ';       
                 }                 
             }
         }
