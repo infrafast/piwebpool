aae47542edd4d79c7edb80ae1de7f3c93e3aaf5d
infrafast <szemrot@hotmail.com>
2016-08-02 16:29:43 +0200

    Modified functions.php

--- pool/functions.php
+++ pool/functions.php
@@ -31,29 +31,26 @@ function JsonAPI($jsonCall,$devID,$value){
     // update  Domoticz JSON
     if(!function_exists("curl_init")) die("cURL extension is not installed");
     // url: $id[1] deviceID = $device value: $id[0]
-    foreach($devices as $device=>$id){ 
-        $jsonCall = str_replace("%i",$devID,$jsonCall);
-        $jsonCall = str_replace("%v",$value,$jsonCall);
-        $curl_options = array(
-                            CURLOPT_URL => $jsonCall,
-                            CURLOPT_HEADER => 0,
-                            CURLOPT_RETURNTRANSFER => TRUE,
-                            CURLOPT_TIMEOUT => 0,
-                            CURLOPT_SSL_VERIFYPEER => 0,
-                            CURLOPT_FOLLOWLOCATION => TRUE,
-                            CURLOPT_ENCODING => 'gzip,deflate',
-                            CURLOPT_USERPWD => $username . ":" . $password                                    
-                    );
-        $ch = curl_init();
-        curl_setopt_array( $ch, $curl_options );
-        $output = curl_exec( $ch );
-        curl_close($ch);
-        $arr = json_decode($output,true);
-        //print_r($arr);
-        // error processing goes here
-        //echo $arr[$statusKey]; 
-        
-    }
+    $jsonCall = str_replace("%i",$devID,$jsonCall);
+    $jsonCall = str_replace("%v",$value,$jsonCall);
+    $curl_options = array(
+                        CURLOPT_URL => $jsonCall,
+                        CURLOPT_HEADER => 0,
+                        CURLOPT_RETURNTRANSFER => TRUE,
+                        CURLOPT_TIMEOUT => 0,
+                        CURLOPT_SSL_VERIFYPEER => 0,
+                        CURLOPT_FOLLOWLOCATION => TRUE,
+                        CURLOPT_ENCODING => 'gzip,deflate',
+                        CURLOPT_USERPWD => $username . ":" . $password                                    
+                );
+    $ch = curl_init();
+    curl_setopt_array( $ch, $curl_options );
+    $output = curl_exec( $ch );
+    curl_close($ch);
+    $arr = json_decode($output,true);
+    //print_r($arr);
+    // error processing goes here
+    //echo $arr[$statusKey]; 
 }
 
 
