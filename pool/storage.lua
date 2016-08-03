48f07d2d383e2d4ee6b69b085cdd71edce3c7ade
infrafast <szemrot@hotmail.com>
2016-08-03 18:42:59 +0200

    Modified storage.lua

--- pool/storage.lua
+++ pool/storage.lua
@@ -5,9 +5,10 @@ local multiRefObjects = {
 local obj1 = {
 	[0] = 1;
 	[2] = "{variable [\"texte\", \"accentu&eacute;\"], & virgule /@# (qualit&eacute; de l'eau)}999";
-	[1] = 1;
 	["clef"] = "text";
+	[1] = 1;
+	["test"] = "3&deg;C attention &agrave; la qualit&eacute; de l'eau et aux caract&egrave;res sp&eacute;&ccedil;iaux [] {} \"OK";
+	["jel"] = 1;
 	["temperatureFaible"] = 1;
-	["gel"] = 1;
 }
 return obj1
