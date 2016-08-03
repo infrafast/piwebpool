7426c7521ac2d6d1e0be027305ac8d93066686a6
infrafast <szemrot@hotmail.com>
2016-08-03 19:22:02 +0200

    Modified functions.php

--- pool/functions.php
+++ pool/functions.php
@@ -196,7 +196,7 @@ function getDevice($id){
 
 
 function getTemperature(){
-    return round( (0.5 + (2.5 - 0.5) * (mt_rand() / mt_getrandmax())), 1, PHP_ROUND_HALF_UP);
+    //return round( (0.5 + (2.5 - 0.5) * (mt_rand() / mt_getrandmax())), 1, PHP_ROUND_HALF_UP);
     //
     for ($i = 0; $i < 2; $i++){
         $v1 = round(readSensor(getDevice("temp")), 1,PHP_ROUND_HALF_UP);
