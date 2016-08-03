0f6d191a54e5a5ea94a26a3b8669b57dcf770f09
infrafast <szemrot@hotmail.com>
2016-08-03 19:07:42 +0200

    Modified header.lua

--- pool/sql/header.lua
+++ pool/sql/header.lua
@@ -170,7 +170,7 @@ writers = {
 
 memoire = {["gel"] = 0, b = "test"};
 persistence.store("storage.lua", memoire);
-t_restored = persistence.load("storage.lua");
+memoire = persistence.load("storage.lua");
 
 memoire = persistence.load("storage.lua");
 
