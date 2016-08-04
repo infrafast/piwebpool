9f8de9e57e6c0c4c58034a8c47a1aae37a3de3c6
infrafast <szemrot@hotmail.com>
2016-08-04 14:30:52 +0200

    Modified script.js

--- pool/js/script.js
+++ pool/js/script.js
@@ -15,16 +15,17 @@ if ("geolocation" in navigator) {
   });
 
 function loadWeather(location, woeid) {
-    alert("loadWeather");
   $.simpleWeather({
     location: location,
     woeid: woeid,
     unit: 'c',
     success: function(weather) {
+      alert("loadWeather success");
       html = '<h2><i class="icon-'+weather.code+'"></i> '+weather.temp+'&deg;'+weather.units.temp+'</h2>';
-    $("#weather").html(html);
+      $("#weather").html(html);
     },
     error: function(error) {
+              alert("loadWeather error");
       $("#weather").html('<p>'+error+'</p>');
     }
   });
