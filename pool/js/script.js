b5ec44c0a908859e469891a6e0d356de963781ec
infrafast <szemrot@hotmail.com>
2016-08-04 14:24:03 +0200

    Modified script.js

--- pool/js/script.js
+++ pool/js/script.js
@@ -1,3 +1,35 @@
+
+
+// weather
+
+/* Does your browser support geolocation? */
+if ("geolocation" in navigator) {
+  $('.js-geolocation').show(); 
+} else {
+  $('.js-geolocation').hide();
+}
+
+/* Where in the world are you? */
+  navigator.geolocation.getCurrentPosition(function(position) {
+    loadWeather(position.coords.latitude+','+position.coords.longitude); //load weather using your lat/lng coordinates
+  });
+
+function loadWeather(location, woeid) {
+  $.simpleWeather({
+    location: location,
+    woeid: woeid,
+    unit: 'c',
+    success: function(weather) {
+      html = '<h2><i class="icon-'+weather.code+'"></i> '+weather.temp+'&deg;'+weather.units.temp+'</h2>';
+    $("#weather").html(html);
+   },
+    error: function(error) {
+      $("#weather").html('<p>'+error+'</p>');
+    }
+  });
+}
+
+
 // Attention  : il faudrait exploiter result.state et result.answer et non pas result.error
 
 $('.header').click(function(){
