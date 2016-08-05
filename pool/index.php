e0b1dd236e87462f505b45d56109532af4dd18da
infrafast <szemrot@hotmail.com>
2016-08-05 10:36:28 +0200

    Modified index.php

--- pool/index.php
+++ pool/index.php
@@ -74,7 +74,7 @@ $tableSettings = new TableGear($optionsSet);
         <script src="blockly/lua_compressed.js"></script>
         <script src="blockly/blocks_compressed.js"></script>
         <script src="blockly/msg/js/fr.js"></script>
-        <link type="text/css" rel="stylesheet" href="css/tablegear.css" />
+        <link rel="stylesheet" href="css/tablegear.css" />
         <link rel="stylesheet" href="css/style.css">
         <link rel="stylesheet" href="css/weather-icons.css">
     
