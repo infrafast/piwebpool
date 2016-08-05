868b70c7ed2791f24325b299c8b61c8748cae905
szemro <szemrot@hotmail.com>
2016-08-05 17:07:55 +0200

    Modified setup.sh

--- pool/setup.sh
+++ pool/setup.sh
@@ -13,6 +13,8 @@ echo "DOWNLOADING AND SETTING UP ALL NECESSARY PACKAGES"
 sudo apt-get update
 sudo apt-get --assume-yes install apache2 php5 php5-mysql php5-gd ssmtp anacron mysql-server lua5.1 liblua5.1 php-pear php5-dev python-serial php5-curl watchdog
 pause
+echo "watchdog setup"
+sudo modprobe bcm2708_wdog
 
 # installing and compiling LUA 
 echo "NOW CONFIGURING LUA  (symlink and lobrary copy)"
