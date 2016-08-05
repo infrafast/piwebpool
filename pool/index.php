7a22bee6eba7a485ac20a0542bf0f32ef29f5674
infrafast <szemrot@hotmail.com>
2016-08-05 14:05:50 +0200

    Modified index.php

--- pool/index.php
+++ pool/index.php
@@ -68,7 +68,7 @@ $tableSettings = new TableGear($optionsSet);
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
         <title>Gestion piscine</title>
-        <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
+        <script type="text/javascript" src="js/jquery/1.4.2/jquery.min.js"></script>
         <script type="text/javascript" src="js/TableGear1.6.1-jQuery.js"></script>
         <script src="blockly/blockly_compressed.js"></script>
         <script src="blockly/lua_compressed.js"></script>
