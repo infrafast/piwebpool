0a3ef5a0d297290c01eac1c59cce4266e59845aa
infrafast <szemrot@hotmail.com>
2016-08-05 12:19:36 +0200

    Modified weather.js

--- pool/js/weather.js
+++ pool/js/weather.js
@@ -23,7 +23,7 @@ function loadWeather(location, woeid) {
       
       for(var i=0;i<3;i++) {
         html += ' '+weather.forecast[i].day
-            +': '+weather.forecast[i].high+'<img src="'+weather.forecast[i].thumbnail+'"></img>';
+            +': '+weather.forecast[i].high+'°<img src="'+weather.forecast[i].thumbnail+'"></img>';
       }      
       
       $("#weather").html(html);
