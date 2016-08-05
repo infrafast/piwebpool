0a89a4a17cf1cdc1f7a0e328ac319d6891606c01
infrafast <szemrot@hotmail.com>
2016-08-05 18:58:32 +0200

    Modified hourlycrontab.sh

--- pool/hourlycrontab.sh
+++ pool/hourlycrontab.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 
-#execute periodic piweb actions
-logger "Executing piweb cronaction"
-cd /var/www/html/pool
-php -dextension=lua.so cronaction.php
-logger "piweb cronaction finished"
 
 # cron script for checking wlan connectivity
 # and reboot raspberry in case of loss
@@ -16,14 +11,14 @@ PING="/bin/ping"
 IFUP="/sbin/ifup"
 IFDOWN="/sbin/ifdown --force"
 
-INTERFACE="eth0"
+INTERFACE="wlan0"
 
 FFLAG="/var/www/html/pool/stuck.fflg"
 # ping test
 $PING -c $PING_COUNT $IP_FOR_TEST > /dev/null 2> /dev/null
 if [ $? -ge 1 ]
 then
-    logger "$INTERFACE seems to be down, trying to bring it up..."
+    logger "ping $IP_FOR_TEST faild : $INTERFACE seems to be down, trying to bring it up..."
         if [ -e $FFLAG ]
         then
                 logger "$INTERFACE is still down, REBOOT to recover ..."
@@ -40,3 +35,10 @@ else
     logger "$IP_FOR_TEST is alive ..."
     rm -f $FFLAG 2>/dev/null
 fi
+
+
+#execute periodic piweb actions
+logger "Executing piweb cronaction"
+cd /var/www/html/pool
+php -dextension=lua.so cronaction.php
+logger "piweb cronaction finished"
