d46e5d1ebcbc6e00750c7fef519d82e38bf5ba2b
infrafast <szemrot@hotmail.com>
2016-08-05 10:07:23 +0200

    Modified index.php

--- pool/index.php
+++ pool/index.php
@@ -80,7 +80,7 @@ $tableSettings = new TableGear($optionsSet);
     <body>
 
 <?php 
-    for ($x = 0; $x <= 10; $x++) echo "n: ".$x."<i class='icon-".$x."'></i><br>";
+    for ($x = 0; $x < 47; $x++) echo "n: ".$x."<i class='icon-".$x."'></i><br>";
 ?>
 
 
