0ed555371ff8b0e51086fc70268c9db800b2c2a5
infrafast <szemrot@hotmail.com>
2016-08-08 15:14:44 +0200

    Modified index.php

--- pool/index.php
+++ pool/index.php
@@ -93,7 +93,7 @@ $tableSettings = new TableGear($optionsSet);
         <td colspan="3"><b><span>-</i></span> Mesures</b></td>
     </tr>
     <tr>
-        <th colspan="3"><div id="weather">Affichage météo?</div><div id="canvas"></div></th>
+        <th colspan="3"><div id="weather">Affichage météo?</div></th>
     </tr>
     <tr>
         <th width=33%>ph</th><th width=33%>redox</th><th width=33%>température</th>
