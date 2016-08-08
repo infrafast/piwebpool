a555d06323e18016978391cdc7c4ea61afe171b9
infrafast <szemrot@hotmail.com>
2016-08-08 09:50:06 +0200

    Modified storage.lua

--- pool/storage.lua
+++ pool/storage.lua
@@ -3,8 +3,9 @@ local multiRefObjects = {
 
 } -- multiRefObjects
 local obj1 = {
-	["temperature"] = 4;
-	["b"] = "test";
+	["b"] = 0;
+	["temperature"] = 0;
+	["traitement1"] = 0;
 	["gel"] = 0;
 }
 return obj1
