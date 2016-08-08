176e64f6978af353179a49a58e3455de7eb73783
infrafast <szemrot@hotmail.com>
2016-08-08 14:59:47 +0200

    Modified bubbles.js

--- pool/js/bubbles.js
+++ pool/js/bubbles.js
@@ -71,10 +71,11 @@
     window.onload = function () {
         alert("bubbles called");
         paper = Raphael("canvas", 500, 250);
-        var infobox = new Infobox(paper, {x:10,y:10, width:250, height:250});
+        var infobox = new Infobox(paper, {x:10,y:10, width:500, height:250});
         infobox.div.html('<div id="weather">Affichage météo?</div>');
         // finally, call the weather snippet
         loadWeather("45.840491, 6.085538");
+        
         circs = paper.set();
         for (i = 0; i < 15; ++i)
         {
