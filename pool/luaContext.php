ddcc59dfcee19e160fa697874c408d9ffd222139
infrafast <szemrot@hotmail.com>
2016-08-09 13:30:16 +0200

    Modified luaContext.php

--- pool/luaContext.php
+++ pool/luaContext.php
@@ -42,7 +42,7 @@ function goLua($luaCode,$materials,$pins,&$feedback,$link){
             while ($row = mysql_fetch_assoc($result)) {
                 $id=($row['id']);
                 $value=($row['value']);
-                $lua->assign("parametre['".$id."'']",$value);
+                $lua->assign("parametre['".$id."']",$value);
                 appendlualog("assign(\"parametre['".$id."''],".$value.")");
             }
         }    
