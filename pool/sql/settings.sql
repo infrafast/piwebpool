460ea70247663369b723d091de0026b150454525
infrafast <szemrot@hotmail.com>
2016-08-09 13:52:44 +0200

    Modified settings.sql

--- pool/sql/settings.sql
+++ pool/sql/settings.sql
@@ -11,6 +11,7 @@ INSERT INTO `settings` (`id`, `value`, `userSetting`,`description`) VALUES
 ('ORPConsign', 750, true, 'Consigne ORP'),
 ('PHConsign', 7.24, true, 'Consigne PH'),
 ('TEMPConsign', 28, true, 'Consigne Température'),
+('email', "szemrot@hotmail.com", true, 'email recevant les notifications'),
 ('logTable', 1, false, ''),
 ('actionTable', 1, false, ''),
 ('blocklyTable', 1, false, ''),
