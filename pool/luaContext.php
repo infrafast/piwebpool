3674a57b8b847e48dca37fa190db5cc05ef8846e
infrafast <szemrot@hotmail.com>
2016-08-09 14:11:14 +0200

    Modified luaContext.php

--- pool/luaContext.php
+++ pool/luaContext.php
@@ -42,9 +42,10 @@ function goLua($luaCode,$materials,$pins,&$feedback,$link){
             while ($row = mysql_fetch_assoc($result)) {
                 $id=($row['id']);
                 $value=($row['value']);
+                if (!is_numeric($value)) $value.="";
                 //$lua->assign("parametre['".$id."']",$value);
                 //appendlualog("   assign(\"parametre['".$id."']\",".$value.")    ");
-                $lua->assign($id,is_numeric($value)?$value:"".$is_numeric($value));
+                $lua->assign($id,$value);
                 appendlualog("   assign(".$id.",".$value.")    ");
             }
         }    
