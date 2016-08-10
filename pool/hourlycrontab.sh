6ffa9ff32ead5be4a385daade01c67ab1a67f224
infrafast <szemrot@hotmail.com>
2016-08-10 13:39:02 +0200

    Modified hourlycrontab.sh

--- pool/hourlycrontab.sh
+++ pool/hourlycrontab.sh
@@ -6,39 +6,37 @@
 IP_FOR_TEST="$(hostname -f).infrafast.com"
 #IP_FOR_TEST="www.google.com"
 PING_COUNT=1
-
+LOGID="HOURLYCRONTAB.SH:"
 PING="/bin/ping"
 IFUP="/sbin/ifup"
 IFDOWN="/sbin/ifdown --force"
-
 INTERFACE="wlan0"
-
 FFLAG="/var/www/html/pool/stuck.fflg"
+
 # ping test
 $PING -c $PING_COUNT $IP_FOR_TEST > /dev/null 2> /dev/null
 if [ $? -ge 1 ]
 then
-    logger "ping $IP_FOR_TEST faild : $INTERFACE seems to be down, trying to bring it up..."
+    logger "$LOGID ping $IP_FOR_TEST faild : $INTERFACE seems to be down..."
         if [ -e $FFLAG ]
         then
-                logger "$INTERFACE is still down, REBOOT to recover ..."
+                logger "$LOGID $INTERFACE is still down, REBOOT to recover ..."
                 rm -f $FFLAG 2>/dev/null
                 sudo reboot
         else
+                logger "$LOGID restarting $INTERFACE ..."
                 touch $FFLAG
                 sudo ifdown $INTERFACE && sudo service networking restart && sudo ifup $INTERFACE
                 sudo systemctl daemon-reload
-                logger "$INTERFACE restarted ..."
         fi
 else
-#    logger "$INTERFACE is up"
-    logger "$IP_FOR_TEST is alive ..."
+    logger "$LOGID $INTERFACE is up and $IP_FOR_TEST is alive"
     rm -f $FFLAG 2>/dev/null
 fi
 
 
 #execute periodic piweb actions
-logger "Executing piweb cronaction"
+logger "$LOGID Executing piweb cronaction"
 cd /var/www/html/pool
-php -dextension=lua.so cronaction.php
-logger "piweb cronaction finished"
+#php -dextension=lua.so cronaction.php
+php cronaction.php
