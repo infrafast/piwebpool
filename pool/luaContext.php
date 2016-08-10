69d675a81433873d11220eed2c974823ab43885d
infrafast <szemrot@hotmail.com>
2016-08-10 10:12:40 +0200

    Modified luaContext.php

--- pool/luaContext.php
+++ pool/luaContext.php
@@ -32,6 +32,8 @@ function goLua($luaCode,$materials,$pins,&$feedback,$link,$scriptID="emptyScript
         $lua->assign("orp",$orpValue);
         $lua->assign("period",intval(getCurrentTimeWindow()));
         $lua->assign("hour",intval(getCurrentTime()));
+        $lua->assign("timestamp",intval(getCurrentTimeWindow()));
+
         
         $lua->assign("scriptID",$scriptID);
 
