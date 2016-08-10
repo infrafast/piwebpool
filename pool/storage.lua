faf4906c6c4b7fa3716bae923391ced612e4575a
infrafast <szemrot@hotmail.com>
2016-08-10 11:31:27 +0200

    Modified storage.lua

--- pool/storage.lua
+++ pool/storage.lua
@@ -7,7 +7,7 @@ local obj1 = {
 	["warning"] = "  Attention au risque de sur-traitement, traitement arr&ecirc;t&eacute;11:17:11, ligne suivante ";
 	["traitement1"] = 0;
 	["gel"] = 0;
-	["erreurLectureTemp"] = 0;
+	["erreurLectureSonde"] = 0;
 	["erreurLecturePh"] = 0;
 	["erreurLectureOrp"] = 0;
 }
