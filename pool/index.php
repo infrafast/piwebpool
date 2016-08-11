c0550ef61072037934a93359905bc9fe6b36a9b8
infrafast <szemrot@hotmail.com>
2016-08-11 14:23:14 +0200

    Modified index.php

--- pool/index.php
+++ pool/index.php
@@ -505,7 +505,6 @@
         });        
         
         function updateGraph(element){
-            element.redraw();
             var link='graph.php?';
             var imgSrc = 'url('+link+element.id+'&period='+document.getElementById('periodID').value+'&width='+element.offsetWidth+'&height='+element.offsetHeight+'&type='+element.classList.item(0)+')';
             element.style.backgroundSize="100% 100%";
