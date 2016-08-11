a74ea787e7032f46814b4207412777cb9e879bbf
infrafast <szemrot@hotmail.com>
2016-08-11 10:46:20 +0200

    Modified index.php

--- pool/index.php
+++ pool/index.php
@@ -1,67 +1,66 @@
 <?php
-
-require_once('configuration.php');
-require_once('functions.php');
-include("include/TableGear1.6.1.php");
-
-$options["database"]["table"]  = "pumpSchedule";
-$options["pagination"] = array();
-$options["title"] = "Planificateur";
-$options["allowDelete"] = false;
-$options["sortable"]  = ""; 
-$options["selects"] = array(
-	"below0" => array("on" => 1, "off" => 0),
-	"0to2" => array("on" => 1, "off" => 0),
-	"2to4" => array("on" => 1, "off" => 0),
-	"4to6" => array("on" => 1, "off" => 0),
-	"6to8" => array("on" => 1, "off" => 0),
-	"8to10" => array("on" => 1, "off" => 0),
-	"10to12" => array("on" => 1, "off" => 0),
-	"12to14" => array("on" => 1, "off" => 0),
-	"14to16" => array("on" => 1, "off" => 0),
-	"16to18" => array("on" => 1, "off" => 0),
-	"18to20" => array("on" => 1, "off" => 0),
-	"20to22" => array("on" => 1, "off" => 0),
-	"22to24" => array("on" => 1, "off" => 0),
-	"24to26" => array("on" => 1, "off" => 0),
-	"26to28" => array("on" => 1, "off" => 0),
-	"above28" => array("on" => 1, "off" => 0)
-); 
-$options["transform"]["below0"] = array("tag" => "img","attrib" => array("src" => "images/{DATA}.png"));
-$options["transform"]["0to2"] = array("tag" => "img","attrib" => array("src" => "images/{DATA}.png"));
-$options["transform"]["2to4"] = array("tag" => "img","attrib" => array("src" => "images/{DATA}.png"));
-$options["transform"]["4to6"] = array("tag" => "img","attrib" => array("src" => "images/{DATA}.png"));
-$options["transform"]["6to8"] = array("tag" => "img","attrib" => array("src" => "images/{DATA}.png"));
-$options["transform"]["8to10"] = array("tag" => "img","attrib" => array("src" => "images/{DATA}.png"));
-$options["transform"]["10to12"] = array("tag" => "img","attrib" => array("src" => "images/{DATA}.png"));
-$options["transform"]["12to14"] = array("tag" => "img","attrib" => array("src" => "images/{DATA}.png"));
-$options["transform"]["14to16"] = array("tag" => "img","attrib" => array("src" => "images/{DATA}.png"));
-$options["transform"]["16to18"] = array("tag" => "img","attrib" => array("src" => "images/{DATA}.png"));
-$options["transform"]["18to20"] = array("tag" => "img","attrib" => array("src" => "images/{DATA}.png"));
-$options["transform"]["20to22"] = array("tag" => "img","attrib" => array("src" => "images/{DATA}.png"));
-$options["transform"]["22to24"] = array("tag" => "img","attrib" => array("src" => "images/{DATA}.png"));
-$options["transform"]["24to26"] = array("tag" => "img","attrib" => array("src" => "images/{DATA}.png"));
-$options["transform"]["26to28"] = array("tag" => "img","attrib" => array("src" => "images/{DATA}.png"));
-$options["transform"]["above28"] = array("tag" => "img","attrib" => array("src" => "images/{DATA}.png"));
-$table = new TableGear($options);
-
-$optionsSet = array();
-$optionsSet["database"] = array();		    
-$optionsSet["database"]["host"]        = $options["database"]["host"];
-$optionsSet["database"]["name"]        = $options["database"]["name"];
-$optionsSet["database"]["username"]    = $options["database"]["username"];
-$optionsSet["database"]["password"]    = $options["database"]["password"] ;
-$optionsSet["database"]["table"]  = "settings";
-$optionsSet["database"]["noAutoQuery"]=true;
-$optionsSet["pagination"] = array();
-$optionsSet["title"] = "Parametres";
-$optionsSet["allowDelete"] = false;
-$optionsSet["sortable"]  = ""; 
-$optionsSet["editable"] = true;
-$optionsSet["selects"] = array(
-	"userSetting" => array("visible" => 1, "invisible" => 0)
-); 
-$tableSettings = new TableGear($optionsSet);
+    require_once('configuration.php');
+    require_once('functions.php');
+    include("include/TableGear1.6.1.php");
+    
+    $options["database"]["table"]  = "pumpSchedule";
+    $options["pagination"] = array();
+    $options["title"] = "Planificateur";
+    $options["allowDelete"] = false;
+    $options["sortable"]  = ""; 
+    $options["selects"] = array(
+    	"below0" => array("on" => 1, "off" => 0),
+    	"0to2" => array("on" => 1, "off" => 0),
+    	"2to4" => array("on" => 1, "off" => 0),
+    	"4to6" => array("on" => 1, "off" => 0),
+    	"6to8" => array("on" => 1, "off" => 0),
+    	"8to10" => array("on" => 1, "off" => 0),
+    	"10to12" => array("on" => 1, "off" => 0),
+    	"12to14" => array("on" => 1, "off" => 0),
+    	"14to16" => array("on" => 1, "off" => 0),
+    	"16to18" => array("on" => 1, "off" => 0),
+    	"18to20" => array("on" => 1, "off" => 0),
+    	"20to22" => array("on" => 1, "off" => 0),
+    	"22to24" => array("on" => 1, "off" => 0),
+    	"24to26" => array("on" => 1, "off" => 0),
+    	"26to28" => array("on" => 1, "off" => 0),
+    	"above28" => array("on" => 1, "off" => 0)
+    ); 
+    $options["transform"]["below0"] = array("tag" => "img","attrib" => array("src" => "images/{DATA}.png"));
+    $options["transform"]["0to2"] = array("tag" => "img","attrib" => array("src" => "images/{DATA}.png"));
+    $options["transform"]["2to4"] = array("tag" => "img","attrib" => array("src" => "images/{DATA}.png"));
+    $options["transform"]["4to6"] = array("tag" => "img","attrib" => array("src" => "images/{DATA}.png"));
+    $options["transform"]["6to8"] = array("tag" => "img","attrib" => array("src" => "images/{DATA}.png"));
+    $options["transform"]["8to10"] = array("tag" => "img","attrib" => array("src" => "images/{DATA}.png"));
+    $options["transform"]["10to12"] = array("tag" => "img","attrib" => array("src" => "images/{DATA}.png"));
+    $options["transform"]["12to14"] = array("tag" => "img","attrib" => array("src" => "images/{DATA}.png"));
+    $options["transform"]["14to16"] = array("tag" => "img","attrib" => array("src" => "images/{DATA}.png"));
+    $options["transform"]["16to18"] = array("tag" => "img","attrib" => array("src" => "images/{DATA}.png"));
+    $options["transform"]["18to20"] = array("tag" => "img","attrib" => array("src" => "images/{DATA}.png"));
+    $options["transform"]["20to22"] = array("tag" => "img","attrib" => array("src" => "images/{DATA}.png"));
+    $options["transform"]["22to24"] = array("tag" => "img","attrib" => array("src" => "images/{DATA}.png"));
+    $options["transform"]["24to26"] = array("tag" => "img","attrib" => array("src" => "images/{DATA}.png"));
+    $options["transform"]["26to28"] = array("tag" => "img","attrib" => array("src" => "images/{DATA}.png"));
+    $options["transform"]["above28"] = array("tag" => "img","attrib" => array("src" => "images/{DATA}.png"));
+    $table = new TableGear($options);
+    
+    $optionsSet = array();
+    $optionsSet["database"] = array();		    
+    $optionsSet["database"]["host"]        = $options["database"]["host"];
+    $optionsSet["database"]["name"]        = $options["database"]["name"];
+    $optionsSet["database"]["username"]    = $options["database"]["username"];
+    $optionsSet["database"]["password"]    = $options["database"]["password"] ;
+    $optionsSet["database"]["table"]  = "settings";
+    $optionsSet["database"]["noAutoQuery"]=true;
+    $optionsSet["pagination"] = array();
+    $optionsSet["title"] = "Parametres";
+    $optionsSet["allowDelete"] = false;
+    $optionsSet["sortable"]  = ""; 
+    $optionsSet["editable"] = true;
+    $optionsSet["selects"] = array(
+    	"userSetting" => array("visible" => 1, "invisible" => 0)
+    ); 
+    $tableSettings = new TableGear($optionsSet);
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
