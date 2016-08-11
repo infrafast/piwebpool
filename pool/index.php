82abdc4d1d859a978a5db826b37a2faef1763a61
infrafast <szemrot@hotmail.com>
2016-08-11 16:14:20 +0200

    Modified index.php

--- pool/index.php
+++ pool/index.php
@@ -237,7 +237,7 @@
                 case 'sensorTable':
                     loadWeather("45.840491, 6.085538",0);
                     updateMeasuresGraphs();
-                    toggleGraph();
+                    toggleGraph(document.getElementById('graph=ph'));
                     refreshValue(document.getElementById('divPhMeasureID'),'Ph');
                     refreshValue(document.getElementById('divORPMeasureID'),'ORP');
                     refreshValue(document.getElementById('divTemperatureMeasureID'),'Temperature');
