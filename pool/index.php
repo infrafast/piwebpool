143515a820b25b4ebfabe7c8e7a1536f9556c024
infrafast <szemrot@hotmail.com>
2016-08-11 10:20:39 +0200

    Modified index.php

--- pool/index.php
+++ pool/index.php
@@ -70,12 +70,12 @@ $tableSettings = new TableGear($optionsSet);
         <title>Piweb</title>
         <script type="text/javascript" src="js/jquery.min.js"></script>
         <script type="text/javascript" src="js/TableGear1.6.1-jQuery.js"></script>
-        <script src="js/piwebscript.js"></script>
-        <script src="js/weather.js"></script>
-        <script src="blockly/blockly_compressed.js"></script>
-        <script src="blockly/lua_compressed.js"></script>
-        <script src="blockly/blocks_compressed.js"></script>
-        <script src="blockly/msg/js/fr.js"></script>
+        <script type="text/javascript" src="js/piwebscript.js"></script>
+        <script type="text/javascript" src="js/weather.js"></script>
+        <script type="text/javascript" src="blockly/blockly_compressed.js"></script>
+        <script type="text/javascript" src="blockly/lua_compressed.js"></script>
+        <script type="text/javascript" src="blockly/blocks_compressed.js"></script>
+        <script type="text/javascript" src="blockly/msg/js/fr.js"></script>
         
         <link rel="stylesheet" href="css/tablegear.css" />
         <link rel="stylesheet" href="css/style.css">
