0c5137ed650a39d1db3a7dc472decb8b0afcf579
infrafast <szemrot@hotmail.com>
2016-08-11 10:09:18 +0200

    Modified index.php

--- pool/index.php
+++ pool/index.php
@@ -204,6 +204,24 @@ $tableSettings = new TableGear($optionsSet);
         </xml>
         
     <script>
+        $('.header').click(function(){
+        	$(this).addClass('loading');
+            $(this).find('span').text(function(_, value){return value=='-'?'+':'-'});
+            $(this).nextUntil('tr.header').slideToggle(); 
+            var id=$(this).attr('id');
+            var urlCall="./action.php?extendedJson&action=updateSetting&id="+id+"&value="+($(this).find('span').text()=='-'?'0':'1');
+            //alert('urlCall : '+urlCall);
+            $.ajax({
+                type: "POST",
+            	url: urlCall,
+            	async:false,
+                success: function(r){
+            }});
+            $(this).removeClass('loading');
+        });
+
+        
+        
         var graphTypes = ["lineType", "barType", "textType"]; 
         
         // call the weather snippet (see weather.js) that fill in the content of <div id="weather">
