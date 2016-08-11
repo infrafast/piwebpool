70206968447e63f670c0989161135b332f7ef15e
infrafast <szemrot@hotmail.com>
2016-08-11 15:26:49 +0200

    Modified index.php

--- pool/index.php
+++ pool/index.php
@@ -493,6 +493,22 @@
           alert("Sauvegarde: "+updateScript(xml_text,Blockly.Lua.workspaceToCode(workspace),script));
           //alert("sent: "+Blockly.Lua.workspaceToCode(workspace));
         }    
+
+        var forceRedraw = function(element){
+        
+            if (!element) { return; }
+        
+            var n = document.createTextNode(' ');
+            var disp = element.style.display;  // don't worry about previous display style
+        
+            element.appendChild(n);
+            element.style.display = 'none';
+        
+            setTimeout(function(){
+                element.style.display = disp;
+                n.parentNode.removeChild(n);
+            },20); // you can play with this timeout to make it as short as possible
+        }
         
         function updateGraph(element){
 
