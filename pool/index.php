8637dd72fe5f8b63cf1781f8fd497c7985145be1
infrafast <szemrot@hotmail.com>
2016-08-11 14:13:15 +0200

    Modified index.php

--- pool/index.php
+++ pool/index.php
@@ -495,7 +495,7 @@
         }    
         
         function updateGraph(element){
-            element..hide().show(0);
+            element.hide().show(0);
             var link='graph.php?';
             var imgSrc = 'url('+link+element.id+'&period='+document.getElementById('periodID').value+'&width='+element.offsetWidth+'&height='+element.offsetHeight+'&type='+element.classList.item(0)+')';
             element.style.backgroundSize="100% 100%";
