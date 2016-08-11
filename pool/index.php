70eb6b34c482fb2ab2a3d7d1d939b4df66444e03
infrafast <szemrot@hotmail.com>
2016-08-11 11:25:58 +0200

    Modified index.php

--- pool/index.php
+++ pool/index.php
@@ -82,6 +82,17 @@
         <link rel="stylesheet" href="css/weather-icons.css">
         <link rel="stylesheet" href="css/weather-snippet.css">
     </head>
+    
+    <script>
+        // Show full page LoadingOverlay
+        $.LoadingOverlay("show");
+        
+        // Hide it after 3 seconds
+        setTimeout(function(){
+            $.LoadingOverlay("hide");
+        }, 3000);
+    </script>
+    
     <body>
 
     <table class="materialTab">
@@ -200,14 +211,6 @@
     </xml>
 
     <script>
-    
-        // Show full page LoadingOverlay
-        $.LoadingOverlay("show");
-        
-        // Hide it after 3 seconds
-        setTimeout(function(){
-            $.LoadingOverlay("hide");
-        }, 3000);
     
         $('.header').click(function(){
         	$(this).addClass('loading');
