125d343d445fd21c8b3184a95df582977bce6e4d
infrafast <szemrot@hotmail.com>
2016-08-12 14:09:21 +0200

    Modified index.php

--- pool/index.php
+++ pool/index.php
@@ -408,6 +408,7 @@
             //alert("received: "+xml_text);
             var xml = Blockly.Xml.textToDom(xml_text);
             Blockly.Xml.domToWorkspace(xml, workspace);
+            alert(document.getElementById('unlock').value);
             if (script=="main" && document.getElementById('unlock').value == true){
                 Blockly.mainWorkspace.options.maxBlocks = 1;
                 Blockly.mainWorkspace.options.disabled = true;
