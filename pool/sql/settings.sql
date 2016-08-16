ef5d2689f1cf83f9dfd0bc7829daee0311602b37
infrafast <szemrot@hotmail.com>
2016-08-16 12:32:40 +0200

    Modified settings.sql

--- pool/sql/settings.sql
+++ pool/sql/settings.sql
@@ -7,7 +7,7 @@ CREATE TABLE IF NOT EXISTS `settings` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
   
 INSERT INTO `settings` (`id`, `value`, `userSetting`,`description`) VALUES
-('webURL', 1, true, 'Activation planificateur filtration'),
+('webURL', 1, true, 'Page web pour push des données'),
 ('scheduler', 1, true, 'Activation planificateur filtration'),
 ('ORPConsign', 750, true, 'Consigne ORP'),
 ('PHConsign', 7.24, true, 'Consigne PH'),
