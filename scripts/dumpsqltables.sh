03b5968918398fc49b465a4d1908f3252d05d3b4
infrafast <szemrot@hotmail.com>
2016-08-18 10:53:21 +0200

    Modified dumpsqltables.sh

--- scripts/dumpsqltables.sh
+++ scripts/dumpsqltables.sh
@@ -2,4 +2,4 @@
 mysqldump -uroot -pQuintal74605 pool measures > measures.sql
 mysqldump -uroot -pQuintal74605 pool pumpSchedule > pumpSchedule.sql
 mysqldump -uroot -pQuintal74605 pool scripts > scripts.sql
-mysqldump -uroot -pQuintal74605 pool measures > measures.sql
+mysqldump -uroot -pQuintal74605 pool settings > settings.sql
