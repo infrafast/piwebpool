440482a45377b5bb8de7a18ed410504532643fb8
infrafast <szemrot@hotmail.com>
2016-08-18 11:38:01 +0200

    Modified setup.sh

--- setup.sh
+++ setup.sh
@@ -92,7 +92,7 @@ sudo usermod -a -G webide www-data
 chmod g+w ../pool
 chmod -R 0775 css js include
 #periodic execution of the script
-sudo ln -s "$piwebpooldir/piwebpool/hourlycrontab.sh" /etc/cron.hourly/poolScheduler
+sudo ln -s "$piwebpooldir/piwebpool/scripts/hourlypiwebpool.sh" /etc/cron.hourly/
 #this is to access the ttyUSB0 from apache
 sudo usermod -a -G dialout www-data
 pause
