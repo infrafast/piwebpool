5ff44f8120f54ae6d52a8a7a6ad96d42941c5d52
infrafast <szemrot@hotmail.com>
2016-08-18 11:50:15 +0200

    Modified setup.sh

--- setup.sh
+++ setup.sh
@@ -21,7 +21,7 @@ pause(){
 #php5-curl is for connecting to donmoticz or any other system via JSON call
 echo "DOWNLOADING AND SETTING UP ALL NECESSARY PACKAGES"
 sudo apt-get update
-sudo apt-get --assume-yes install apache2 php5 php5-mysql php5-gd ssmtp anacron mysql-server lua5.1 liblua5.1 php-pear php5-dev python-serial php5-curl watchdog
+sudo apt-get --assume-yes install apache2 php5 php5-mysql php5-gd ssmtp anacron mysql-server lua5.1 liblua5.1 php-pear php5-dev python-serial php5-curl
 pause
 #echo "watchdog setup"
 #sudo modprobe bcm2708_wdog
