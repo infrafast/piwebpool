ff46fb8a4fe0b41ff8239ffa6e02eb4bc47e8371
infrafast <szemrot@hotmail.com>
2016-08-19 15:19:29 +0200

    clean up webpages

--- storage.lua
+++ storage.lua
@@ -7,6 +7,6 @@ local obj1 = {
 	["gel"] = 0;
 	["erreurLectureSonde"] = 0;
 	["traitement1"] = 0;
-	["warning"] = "  Sondes &agrave; v&eacute;rifier, erreur de lecture 19:35:32: Reprise traitement19:36:36: Reprise traitement";
+	["warning"] = "  Sondes &agrave; v&eacute;rifier, erreur de lecture 19:35:32: Reprise traitement19:36:36: Reprise traitement06:17:06: S&eacute;curit&eacute; sur-traitement.2016-08-19 14:17:06: Reprise traitement";
 }
 return obj1
