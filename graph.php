e69eeccfd0f64d893c7b0c292f5ef2cf5ffd5f62
szemro <szemrot@hotmail.com>
2016-08-24 15:11:05 +0200

    Modified graph.php

--- graph.php
+++ graph.php
@@ -198,13 +198,7 @@ if (!$result) {
     while ($row = mysql_fetch_assoc($result)) {
         $id=($row['id']);
         $value=($row['value']);
-        if (is_numeric($value)) 
-        $lua->assign($id,floatval($value));
-        else 
-        $lua->assign($id,$value);
-        //$lua->assign("parametre['".$id."']",$value);
-        //appendlualog("   assign(parametre['".$id."'],".$value.")    ");
-        //appendlualog("   assign(".$id.",".$value.")    ");
+        
     }
 }    
 mysql_free_result($result);
