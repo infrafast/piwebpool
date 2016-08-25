e28a72655c8801957b8e00c3323742f3ddeb89c0
infrafast <szemrot@hotmail.com>
2016-08-25 16:39:02 +0200

    update wepages

--- storage.lua
+++ storage.lua
@@ -7,6 +7,6 @@ local obj1 = {
 	["gel"] = 0;
 	["erreurLectureSonde"] = 0;
 	["traitement1"] = 0;
-	["warning"] = " 2016-08-20 05:17:06: Reprise traitement2016-08-20 13:17:26: Reprise traitement";
+	["warning"] = " 2016-08-25 06:17:06: S&eacute;curit&eacute; sur-traitement. 2016-08-25 11:17:27: Reprise traitement. 2016-08-25 14:17:06: Reprise traitement. ";
 }
 return obj1
