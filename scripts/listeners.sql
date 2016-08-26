ad00b67c6d822482d41f77b626c93b3e6f7f3dd0
infrafast <szemrot@hotmail.com>
2016-08-26 14:39:51 +0200

    Uploaded new File.

---
+++ scripts/listeners.sql
@@ -0,0 +1,47 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.3.1
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Aug 26, 2016 at 02:39 PM
+-- Server version: 5.5.49-MariaDB
+-- PHP Version: 5.6.21
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `pool`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `listeners`
+--
+
+DROP TABLE IF EXISTS `listeners`;
+CREATE TABLE `listeners` (
+  `url` char(255) NOT NULL,
+  `material` char(60) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `listeners`
+--
+ALTER TABLE `listeners`
+  ADD PRIMARY KEY (`url`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
