263e528bf5366082eb2b176d366a7968aaa06d87
infrafast <szemrot@hotmail.com>
2016-08-26 15:58:21 +0200

    Modified index.php

--- index.php
+++ index.php
@@ -305,18 +305,26 @@
         };
     
         Blockly.Blocks['register'] = {
-            init: function() {
-                this.appendValueInput("subscribe")
-                    .setCheck(null)
-                    .appendField("souscrire")
-                    .appendField(new Blockly.FieldDropdown([
-                        <?php foreach($materials as $material=>$pin) echo '["'.$material.'","'.$material.'"],';?>
-                        ["",""]
-                      ]), "material");
-                this.setPreviousStatement(true, null);
-                this.setNextStatement(true, null);
-                this.setColour(65);
-            }
+          init: function() {
+            this.appendValueInput("url")
+                .setCheck(null)
+                .appendField("souscrire")
+                .appendField(new Blockly.FieldDropdown([["option", "OPTIONNAME"], ["option", "OPTIONNAME"], ["option", "OPTIONNAME"]]), "material")
+                .appendField(" URL");
+            this.appendValueInput("onValue")
+                .setCheck(null)
+                .setAlign(Blockly.ALIGN_RIGHT)
+                .appendField("valeur ON");
+            this.appendValueInput("offValue")
+                .setCheck(null)
+                .setAlign(Blockly.ALIGN_RIGHT)
+                .appendField("valeur OFF");
+            this.setPreviousStatement(true, null);
+            this.setNextStatement(true, null);
+            this.setColour(65);
+            this.setTooltip('');
+            this.setHelpUrl('http://www.example.com/');
+          }
         };
         
         Blockly.Lua['register'] = function(block) {
