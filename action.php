a8f187762794965cca813d8cbfc8855bf43db763
infrafast <szemrot@hotmail.com>
2016-08-26 14:47:17 +0200

    Modified action.php

--- action.php
+++ action.php
@@ -221,7 +221,7 @@ if(isset($_['action'])){
             mysql_connect($options["database"]["host"],$options["database"]["username"],$options["database"]["password"]) or die('error connection');
             mysql_select_db($options["database"]["name"]) or die('error database selection');
             //db related variables
-            $sql    = "SELECT url,material from listeners where material="++";";
+            $sql    = "SELECT url,material from listeners where material='"+$material+"';";
             $result = mysql_query($sql);
             if (!$result) {
                 $feedback=$feedback." ".mysql_error();
