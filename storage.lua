6651c504e4f13cafc0b0fc0063b522cd4718d86f
infrafast <szemrot@hotmail.com>
2016-08-26 17:30:59 +0200

    introduced listeners. Still loop issue. Added PAC and treatmt2 in PLCl and domoticz

--- storage.lua
+++ storage.lua
@@ -5,8 +5,8 @@ local multiRefObjects = {
 local obj1 = {
 	["temperature"] = 0;
 	["gel"] = 0;
-	["erreurLectureSonde"] = 0;
+	["erreurLectureSonde"] = 1;
 	["traitement1"] = 0;
-	["warning"] = " 2016-08-25 06:17:06: S&eacute;curit&eacute; sur-traitement. 2016-08-25 11:17:27: Reprise traitement. 2016-08-25 14:17:06: Reprise traitement. ";
+	["warning"] = " 2016-08-26 13:17:28: Reprise traitement. ";
 }
 return obj1
