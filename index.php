459e25e7647474a45a9a9ae8b4d356367222625d
infrafast <szemrot@hotmail.com>
2016-08-26 14:09:59 +0200

    Modified index.php

--- index.php
+++ index.php
@@ -316,8 +316,6 @@
                 this.setPreviousStatement(true, null);
                 this.setNextStatement(true, null);
                 this.setColour(65);
-                this.setTooltip('');
-                this.setHelpUrl('http://www.example.com/');
             }
         };
         
