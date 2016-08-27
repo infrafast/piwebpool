1635fd187f85a08da2892c090b36efce67955d2d
infrafast <szemrot@hotmail.com>
2016-08-27 11:51:03 +0200

    Modified index.php

--- index.php
+++ index.php
@@ -195,6 +195,7 @@
     
     <?= $tableSettings->fetchData("SELECT id,value,description from settings where userSetting=true;"); $tableSettings->getTable(); ?>
     <?= $tableSettings->getJavascript("jquery") ?>
+    <?= $tableSubscriberss->getJavascript("jquery") ?>
 
     <table class="materialTab">
     <tr class="header" id="logTable">
