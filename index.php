1cca454ee1be0bde0746f30d0129c8e782caeb9a
infrafast <szemrot@hotmail.com>
2016-09-01 15:52:10 +0200

    Modified index.php

--- index.php
+++ index.php
@@ -600,7 +600,8 @@
         });
 
         // collapse all table as per settings stored in the database
-        var collapsableTableList = ['actionTable','Planificateur','sensorTable','blocklyTable','logTable','Parametres','Souscripteurs'];
+        //var collapsableTableList = ['actionTable','Planificateur','sensorTable','blocklyTable','logTable','Parametres','Souscripteurs'];
+        var collapsableTableList = ['actionTable','sensorTable','blocklyTable','logTable'];
         for (var tableID in collapsableTableList) {
             // we collapse the section if toggleValue is 1
             if (actionCall('action=getSetting&id='+collapsableTableList[tableID],false,null,false,false)=="1") 
