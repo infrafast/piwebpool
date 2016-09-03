692fcaf1859c9ad3ef1f3c18c09678506ad16a2b
infrafast <szemrot@hotmail.com>
2016-09-03 12:08:44 +0200

    Modified index.php

--- index.php
+++ index.php
@@ -632,7 +632,7 @@
         // callback function to update code related xml and lua when the workspace is modified
         workspace.addChangeListener(myUpdateFunction);
         
-        var interval = setInterval(updateSensorValues, 5000);
+        var interval = setInterval(updateSensorValues, 30000);
 
         </script>
     </body>
