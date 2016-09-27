a91a95947122f8dc08bbe3b4db3b0b685b256024
infrafast <szemrot@hotmail.com>
2016-09-27 15:58:22 +0200

    Modified index.php

--- index.php
+++ index.php
@@ -652,9 +652,11 @@
             var source = new EventSource("functions.php");
             source.onmessage = function(event) {
                 //document.getElementById("result").innerHTML += event.data + "<br>";
+                alert("");
             };
         } else {
             //document.getElementById("result").innerHTML = "Sorry, your browser does not support server-sent events...";
+            alert("not supported");
         }
 
 
