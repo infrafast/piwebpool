2e31edc75a4f419b436268118ee7c4886ba6c78d
infrafast <szemrot@hotmail.com>
2016-09-27 17:06:24 +0200

    Modified index.php

--- index.php
+++ index.php
@@ -655,8 +655,8 @@
         
         // pull refresh interval
         var intervalMeasures = setInterval(updateSensorValues, 100000);
-        // see github issue #29, for the moment, we pull to fake an update every 1000ms = 1s
-        var intervalButton = setInterval(updateCommandsButton, 1000);
+        // see github issue #29, for the moment, we pull to fake an update every 2000ms = 2s
+        var intervalButton = setInterval(updateCommandsButton, 2000);
         
         
         </script>
