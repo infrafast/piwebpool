f8aad02e93204fd1accb60d7cbc0003ac30550c1
infrafast <szemrot@hotmail.com>
2016-09-27 15:38:00 +0200

    Modified index.php

--- index.php
+++ index.php
@@ -559,7 +559,7 @@
                     $buttonID = "commandButtonID".$material;
                     echo "var button".$material."=actionCall('getState=".$material."',false,null,false,false)==0?'off':'on';\n"; 
                     echo "document.getElementById('".$buttonID."').innerHTML='<br>'+button".$material."+'<br><br>';\n";
-                    echo "document.getElementById('".$buttonID."').classList.add('button".$material."');\n";
+                    echo "document.getElementById('".$buttonID."').classList.add(button".$material.");\n";
                 }
             ?>
         }
