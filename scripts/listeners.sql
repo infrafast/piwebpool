0d9b4fa8590f67d369b0d167ee5e30f48e29c3d4
infrafast <szemrot@hotmail.com>
2016-10-05 23:04:41 +0200

    Modified listeners.sql

--- scripts/listeners.sql
+++ scripts/listeners.sql
@@ -1,5 +1,6 @@
 DROP TABLE IF EXISTS `listeners`;
 CREATE TABLE `listeners` (
+  `id` int NOT NULL AUTO_INCREMENT,
   `url` char(255) NOT NULL,
   `material` char(60) NOT NULL,
   `valueOn` char(32) NOT NULL,
