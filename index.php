57751e0ad8359dff3c0b38d3e39aa2aba37cb92b
infrafast <szemrot@hotmail.com>
2016-10-07 14:40:58 +0200

    Modified index.php

--- index.php
+++ index.php
@@ -193,7 +193,7 @@
     
     
     
-    <?= $tableSettings->fetchData("SELECT description,value,id from settings where userSetting=true;");?>
+    <?= $tableSettings->fetchData("SELECT key,description,value,id from settings where userSetting=true;");?>
     <?= $tableSettings->getTable(); ?>
     <?= $tableSettings->getJavascript("jquery"); ?>
     
