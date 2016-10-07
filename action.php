e9654b42846c40271afb3d29825aaddff9bb9728
infrafast <szemrot@hotmail.com>
2016-10-07 10:24:24 +0200

    Modified action.php

--- action.php
+++ action.php
@@ -53,7 +53,7 @@ if(isset($_['action'])){
                             $result['answer'] = "ERROR";
                             $result['state'] = mysql_error();
                         }else
-                            $result['answer']="Offset retenu";   
+                            $result['answer']="Ecart retenu";   
                             $result['state']=$deltaTemp;
                             
                     }
