23c672a38d50d5e8ceec216c34f18b7f02a3d509
infrafast <szemrot@hotmail.com>
2017-05-25 11:08:14 +0200

    Modified index.php

--- index.php
+++ index.php
@@ -139,7 +139,7 @@
         <td class='textType' id="graph=temperature" onclick="toggleGraph(this);" ></td>
     </tr>
     <tr>
-        <td id="PhCalibrateID" style="background-repeat:no-repeat; background-image: url('');"><input type="button" value="Etalonner" onclick="calibrateAndRefresh('Ph',document.getElementById('phCalValueID').value);"><input type="text" id="phCalValueID" name="phCalValue" value="7.00" maxlength="4" size="4"><input type="checkbox" id="tempCompensation" onclick="setPersistantSettingsBoxes(id)"> T° Comp.</td>
+        <td id="PhCalibrateID" style="background-repeat:no-repeat; background-image: url('');"><input type="button" value="Etalonner" onclick="calibrateAndRefresh('Ph',document.getElementById('phCalValueID').value);"><input type="text" id="phCalValueID" name="phCalValue" value="7.00" maxlength="4" size="4"><input type="checkbox" id="tempCompensation" onclick="setPersistantSettingsBoxes(id);"> T° Comp.</td>
         <td id="ORPCalibrateID" style="background-repeat:no-repeat; background-image: url('');"><input type="button" value="Etalonner" onclick="calibrateAndRefresh('ORP',document.getElementById('orpCalValueID').value);"><input type="text" id="orpCalValueID" name="orpCalValue" value="650" maxlength="4" size="4">mV</td>
         <td id="TempCalibrateID" style="background-repeat:no-repeat; background-image: url('');"><input type="button" value="Etalonner" onclick="calibrateAndRefresh('Temp',document.getElementById('tempCalValueID').value);"><input type="text" id="tempCalValueID" name="tempCalValue" value="20" maxlength="4" size="4">°C</td>        
         <td></td>
